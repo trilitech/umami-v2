4cf9b5db71da98638ba33b4803173d6510fed2e2
Lev Kowalski <levkowalski@gmail.com>
2023-03-10 09:40:58 +0000

    Add router

---
+++ src/Router.tsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
