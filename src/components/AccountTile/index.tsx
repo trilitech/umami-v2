1660a952e7fa81c877b6635d724b6d8c52fa1913
Lev Kowalski <levkowalski@gmail.com>
2023-03-16 20:24:42 +0100

    Fix prop duplication

--- src/components/AccountTile/index.tsx
+++ src/components/AccountTile/index.tsx
@@ -1,16 +1,13 @@
 import { format } from "@taquito/utils";
 import BigNumber from "bignumber.js";
 import React from "react";
-import { AccountTileDisplay } from "./AccountTileDisplay";
+import { AccountTileDisplay, Props } from "./AccountTileDisplay";
 
-// TODO remove prop duplication from display component
-const AccountTile: React.FC<{
-  label: string;
-  address: string;
-  balance: BigNumber | null;
-  onClick: React.MouseEventHandler<HTMLDivElement>;
-  selected?: boolean;
-}> = ({ address, onClick, balance, selected = false, label }) => {
+const AccountTile: React.FC<
+  Omit<Props, "balance"> & {
+    balance: BigNumber | null;
+  }
+> = ({ address, onClick, balance, selected = false, label }) => {
   const prettyBalance = balance && `${format("mutez", "tz", balance)} tez`;
   return (
     <AccountTileDisplay
