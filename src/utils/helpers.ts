5f9332632f41784c291aa33bc2633974a7f5ddcc
Lev Kowalski <levkowalski@gmail.com>
2023-03-22 22:19:43 +0100

    helpers

---
+++ src/utils/helpers.ts
@@ -0,0 +1,7 @@
+export function filterNulls<T>(arr: Array<T | null>): T[] {
+  return arr.filter((v) => v !== null) as T[];
+}
+
+export function objectMap<T, R>(obj: Record<string, T>, fn: (v: T) => R) {
+  return Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, fn(v)]));
+}
