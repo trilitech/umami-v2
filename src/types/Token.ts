686b506912a4e46b014da0b99e8c90b8982990fe
Lev Kowalski <levkowalski@gmail.com>
2023-03-22 15:32:10 +0100

    Use token type from tzkt sdk and add metadata type that's not included

--- src/types/Token.ts
+++ src/types/Token.ts
@@ -1,14 +1,44 @@
-// Didn't find anything equivalent in @airgap/tezos
-// TODO add other fields
-export type Token = {
-  account: { address: string };
-  balance: string;
-  id: number;
-  token: {
-    contract: { address: string };
-    id: number;
-    metadata?: {
-      displayUri?: string;
+import * as tzktApi from "@tzkt/sdk-api";
+
+type TokenMetadata = {
+  name?: string;
+  symbol?: string;
+  decimals?: string;
+  description?: string;
+  artifactUri?: string;
+  displayUri?: string;
+  thumbnailUri?: string;
+  externalUri?: string;
+  isBooleanAmount?: boolean;
+  shouldPreferSymbol?: boolean;
+  isTransferable?: boolean;
+  creators?: string[];
+  tags?: string[];
+  rights?: string;
+  royalties?: {
+    shares: {
+      [prop: string]: string;
     };
+    decimals: string;
   };
+  attributes?: Array<{
+    name: string;
+    value: string;
+  }>;
+  date?: string;
+  type?: string;
+  rarity?: string;
+  language?: string;
+  formats?: Array<{
+    uri?: string;
+    fileName?: string;
+    fileSize?: string;
+    mimeType?: string;
+  }>;
 };
+
+type tokenInfo = Omit<tzktApi.TokenInfo, "metadata"> & {
+  metadata?: TokenMetadata;
+};
+
+export type Token = Omit<tzktApi.TokenBalance, "token"> & { token?: tokenInfo };
