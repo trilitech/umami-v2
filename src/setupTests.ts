c975308fa96cf6f90cea2f07509edc524903ac96
Lev Kowalski <levkowalski@gmail.com>
2023-03-23 12:53:43 +0100

    move setup tests to ts

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,13 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+jest.mock("@airgap/tezos/node_modules/@airgap/sapling-wasm", () => {
+  return {};
+});
+
+// Fixes: Cannot find module '@tzkt/oazapfts/runtime' from 'node_modules/@tzkt/sdk-api/build/main/index.js'
+jest.mock("@tzkt/sdk-api", () => {
+  return {};
+});
