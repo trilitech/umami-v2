df09a77d6305d31f53bd7cfefef8c4508d9b42e7
Lev Kowalski <levkowalski@gmail.com>
2023-03-23 16:44:37 +0100

    header for accounts in home

--- src/views/home/AccountsList.tsx
+++ src/views/home/AccountsList.tsx
@@ -1,11 +1,22 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
+import { BsWindowPlus } from "react-icons/bs";
 import AccountTile from "../../components/AccountTile";
+import { IconAndTextBtn } from "../../components/IconAndTextBtn";
 import { formatPkh } from "../../utils/format";
 import accountsSlice from "../../utils/store/accountsSlice";
 import { useAppDispatch, useAppSelector } from "../../utils/store/hooks";
 import AccountDisplayDrawer from "./AccountDisplayDrawer";
 
 const { setSelected } = accountsSlice.actions;
+
+const Header = () => {
+  return (
+    <Flex justifyContent={"space-between"} mt={4} mb={4}>
+      <Heading fontSize={"x-large"}>Accounts</Heading>
+      <IconAndTextBtn label="Add/Create" icon={BsWindowPlus} />
+    </Flex>
+  );
+};
 
 const AccountsList: React.FC<{ onOpen: () => void }> = (props) => {
   const { items: accounts, selected } = useAppSelector((s) => s.accounts);
@@ -15,6 +26,7 @@ const AccountsList: React.FC<{ onOpen: () => void }> = (props) => {
 
   return (
     <Box>
+      <Header />
       {accounts.map((a) => {
         const balance = balances[a.pkh]?.tez;
         return (
