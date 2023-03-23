c168ed50add5c37a5e9532f26006ad55afbe0547
Lev Kowalski <levkowalski@gmail.com>
2023-03-23 16:43:57 +0100

    Improve operations

--- src/views/home/OperationsList.tsx
+++ src/views/home/OperationsList.tsx
@@ -1,6 +1,6 @@
 import { useAppSelector } from "../../utils/store/hooks";
 
-import { Box } from "@chakra-ui/react";
+import { Box, Tab, TabList, Tabs } from "@chakra-ui/react";
 import { OperationTile } from "../../components/OperationTile";
 import { formatPkh } from "../../utils/format";
 
@@ -8,20 +8,34 @@ export const OperationsList = () => {
   const operations = useAppSelector((s) => s.assets.operations);
 
   const operationEls = Object.values(operations).flatMap((b) => b);
-  const last = operationEls.slice(0, 5);
+  const last = operationEls.slice(0, 20);
+
+  const operations2 = last.map((op) => {
+    return <OperationTile operation={op} />;
+  });
 
   return (
-    <Box>
-      {last.map((t) => {
-        return (
-          <OperationTile
-            key={(t.hash || "") + t.from[0]}
-            from={formatPkh(t.from[0])}
-            to={formatPkh(t.to[0])}
-            amount={t.amount}
-          />
-        );
-      })}
-    </Box>
+    <Tabs
+      height={"100%"}
+      display={"flex"}
+      flexDirection="column"
+      mt={4}
+      bg="umami.gray.900"
+      borderRadius={4}
+      // color scheme not workkig even when put int 50-900 range
+      // TODO Fix
+      // https://chakra-ui.com/docs/components/tabs
+    >
+      <TabList>
+        <Tab>All</Tab>
+        <Tab>Sent</Tab>
+        <Tab>Received</Tab>
+        <Tab>Delegations</Tab>
+      </TabList>
+
+      <Box minHeight={"10px"} overflow={"scroll"}>
+        {operations2}
+      </Box>
+    </Tabs>
   );
 };
