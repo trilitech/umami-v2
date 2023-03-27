247944b504aba12f6fe213310f4334991f87538f
Lev Kowalski <levkowalski@gmail.com>
2023-03-27 20:51:29 +0200

    Implement methods to fetch tez and token transactions

--- src/types/Operation.ts
+++ src/types/Operation.ts
@@ -1,6 +1,35 @@
+import * as tzktApi from "@tzkt/sdk-api";
+import { TokenMetadata } from "./Token";
+
 import { IAirGapTransaction } from "@airgap/coinlib-core/interfaces/IAirGapTransaction";
 
 export type Operation = Omit<
   IAirGapTransaction,
   "protocolIdentifier" | "network"
 >[];
+
+type TokenInfo = Omit<tzktApi.TokenInfo, "metadata"> & {
+  metadata?: TokenMetadata;
+};
+
+export type TokenTransfer = Omit<tzktApi.TokenTransfer, "token"> & {
+  token?: null | TokenInfo;
+};
+
+export type TezTransfer = tzktApi.TransactionOperation;
+
+// OperationDisplay is nicely formated for display in tables
+export type OperationDisplay = {
+  type?: string;
+  amount: {
+    prettyDisplay: string;
+    url?: string;
+  };
+  fee?: string;
+  sender: string;
+  recipient: string;
+  status?: string;
+  prettyTimestamp: string;
+  timestamp: string;
+  tzktUrl?: string;
+};
