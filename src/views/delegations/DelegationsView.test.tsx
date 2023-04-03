7ab6feea68217e32d69ee54a9e27c44cdb5bbfc5
Lev Kowalski <levkowalski@gmail.com>
2023-04-03 20:33:25 +0200

    Delegations View

---
+++ src/views/delegations/DelegationsView.test.tsx
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import { mockDelegation, mockPkh } from "../../mocks/factories";
+import { ReduxStore } from "../../providers/ReduxStore";
+import assetsSlice from "../../utils/store/assetsSlice";
+import { store } from "../../utils/store/store";
+import DelegationsView from "./DelegationsView";
+
+jest.mock("react-query");
+const { updateDelegations } = assetsSlice.actions;
+
+describe("<DelegationsView />", () => {
+  it("should display the delegations of all accounts", () => {
+    store.dispatch(
+      updateDelegations([
+        {
+          pkh: mockPkh(1),
+          delegation: mockDelegation(
+            1,
+            17890,
+            mockPkh(4),
+            "Baker 1",
+            new Date("2022-10-01")
+          ),
+        },
+        {
+          pkh: mockPkh(2),
+          delegation: mockDelegation(2, 85000, mockPkh(5), "Baker 2"),
+        },
+        {
+          pkh: mockPkh(3),
+          delegation: mockDelegation(3, 37490, mockPkh(6), "Baker 3"),
+        },
+      ])
+    );
+    render(
+      <ReduxStore>
+        <DelegationsView />
+      </ReduxStore>
+    );
+
+    expect(screen.getAllByTestId("delegation-row")).toHaveLength(3);
+  });
+});
---
+++ src/views/delegations/DelegationsView.tsx
@@ -0,0 +1,202 @@
+import {
+  Box,
+  Flex,
+  IconButton,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { formatRelative } from "date-fns";
+import React from "react";
+import { TbFilter } from "react-icons/tb";
+import { VscWand } from "react-icons/vsc";
+import { useQuery } from "react-query";
+import { useRenderAccountSmallTile } from "../../components/AccountSelector/AccountSmallTile";
+import { IconAndTextBtn } from "../../components/IconAndTextBtn";
+import { TopBar } from "../../components/TopBar";
+import { Delegation, makeDelegation } from "../../types/Delegation";
+import { filterNulls } from "../../utils/helpers";
+import {
+  useAllDelegations,
+  useGetAccountBalance,
+} from "../../utils/hooks/assetsHooks";
+import assetsSlice from "../../utils/store/assetsSlice";
+import { useAppDispatch } from "../../utils/store/hooks";
+import { prettyTezAmount } from "../../utils/store/impureFormat";
+import { getBakers } from "../../utils/tezos";
+import { useSendFormModal } from "../home/useSendFormModal";
+import { useRenderBakerSmallTile } from "./BakerSmallTile";
+import { CiCircleRemove } from "react-icons/ci";
+import { MdOutlineModeEdit } from "react-icons/md";
+
+const DelegationsTable = ({
+  delegations,
+  onRemoveDelegate,
+  onChangeDelegate,
+}: {
+  delegations: Delegation[];
+  onRemoveDelegate: (pkh: string) => void;
+  onChangeDelegate: (pkh: string, baker: string) => void;
+}) => {
+  const now = new Date();
+  const renderAccountTile = useRenderAccountSmallTile();
+  const renderBakerTile = useRenderBakerSmallTile();
+
+  const getAccountBalance = useGetAccountBalance();
+  return (
+    <TableContainer overflowX="unset" overflowY="unset">
+      <Table>
+        {
+          // Finally a way to have a sticky Header
+          // https://github.com/chakra-ui/chakra-ui/discussions/5656#discussioncomment-3320528
+        }
+        <Thead
+          position="sticky"
+          top={0}
+          zIndex="docked"
+          bg="umami.gray.900"
+          borderRadius={4}
+        >
+          <Tr>
+            <Th>Account:</Th>
+            <Th>Initial Balance:</Th>
+            <Th>Current Balance:</Th>
+            <Th>Duration:</Th>
+            <Th>Last Reward:</Th>
+            <Th>Baker:</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {delegations.map((d) => {
+            const balance = getAccountBalance(d.sender);
+            return (
+              <Tr key={d.id} data-testid={`delegation-row`}>
+                <Td>{renderAccountTile(d.sender)}</Td>
+                <Td>{d.amount && prettyTezAmount(d.amount)}</Td>
+                <Td>{balance && prettyTezAmount(balance)}</Td>
+                <Td>
+                  {d.timestamp &&
+                    `Since ${formatRelative(new Date(d.timestamp), now)}`}
+                </Td>
+                <Td />
+                <Td>
+                  <Flex alignItems={"center"}>
+                    {renderBakerTile(d.delegate.address)}
+                    <IconButton
+                      ml={2}
+                      mr={2}
+                      onClick={() =>
+                        onChangeDelegate(d.sender, d.delegate.address)
+                      }
+                      borderRadius={"50%"}
+                      aria-label="Change Baker"
+                      icon={<MdOutlineModeEdit />}
+                      mb={2}
+                    />
+                    <IconButton
+                      ml={2}
+                      mr={2}
+                      onClick={() => onRemoveDelegate(d.sender)}
+                      borderRadius={"50%"}
+                      aria-label="Delete Baker"
+                      icon={<CiCircleRemove />}
+                      mb={2}
+                    />
+                  </Flex>
+                </Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+const DelegateButton = () => {
+  const { modalElement, onOpen } = useSendFormModal();
+
+  return (
+    <>
+      <IconAndTextBtn
+        onClick={() =>
+          onOpen({
+            mode: { type: "delegation" },
+          })
+        }
+        color="umami.green"
+        icon={VscWand}
+        label="Delegate"
+      />
+
+      {modalElement}
+    </>
+  );
+};
+
+export const FilterController: React.FC = () => {
+  return (
+    <Flex alignItems={"center"} mb={4} mt={4}>
+      <IconAndTextBtn icon={TbFilter} label="Filter" flex={1} />
+      <DelegateButton />
+    </Flex>
+  );
+};
+
+const { updateBakers } = assetsSlice.actions;
+
+const DelegationsView = () => {
+  const delegations = useAllDelegations();
+
+  const dispatch = useAppDispatch();
+  useQuery("bakers", async () => {
+    const bakers = await getBakers();
+    dispatch(updateBakers(bakers));
+  });
+  const allDelegations = Object.values(delegations).flat();
+  const formatedDelegations = filterNulls(allDelegations.map(makeDelegation));
+  const { modalElement, onOpen } = useSendFormModal();
+
+  const handleRemoveDelegate = (pkh: string) => {
+    onOpen({
+      sender: pkh,
+      mode: {
+        type: "delegation",
+        data: { undelegate: true },
+      },
+    });
+  };
+
+  const handleEditDelegate = (pkh: string, baker: string) => {
+    onOpen({
+      sender: pkh,
+      recipient: baker,
+      mode: {
+        type: "delegation",
+      },
+    });
+  };
+
+  return (
+    <Flex direction="column" height={"100%"}>
+      <TopBar title="Delegations" />
+      <FilterController />
+      <Box overflow={"scroll"}>
+        <Box maxH={40} overflow="scroll"></Box>
+        <DelegationsTable
+          onChangeDelegate={handleEditDelegate}
+          onRemoveDelegate={handleRemoveDelegate}
+          delegations={formatedDelegations}
+        />
+      </Box>
+      {modalElement}
+    </Flex>
+  );
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default DelegationsView;
