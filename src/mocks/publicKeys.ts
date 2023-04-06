36d6c6d0f33c186b4efe881fa168141b83225274
Lev Kowalski <levkowalski@gmail.com>
2023-04-06 14:53:41 +0200

    add missing fields to pks

--- src/mocks/publicKeys.ts
+++ src/mocks/publicKeys.ts
@@ -4,10 +4,10 @@ export const publicKeys1 = {
 };
 
 export const publicKeys2 = {
-  pk: "",
+  pk: "edpkuDBhPULoNAoQbjDUo6pYdpY5o3DugXo1GAJVQGzGMGFyKUVcKN",
   pkh: "tz1Te4MXuNYxyyuPqmAQdnKwkD8ZgSF9M7d6",
 };
 export const publicKeys3 = {
-  pk: "edpkuwYWCugiYG7nMnVUdopFmyc3sbMSiLqsJHTQgGtVhtSdLSw6HG",
+  pk: "edpktzYEtcJypEEhzZva7QPc8QcvBuKAsXSmTpR1wFPna3xWB48QDy",
   pkh: "tz1g7Vk9dxDALJUp4w1UTnC41ssvRa7Q4XyS",
 };
