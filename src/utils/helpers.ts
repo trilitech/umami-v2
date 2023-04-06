2a05ecc9d57070775e6652de17a1cfeb7421e577
Lev Kowalski <levkowalski@gmail.com>
2023-04-06 18:08:55 +0200

    Add helper functions

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -5,3 +5,13 @@ export function filterNulls<T>(arr: Array<T | null | undefined>): T[] {
 export function objectMap<T, R>(obj: Record<string, T>, fn: (v: T) => R) {
   return Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, fn(v)]));
 }
+
+export function zip<A, B>(a: A[], b: B[]) {
+  return a.map((k, i) => [k, b[i]] as [A, B]);
+}
+
+export function recordToMap<A extends string | number | symbol, B>(
+  record: Record<A, B>
+) {
+  return new Map(Object.entries(record) as [A, B][]);
+}
