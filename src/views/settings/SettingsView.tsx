40399643d639eeb30244cc3b4ea196dcf468c9e5
Leo Unoki <leo.unoki@trili.tech>
2023-04-06 09:24:10 +0100

    Remove logout button

--- src/views/settings/SettingsView.tsx
+++ src/views/settings/SettingsView.tsx
@@ -1,18 +1,15 @@
 import { TezosNetwork } from "@airgap/tezos";
-import { Button } from "@chakra-ui/button";
 import { Grid, GridItem } from "@chakra-ui/layout";
 import { Box, Flex, Heading, Text, Switch } from "@chakra-ui/react";
 import { NetworkSelectorDisplay } from "../../components/NetworkSelector/NetworkSelectorDisplay";
 import { SettingsCard } from "../../components/SettingsCard";
 import { TopBar } from "../../components/TopBar";
-import { useReset } from "../../utils/hooks/accountHooks";
 import { useSelectedNetwork } from "../../utils/hooks/assetsHooks";
 import assetsSlice from "../../utils/store/assetsSlice";
 import { useAppDispatch } from "../../utils/store/hooks";
 import ErrorLogsDrawerCard from "./ErrorLogsDrawerCard";
 
 export default function SettingsView() {
-  const reset = useReset();
   const network = useSelectedNetwork();
   const dispatch = useAppDispatch();
 
@@ -30,11 +27,9 @@ export default function SettingsView() {
       gridTemplateRows={"0fr 1fr 1fr"}
       gridTemplateColumns={"1fr 1fr"}
       gap="1"
-      ml={5}
     >
       <GridItem area={"header"}>
         <TopBar title="Settings" />
-        <Button onClick={reset}>logout</Button>
       </GridItem>
       <GridItem area={"main"}>
         <SettingsSection title="General">
