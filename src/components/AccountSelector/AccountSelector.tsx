5c419fa45fb0e4733937991a02bdd9c3ea087d74
Lev Kowalski <levkowalski@gmail.com>
2023-04-13 16:56:15 +0200

    Remove dead code from Account Selector

--- src/components/AccountSelector/AccountSelector.tsx
+++ src/components/AccountSelector/AccountSelector.tsx
@@ -9,48 +9,6 @@ const renderAccount = (account: Account) => (
   <AccountSmallTile pkh={account.pkh} label={account.label} />
 );
 
-export const AccountSelector: React.FC<{
-  accounts: Account[];
-  onSelect: (a: Account) => void;
-  selected?: string;
-  isDisabled?: boolean;
-}> = ({ accounts, onSelect = () => {}, selected, isDisabled }) => {
-  const selectedAccount = accounts.find((a) => a.pkh === selected);
-
-  return (
-    <Menu>
-      <MenuButton
-        isDisabled={isDisabled}
-        data-testid="account-selector"
-        w={"100%"}
-        textAlign="left"
-        as={Button}
-        rightIcon={<ChevronDownIcon />}
-        h={16}
-      >
-        {selectedAccount ? renderAccount(selectedAccount) : "Select an account"}
-      </MenuButton>
-      <MenuList bg={"umami.gray.900"}>
-        {accounts.map((account) => (
-          // TODO refactor with AccountTile in home
-          <MenuItem
-            onClick={(e) => {
-              onSelect(account);
-            }}
-            key={account.pkh}
-            minH="48px"
-            w="100%"
-            // TODO implement hover color that disapeared
-            bg={"umami.gray.900"}
-          >
-            {renderAccount(account)}
-          </MenuItem>
-        ))}
-      </MenuList>
-    </Menu>
-  );
-};
-
 export const ConnectedAccountSelector: React.FC<{
   onSelect: (a: Account) => void;
   selected?: string;
@@ -74,7 +32,6 @@ export const ConnectedAccountSelector: React.FC<{
       </MenuButton>
       <MenuList bg={"umami.gray.900"}>
         {accounts.map((account) => (
-          // TODO refactor with AccountTile in home
           <MenuItem
             value={account.pkh}
             aria-label={account.label}
