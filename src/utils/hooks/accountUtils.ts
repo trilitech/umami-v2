cb8f12435ce736d6b8aa487fbadf713137bbb904
Lev Kowalski <levkowalski@gmail.com>
2023-04-19 21:41:51 +0200

    Implement hook that deducts sk for a Mnemonic account

--- src/utils/hooks/accountUtils.ts
+++ src/utils/hooks/accountUtils.ts
@@ -1,4 +1,8 @@
 import BigNumber from "bignumber.js";
+import { MnemonicAccount } from "../../types/Account";
+import { decrypt } from "../aes";
+import { deriveSkFromMnemonic } from "../restoreAccounts";
+import { useAppSelector } from "../store/hooks";
 
 export const getTotalBalance = (balances: Record<string, BigNumber | null>) => {
   const totalTez = Object.values(balances).reduce((acc, curr) => {
@@ -10,4 +14,17 @@ export const getTotalBalance = (balances: Record<string, BigNumber | null>) => {
   }, null);
 
   return totalTez;
+};
+
+export const useGetSk = () => {
+  const seedPhrases = useAppSelector((s) => s.accounts.seedPhrases);
+  return async (a: MnemonicAccount, password: string) => {
+    const encryptedMnemonic = seedPhrases[a.seedFingerPrint];
+    if (!encryptedMnemonic) {
+      throw new Error(`Missing seedphrase for account ${a.pkh}`);
+    }
+
+    const mnemonic = await decrypt(encryptedMnemonic, password);
+    return deriveSkFromMnemonic(mnemonic, a.derivationPath, a.curve);
+  };
 };
