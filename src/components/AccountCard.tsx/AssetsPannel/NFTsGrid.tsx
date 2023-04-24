6e73dbd2481a56467cef86e9321d0b07962d3814
Leo Unoki <leo.unoki@trili.tech>
2023-04-24 18:28:36 +0100

    Display nfts in account card

---
+++ src/components/AccountCard.tsx/AssetsPannel/NFTsGrid.tsx
@@ -0,0 +1,37 @@
+import {
+  AspectRatio,
+  Box,
+  Image,
+  Heading,
+  SimpleGrid,
+  SimpleGridProps,
+} from "@chakra-ui/react";
+import { FC } from "react";
+import { NFT } from "../../../types/Asset";
+import { truncate } from "../../../utils/format";
+
+export const NFTsGrid: FC<{ nfts: NFT[] } & SimpleGridProps> = ({
+  nfts,
+  ...rest
+}) => {
+  return (
+    <Box overflow={"scroll"} p={2}>
+      <SimpleGrid {...rest}>
+        {nfts.map((nft, i) => {
+          return (
+            <Box key={i}>
+              <AspectRatio key={nft.contract + i} width={"100%"} ratio={1}>
+                <Image width="100%" height={40} src={nft.metadata.displayUri} />
+              </AspectRatio>
+              {nft.metadata.name && (
+                <Heading size="sm" mt={3}>
+                  {truncate(nft.metadata.name, 12)}
+                </Heading>
+              )}
+            </Box>
+          );
+        })}
+      </SimpleGrid>
+    </Box>
+  );
+};
