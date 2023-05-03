e06df20d6db372a9def8728deb73cb7c8d0de8ff
Lev Kowalski <levkowalski@gmail.com>
2023-05-03 10:55:37 +0200

    wip

--- src/utils/beacon/PermissionRequestDisplay.tsx
+++ src/utils/beacon/PermissionRequestDisplay.tsx
@@ -1,4 +1,11 @@
-import { PermissionRequestOutput } from "@airgap/beacon-wallet";
+import {
+  BeaconMessageType,
+  BeaconResponseInputMessage,
+  Network,
+  NetworkType,
+  PermissionRequestOutput,
+} from "@airgap/beacon-wallet";
+import { TezosNetwork } from "@airgap/tezos";
 import {
   ModalContent,
   ModalCloseButton,
@@ -7,12 +14,57 @@ import {
   ModalFooter,
   AspectRatio,
   Image,
+  Button,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
+import { ConnectedAccountSelector } from "../../components/AccountSelector/AccountSelector";
+import { Account } from "../../types/Account";
+import { useSelectedNetwork } from "../hooks/assetsHooks";
+import { walletClient } from "./Beacon";
+
+const tezosNetworkToBeaconNetwork = (n: TezosNetwork) => {
+  if (n === TezosNetwork.MAINNET) {
+    return NetworkType.MAINNET;
+  } else if (n === TezosNetwork.GHOSTNET) {
+    return NetworkType.GHOSTNET;
+  }
+
+  const foo: never = n;
+  throw new Error(foo);
+};
 
 const PermissionRequest: React.FC<{
   request: PermissionRequestOutput;
 }> = ({ request }) => {
+  const [account, setAccount] = useState<Account>();
+  const network = useSelectedNetwork();
+  // const response: BeaconResponseInputMessage = {
+  //   type: BeaconMessageType.PermissionResponse,
+  //   network: message.network, // Use the same network that the user requested
+  //   scopes: message.scopes,
+  //   id: message.id,
+  //   publicKey: publicKey,
+  // };
+
+  // // Send response back to DApp
+  // walletClient.respond(response);
+
+  const grant = () => {
+    if (!account) {
+      throw new Error("foo");
+    }
+
+    const response: BeaconResponseInputMessage = {
+      type: BeaconMessageType.PermissionResponse,
+      network: { type: tezosNetworkToBeaconNetwork(network) }, // Use the same network that the user requested
+      scopes: request.scopes,
+      id: request.id,
+      publicKey: account.pk,
+    };
+
+    walletClient.respond(response);
+  };
+
   return (
     <ModalContent>
       <ModalHeader>
@@ -21,12 +73,22 @@ const PermissionRequest: React.FC<{
 
       <ModalCloseButton />
       <ModalBody>
+        <ConnectedAccountSelector
+          selected={account && account.pkh}
+          onSelect={(a) => {
+            setAccount(a);
+          }}
+        />
         <AspectRatio width={"100%"} ratio={1}>
           <Image width="100%" height={40} src={request.appMetadata.icon} />
         </AspectRatio>
       </ModalBody>
 
-      <ModalFooter>hello</ModalFooter>
+      <ModalFooter>
+        <Button isDisabled={!account} onClick={(_) => grant()}>
+          Grant
+        </Button>
+      </ModalFooter>
     </ModalContent>
   );
 };
