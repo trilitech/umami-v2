03043fefbd36d49a5361cf35d81b9d468f1bd173
Lev Kowalski <levkowalski@gmail.com>
2023-05-03 11:16:47 +0200

    wip

---
+++ src/utils/beacon/SignPayloadRequestPannel.tsx
@@ -0,0 +1,32 @@
+import { SignPayloadRequestOutput } from "@airgap/beacon-wallet";
+import {
+  Button,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "@chakra-ui/react";
+import React from "react";
+
+const SignPayloadRequestPannel: React.FC<{
+  request: SignPayloadRequestOutput;
+}> = ({ request }) => {
+  console.log(request);
+  return (
+    <ModalContent>
+      <ModalHeader>
+        Sign payload Request from {request.appMetadata.name}
+      </ModalHeader>
+
+      <ModalCloseButton />
+      <ModalBody>{request.payload}</ModalBody>
+
+      <ModalFooter>
+        <Button>Sign</Button>
+      </ModalFooter>
+    </ModalContent>
+  );
+};
+
+export default SignPayloadRequestPannel;
