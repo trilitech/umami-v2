1dc4dbe3a1646f675f5e572ea38e287cfa36b39c
Sergey Kintsel <sergey.kintsel@trili.tech>
2023-05-10 09:35:07 +0300

    Replace AppImage linux target with deb, rpm, and snap

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,12 +1,11 @@
 const { notarize } = require("electron-notarize");
 
 exports.default = async function notarizing(context) {
-  console.log("Notarizing");
   const { electronPlatformName, appOutDir } = context;
   if (electronPlatformName !== "darwin") {
     return;
   }
-
+  console.log("Notarizing");
   const appName = context.packager.appInfo.productFilename;
 
   return await notarize({
