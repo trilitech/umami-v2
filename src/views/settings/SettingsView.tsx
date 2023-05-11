da17a8e78ddc35d6836253c0b94bdb49f1c0e55c
Leo Unoki <leo.unoki@trili.tech>
2023-05-11 12:37:52 +0100

    Refactor, rename

--- src/views/settings/SettingsView.tsx
+++ src/views/settings/SettingsView.tsx
@@ -1,12 +1,24 @@
 import { Grid, GridItem } from "@chakra-ui/layout";
-import { Box, Flex, Heading, Text, Switch, Button } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Switch,
+  Button,
+  Divider,
+  Input,
+} from "@chakra-ui/react";
 import { addPeer } from "../../utils/beacon/beacon";
-import { SettingsCard } from "../../components/ClickableCard";
+import ClickableCard, { SettingsCard } from "../../components/ClickableCard";
 import NetworkSelector from "../../components/NetworkSelector";
 import { TopBar } from "../../components/TopBar";
 import { useReset } from "../../utils/hooks/accountHooks";
 import BeaconPeers from "../../utils/beacon/BeaconPeers";
 import ErrorLogsDrawerCard from "./ErrorLogsDrawerCard";
+import { IconAndTextBtn } from "../../components/IconAndTextBtn";
+import { TfiReload } from "react-icons/tfi";
+import { BsFolder2Open } from "react-icons/bs";
 
 export default function SettingsView() {
   const reset = useReset();
@@ -26,42 +38,49 @@ export default function SettingsView() {
         <TopBar title="Settings" />
       </GridItem>
       <GridItem area={"main"}>
-        <SettingsSection title="General">
-          <SettingsCard left="Theme">
-            <Flex alignItems="center" justifyContent="space-between">
-              <Text size="sm">Light</Text>
-              <Switch marginX={3} isChecked isDisabled />
-              <Heading size="sm">Dark</Heading>
-            </Flex>
-          </SettingsCard>
-          <SettingsCard left="Network">
-            <Box>
-              <NetworkSelector />
-            </Box>
-          </SettingsCard>
-          <SettingsCard left="Paste Beacon Pairing Code">
-            <Box>
-              <Button
-                onClick={async () => {
-                  navigator.clipboard.readText().then((text) => {
-                    addPeer(text);
-                  });
-                }}
-              >
-                Paste
-              </Button>
-            </Box>
-          </SettingsCard>
-          <BeaconPeers />
-          <ErrorLogsDrawerCard />
-          <Button onClick={reset}>{"Erase secrets"}</Button>
-        </SettingsSection>
+        <Button onClick={reset}>{"Erase secrets"}</Button>
+        <GeneralSection />
       </GridItem>
     </Grid>
   );
 }
 
-const SettingsSection: React.FC<{
+const GeneralSection = () => {
+  return (
+    <SectionContainer title="General">
+      <SettingsCard left="Theme">
+        <Flex alignItems="center" justifyContent="space-between">
+          <Text size="sm">Light</Text>
+          <Switch marginX={3} isChecked isDisabled />
+          <Heading size="sm">Dark</Heading>
+        </Flex>
+      </SettingsCard>
+      <SettingsCard left="Network">
+        <Box>
+          <NetworkSelector />
+        </Box>
+      </SettingsCard>
+
+      <SettingsCard left="Paste Beacon Pairing Code">
+        <Box>
+          <Button
+            onClick={async () => {
+              navigator.clipboard.readText().then((text) => {
+                addPeer(text);
+              });
+            }}
+          >
+            Paste
+          </Button>
+        </Box>
+      </SettingsCard>
+      <BeaconPeers />
+      <ErrorLogsDrawerCard />
+    </SectionContainer>
+  );
+};
+
+const SectionContainer: React.FC<{
   title: string;
   children: React.ReactNode;
 }> = ({ title, children }) => {
