cf765fd9906cfff11dfdda969014348540e96520
Lukas Schönbächler <l.schoenbaechler@papers.ch>
2023-05-14 23:24:41 +0200

    wip

--- src/utils/store/thunks/restoreAccountsFromSocial.ts
+++ src/utils/store/thunks/restoreAccountsFromSocial.ts
@@ -1,23 +1,21 @@
 import { AnyAction, ThunkAction } from "@reduxjs/toolkit";
-import { AccountType, LedgerAccount } from "../../../types/Account";
+import { AccountType, SocialAccount } from "../../../types/Account";
 import accountsSlice from "../accountsSlice";
 import { RootState } from "../store";
 
 const { add } = accountsSlice.actions;
 
-export const restoreAccountsFromLedger = (
-  derivationPath: string,
+export const restoreAccountsFromSocial = (
   pk: string,
   pkh: string,
   label?: string
 ): ThunkAction<Promise<void>, RootState, unknown, AnyAction> => {
   return async (dispatch) => {
-    const account: LedgerAccount = {
-      derivationPath,
-      curve: "ed25519",
-      type: AccountType.LEDGER,
+    const account: SocialAccount = {
+      type: AccountType.SOCIAL,
       pk: pk,
       pkh: pkh,
+      idp: "google",
       label,
     };
     dispatch(add([account]));
