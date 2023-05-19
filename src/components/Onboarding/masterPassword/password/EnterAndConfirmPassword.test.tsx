e7bc0f9fd87cabb03d736b6c003becac997d8320
Lukas Schönbächler <l.schoenbaechler@papers.ch>
2023-05-19 16:13:01 +0200

    feat(test): add more tests

---
+++ src/components/Onboarding/masterPassword/password/EnterAndConfirmPassword.test.tsx
@@ -0,0 +1,55 @@
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import { EnterAndComfirmPassword } from "./EnterAndConfirmPassword";
+
+const fixture = (onSubmit: (password: string) => void, isLoading: boolean) => (
+  <EnterAndComfirmPassword onSubmit={onSubmit} isLoading={isLoading} />
+);
+
+const checkPasswords = async (
+  password: string,
+  confirmation: string,
+  expectedResult: boolean
+) => {
+  const submit = screen.getByRole("button", { name: /submit/i });
+  await waitFor(() => {
+    expect(submit).toBeDisabled();
+  });
+  const passwordField = screen.getByTestId("password");
+  const confirmationField = screen.getByTestId("confirmation");
+  fireEvent.change(passwordField, { target: { value: password } });
+  fireEvent.change(confirmationField, { target: { value: confirmation } });
+  await waitFor(() => {
+    if (expectedResult) {
+      expect(submit).toBeEnabled();
+    } else {
+      expect(submit).toBeDisabled();
+    }
+  });
+};
+
+describe("<EnterAndConfirmPassword />", () => {
+  describe("Form", () => {
+    test("Working verification", async () => {
+      render(fixture(() => {}, false));
+      checkPasswords("test", "test", true);
+    });
+
+    test("Not matching password", async () => {
+      render(fixture(() => {}, false));
+      checkPasswords("test", "test1", false);
+    });
+
+    test("Not meeting password policy", async () => {
+      render(fixture(() => {}, false));
+      checkPasswords("tes", "tes", false);
+    });
+
+    test("Form is loading", async () => {
+      render(fixture(() => {}, true));
+      const submit = screen.getByRole("button", { name: /submit/i });
+      await waitFor(() => {
+        expect(submit).toBeDisabled();
+      });
+    });
+  });
+});
