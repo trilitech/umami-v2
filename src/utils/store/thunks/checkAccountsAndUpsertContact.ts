0e6d4474b2931b92aef7454b19597e00531baf71
Leo Unoki <leo.unoki@trili.tech>
2023-05-19 11:09:01 +0100

    Add thunk

---
+++ src/utils/store/thunks/checkAccountsAndUpsertContact.ts
@@ -0,0 +1,25 @@
+import { Contact } from "../../../types/Contact";
+import { AnyAction, ThunkAction } from "@reduxjs/toolkit";
+import { contactsActions } from "../contactsSlice";
+import { RootState } from "../store";
+
+const { upsert } = contactsActions;
+
+const checkAccountsAndUpsertContact = (
+  contact: Contact
+): ThunkAction<void, RootState, unknown, AnyAction> => {
+  return (dispatch, getState) => {
+    const { accounts } = getState();
+    const existingAccount = accounts.items.find((account) => {
+      return account.pkh === contact.pkh || account.label === contact.name;
+    });
+
+    if (existingAccount) {
+      return;
+    }
+
+    dispatch(upsert(contact));
+  };
+};
+
+export default checkAccountsAndUpsertContact;
