5d787f2d95a507d418168602af8893f2d68f1219
Lev Kowalski <levkowalski@gmail.com>
2023-05-22 18:27:43 +0200

    generic confirmation modal component

---
+++ src/utils/hooks/confirmModal.tsx
@@ -0,0 +1,45 @@
+import {
+  useDisclosure,
+  Modal,
+  ModalContent,
+  ModalCloseButton,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+} from "@chakra-ui/react";
+import { useRef } from "react";
+
+export const useConfirmation = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const paramsRef = useRef({ onConfirm: () => {}, body: "" });
+
+  return {
+    element: (
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalContent bg="umami.gray.900">
+          <ModalCloseButton />
+          <ModalHeader textAlign={"center"}>Confirmation</ModalHeader>
+          <ModalBody>{paramsRef.current.body}</ModalBody>
+          <ModalFooter>
+            <Button
+              onClick={() => paramsRef.current.onConfirm()}
+              bg="umami.blue"
+            >
+              Confirm
+            </Button>
+            <Button onClick={onClose} ml={2}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    ),
+    onClose,
+    onOpen: (params: { onConfirm: () => void; body: string }) => {
+      paramsRef.current = params;
+      onOpen();
+    },
+  };
+};
