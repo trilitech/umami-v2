6f5500ad6d9fc0293099e3c1def1c0d227f34230
Lev Kowalski <levkowalski@gmail.com>
2023-05-24 18:01:10 +0200

    implement account derivation

---
+++ src/views/home/DeriveAccountDisplay.tsx.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import EnterPassword from "../../components/Onboarding/masterPassword/password/EnterPassword";
+import NameAccountDisplay from "../../components/Onboarding/nameAccount/NameAccountDisplay";
+
+const DeriveAccountDisplay = (props: {
+  onSubmit: (args: { name: string; password: string }) => void;
+  isLoading: boolean;
+  title?: string;
+  subtitle?: string;
+}) => {
+  const [name, setName] = useState<string>();
+
+  if (name === undefined) {
+    return (
+      <NameAccountDisplay
+        title={props.title}
+        subtitle={props.subtitle}
+        onSubmit={(p) => setName(p.accountName)}
+      />
+    );
+  }
+
+  return (
+    <EnterPassword
+      onSubmit={(p) => props.onSubmit({ name, password: p })}
+      isLoading={props.isLoading}
+    />
+  );
+};
+
+export default DeriveAccountDisplay;
