590ad5138ed40c3099a5dc6d444f9b8423f8adb4
Lev Kowalski <levkowalski@gmail.com>
2023-05-30 14:25:34 +0200

    remove unlisted hook dependecy warning

--- src/utils/useDeeplinkHandler.ts
+++ src/utils/useDeeplinkHandler.ts
@@ -1,38 +1,50 @@
 import { useToast } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useAddPeer } from "./beacon/beacon";
 import { parseParams } from "../GoogleAuth";
 
 export const useDeeplinkHandler = () => {
   const toast = useToast();
   const addPeer = useAddPeer();
+
+  const addPeerRef = useRef(addPeer);
+  const toastRef = useRef(toast);
+
+  const handleDeepLink = (_: any, url: string) => {
+    // Print for debugging
+    console.log("onDeeplink", url);
+    if (url.startsWith("umami://auth/")) {
+      // Deeplink handler for Kukai
+      const params = parseParams(url);
+      window.postMessage(params);
+    } else if (url.startsWith("umami://?type=tzip10&data=")) {
+      // Deeplink handler for beacon
+      const params: URLSearchParams = new URL(url).searchParams;
+      const payload = params.get("data");
+      if (payload) {
+        addPeerRef.current(payload);
+      } else {
+        toastRef.current({
+          title: "Linkhandler",
+          description: "Data Payload missing",
+        });
+      }
+    } else {
+      toastRef.current({
+        title: "Linkhandler",
+        description: "Unsupported link type",
+      });
+    }
+  };
+
   useEffect(() => {
     const internalWindows = window as any;
     if (internalWindows && internalWindows.electronAPI) {
-      internalWindows.electronAPI.onDeeplink((_: any, url: string) => {
-        // Print for debugging
-        console.log("onDeeplink", url);
-        if (url.startsWith("umami://auth/")) {
-          // Deeplink handler for Kukai
-          const params = parseParams(url);
-          window.postMessage(params);
-        } else if (url.startsWith("umami://?type=tzip10&data=")) {
-          // Deeplink handler for beacon
-          const params: URLSearchParams = new URL(url).searchParams;
-          const payload = params.get("data");
-          if (payload) {
-            addPeer(payload);
-          } else {
-            toast({
-              title: "Linkhandler",
-              description: "Data Payload missing",
-            });
-          }
-        } else {
-          toast({ title: "Linkhandler", description: "Unsupported link type" });
-        }
-      });
+      internalWindows.electronAPI.onDeeplink(handleDeepLink);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); // Only a single time on mount
+
+    return () => {
+      // TODO cleanup deeplink listner
+    };
+  }, []);
 };
