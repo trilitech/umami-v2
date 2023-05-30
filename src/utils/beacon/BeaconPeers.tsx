62d30ba13e44e76ef9f2869985e2fb1be5623d15
Lev Kowalski <levkowalski@gmail.com>
2023-05-30 17:10:31 +0200

    Implement beacon peers drawer

--- src/utils/beacon/BeaconPeers.tsx
+++ src/utils/beacon/BeaconPeers.tsx
@@ -2,51 +2,93 @@ import { PeerInfo } from "@airgap/beacon-wallet";
 import {
   AspectRatio,
   Flex,
-  Heading,
   IconButton,
   Image,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
 } from "@chakra-ui/react";
 import { BsTrash } from "react-icons/bs";
-import { usePeers, useRefreshPeers, walletClient } from "./beacon";
+import { usePeers, useRemovePeer } from "./beacon";
 
 // Displays the list of beacon peers
 
-export const PeerDisplay = ({ peerInfo }: { peerInfo: PeerInfo }) => {
-  const refreshPeers = useRefreshPeers();
-  const icon = (peerInfo as any).icon;
+const peerRow = (peerInfo: PeerInfo, onRemove: () => void) => {
+  console.log(peerInfo);
   return (
-    <Flex key={peerInfo.name} mt={2} mb={2}>
-      <Flex flex={1} alignItems="center">
-        <AspectRatio width={12} ratio={4 / 4}>
-          <Image width="100%" src={icon} />
-        </AspectRatio>
-        <Heading size={"sm"} ml={4}>
-          {peerInfo.name}
-        </Heading>
-      </Flex>
+    <Tr>
+      <Td>
+        <Flex alignItems={"center"}>
+          <AspectRatio width={4} ratio={1}>
+            <Image width="100%" src={(peerInfo as any).icon} />
+          </AspectRatio>
+          <Text size={"sm"} ml={2}>
+            {peerInfo.name}
+          </Text>
+        </Flex>
+      </Td>
+      <Td>
+        <Text size={"sm"} ml={2}>
+          {(peerInfo as any).relayServer}
+        </Text>
+      </Td>
+      <Td>
+        <IconButton
+          onClick={onRemove}
+          aria-label="Remove Peer"
+          icon={<BsTrash />}
+        />
+      </Td>
+    </Tr>
+  );
+};
 
-      <IconButton
-        onClick={() => {
-          walletClient.removePeer(peerInfo as any).then(refreshPeers); // Bug in types
-        }}
-        aria-label="Delete Peer"
-        icon={<BsTrash />}
-      />
-    </Flex>
+export const PeersDisplay = ({
+  peerInfos,
+  removePeer,
+}: {
+  peerInfos: PeerInfo[];
+  removePeer: (peer: PeerInfo) => void;
+}) => {
+  return (
+    <TableContainer overflowX="unset" overflowY="unset">
+      <Table>
+        <Thead top={0} bg="umami.gray.900" borderRadius={4}>
+          <Tr>
+            <Th>Name:</Th>
+            <Th>Relay server:</Th>
+            <Th>Delete:</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {peerInfos.map((peerInfo) =>
+            peerRow(peerInfo, () => removePeer(peerInfo))
+          )}
+        </Tbody>
+      </Table>
+    </TableContainer>
   );
 };
 
 const BeaconPeers = () => {
   const { data } = usePeers();
+  const removePeer = useRemovePeer();
   const peers = data || [];
-  return (
-    <>
-      <Heading>Peers</Heading>
-      {peers.map((peerInfo) => (
-        <PeerDisplay peerInfo={peerInfo} />
-      ))}
-    </>
-  );
+
+  if (peers.length === 0) {
+    return (
+      <Text mt={4} color={"text.dark"}>
+        Paste a peer request or open a deeplink from inside a dApp...
+      </Text>
+    );
+  }
+
+  return <PeersDisplay peerInfos={peers} removePeer={removePeer} />;
 };
 
 export default BeaconPeers;
