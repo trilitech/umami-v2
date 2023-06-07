97fc81d72b44ea6e096c70792c8266ad5e34c821
Leo Unoki <leo.unoki@trili.tech>
2023-06-07 16:47:20 +0100

    Add approvers to card

---
+++ src/components/AccountCard.tsx/MultisigSigners.tsx
@@ -0,0 +1,59 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Icon,
+  Tag,
+  Text,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import colors from "../../style/colors";
+import { WalletAccountPkh } from "../../utils/multisig/types";
+import AccountOrContactTile from "../AccountOrContactTile";
+
+const MultisigSigners: React.FC<{
+  signers: WalletAccountPkh[];
+}> = ({ signers }) => {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <Box w="100%" bg={colors.gray[600]} p={3} borderRadius={6} m={5}>
+      <Flex justifyContent="space-between">
+        <Heading size="sm">Approvers</Heading>
+        <Icon
+          as={isOpen ? IoIosArrowUp : IoIosArrowDown}
+          cursor="pointer"
+          _hover={{
+            color: colors.gray[600],
+          }}
+          onClick={() => setIsOpen((prev) => !prev)}
+          data-testid="multisig-toggle-button"
+        />
+      </Flex>
+      {isOpen && (
+        <Wrap mt="3" data-testid="multisig-tag-section">
+          {signers.map((pkh) => {
+            return (
+              <WrapItem
+                key={pkh}
+                borderRadius="100px"
+                padding="3px 8px"
+                bg={colors.gray[600]}
+              >
+                <Text data-testid="multisig-tag" color={colors.gray[400]}>
+                  <Tag borderRadius="full">
+                    <AccountOrContactTile pkh={pkh} />
+                  </Tag>
+                </Text>
+              </WrapItem>
+            );
+          })}
+        </Wrap>
+      )}
+    </Box>
+  );
+};
+
+export default MultisigSigners;
