730177a24d17acd102009344763d86479860e5e3
Lev Kowalski <levkowalski@gmail.com>
2023-06-22 11:23:08 +0200

    make asset pannel full width

--- src/components/AccountCard/AssetsPannel/AssetsPannel.tsx
+++ src/components/AccountCard/AssetsPannel/AssetsPannel.tsx
@@ -24,8 +24,8 @@ export const AssetsPannel: React.FC<{
       mt={4}
       bg="umami.gray.900"
       borderRadius={4}
-      w="500px"
       data-testid="asset-panel"
+      w="100%"
       // color scheme not workkig even when put int 50-900 range
       // TODO Fix
       // https://chakra-ui.com/docs/components/tabs
