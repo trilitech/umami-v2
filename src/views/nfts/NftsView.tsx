bc769efdc10eebbb79c766f34f96c7ede92717ca
Lev Kowalski <levkowalski@gmail.com>
2023-06-22 14:25:47 +0200

    rename

--- src/views/nfts/NftsView.tsx
+++ src/views/nfts/NftsView.tsx
@@ -13,7 +13,7 @@ import { BsArrowDownUp } from "react-icons/bs";
 import { TbFilter } from "react-icons/tb";
 import { useParams } from "react-router-dom";
 import { IconAndTextBtn } from "../../components/IconAndTextBtn";
-import { NoNFTS } from "../../components/NoItems/NoItems";
+import { NoNFTs } from "../../components/NoItems/NoItems";
 import { TopBar } from "../../components/TopBar";
 import { NFT } from "../../types/Asset";
 import { useAllNfts } from "../../utils/hooks/assetsHooks";
@@ -78,7 +78,7 @@ const NFTsViewBase = () => {
           </Drawer>
         </>
       ) : (
-        <NoNFTS />
+        <NoNFTs />
       )}
     </Flex>
   );
