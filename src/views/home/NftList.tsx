a17d1d6f625dd0015bc6e7e701a88a612909f8cf
Lev Kowalski <levkowalski@gmail.com>
2023-06-22 14:23:18 +0200

    Use early returns

--- src/views/home/NftList.tsx
+++ src/views/home/NftList.tsx
@@ -1,12 +1,11 @@
 import { Tab, TabList, Tabs } from "@chakra-ui/react";
 import { NFTsGrid } from "../../components/AccountCard/AssetsPannel/NFTsGrid";
 import NestedScroll from "../../components/NestedScroll";
-import { NoNFTS } from "../../components/NoItems/NoItems";
 import { useAllNfts } from "../../utils/hooks/assetsHooks";
 export const NftList = () => {
   const nfts = useAllNfts();
 
-  const allOwnedNfts = Object.values(nfts).flat();
+  const allOwnedNFTs = Object.values(nfts).flat();
 
   return (
     <Tabs height="100%" display="flex" flexDirection="column" bg="umami.gray.900" borderRadius={4}>
@@ -14,13 +13,9 @@ export const NftList = () => {
         <Tab>All NFTs</Tab>
       </TabList>
 
-      {allOwnedNfts.length === 0 ? (
-        <NoNFTS small />
-      ) : (
-        <NestedScroll>
-          <NFTsGrid nfts={allOwnedNfts} columns={4} spacing={4} />
-        </NestedScroll>
-      )}
+      <NestedScroll>
+        <NFTsGrid nfts={allOwnedNFTs} columns={4} spacing={4} />
+      </NestedScroll>
     </Tabs>
   );
 };
