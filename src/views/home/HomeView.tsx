889e41bf5fbd806a253be30ebb43f0fecee7ab71
Lev Kowalski <levkowalski@gmail.com>
2023-06-23 13:20:28 +0200

    Fix AccountList scroll limit and refactor HomeView css

--- src/views/home/HomeView.tsx
+++ src/views/home/HomeView.tsx
@@ -1,23 +1,33 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, FlexProps } from "@chakra-ui/react";
+import { ReactNode } from "react";
 import { TopBar } from "../../components/TopBar";
 import AccountListWithDrawer from "./AccountListWithDrawer";
+import { AccountListHeader } from "./AccountsList";
 
 import { NftList } from "./NftList";
 import { OperationsListPanel } from "./OperationsListPanel";
 
+// Reference unit for all the margin and padding ajustements
+const BASE = 6;
+
 export default function HomeView() {
   return (
-    <Flex direction="column" height="100%">
+    <Flex direction="column" height="100%" px={BASE}>
       <TopBar title="Overview" />
       <Flex flex={1} minHeight={1}>
-        <Box flex={1} mr={2} overflow="hidden" mb={2} borderRadius={4}>
-          <AccountListWithDrawer />
-        </Box>
-        <Flex direction="column" flex={1} minHeight={1} ml={2}>
-          <Box flex={1} overflow="hidden" mb={2} borderRadius={4}>
+        {/* Left Column*/}
+        <Flex direction="column" flex={1} mr={BASE / 2} pb={BASE / 2}>
+          <AccountListHeader />
+          <Box flex={1} overflow="hidden" borderRadius={4}>
+            <AccountListWithDrawer />
+          </Box>
+        </Flex>
+        {/* Right Column*/}
+        <Flex direction="column" flex={1} ml={BASE / 2} pb={BASE / 2}>
+          <Box flex={1} overflow="hidden" borderRadius={4} mb={BASE / 2}>
             <OperationsListPanel />
           </Box>
-          <Box flex={1} overflow="hidden" mb={2} borderRadius={4}>
+          <Box flex={1} overflow="hidden" borderRadius={4}>
             <NftList />
           </Box>
         </Flex>
