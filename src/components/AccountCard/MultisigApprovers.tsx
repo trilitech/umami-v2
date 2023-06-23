fc6aa65eca5c5ba16dbb58cfe7d4b08865847d74
Leo Unoki <leo.unoki@trili.tech>
2023-06-23 17:03:07 +0100

    Refactor approvers

--- src/components/AccountCard/MultisigApprovers.tsx
+++ src/components/AccountCard/MultisigApprovers.tsx
@@ -1,6 +1,16 @@
-import { Box, Flex, Heading, Icon, Tag, useDisclosure, Wrap, WrapItem } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Heading,
+  Tag,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
 import React from "react";
-import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import colors from "../../style/colors";
 import { ImplicitAddress } from "../../types/Address";
 import AccountOrContactTile from "../AccountOrContactTile";
@@ -8,41 +18,33 @@ import AccountOrContactTile from "../AccountOrContactTile";
 const MultisigApprovers: React.FC<{
   signers: ImplicitAddress[];
 }> = ({ signers }) => {
-  const { isOpen, getButtonProps } = useDisclosure({
-    defaultIsOpen: true,
-  });
   return (
-    <Box w="100%" bg={colors.gray[600]} p={3} borderRadius={6} m={5}>
-      <Flex justifyContent="space-between">
-        <Heading size="sm">Approvers</Heading>
-        <Icon
-          as={isOpen ? IoIosArrowUp : IoIosArrowDown}
-          cursor="pointer"
-          _hover={{
-            color: colors.gray[600],
-          }}
-          {...getButtonProps()}
-          data-testid="multisig-toggle-button"
-        />
-      </Flex>
-      {isOpen && (
-        <Wrap mt="3" data-testid="multisig-tag-section">
-          {signers.map(signer => {
-            return (
-              <WrapItem
-                key={signer.pkh}
-                borderRadius="100px"
-                padding="3px 8px"
-                bg={colors.gray[600]}
-              >
-                <Tag data-testid="multisig-tag" color={colors.gray[400]} borderRadius="full">
-                  <AccountOrContactTile pkh={signer.pkh} />
-                </Tag>
-              </WrapItem>
-            );
-          })}
-        </Wrap>
-      )}
+    <Box w="100%" bg={colors.gray[800]} p={3} borderRadius={6} m={5}>
+      <Accordion allowMultiple={true}>
+        <AccordionItem bg={colors.gray[800]} border="none" borderRadius="8px">
+          <h2>
+            <AccordionButton as="span" flex="1" textAlign="left">
+              <Heading w="100%" size="sm">
+                Approvers
+              </Heading>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel>
+            <Wrap mt="3" data-testid="multisig-tag-section">
+              {signers.map(signer => {
+                return (
+                  <WrapItem key={signer.pkh} borderRadius="100px" padding="3px 8px">
+                    <Tag color={colors.gray[400]} borderRadius="full">
+                      <AccountOrContactTile pkh={signer.pkh} />
+                    </Tag>
+                  </WrapItem>
+                );
+              })}
+            </Wrap>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </Box>
   );
 };
