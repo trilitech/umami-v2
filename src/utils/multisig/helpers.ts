74f825605b30ca5f46e34fd527965536d8e16cd4
Sergey Kintsel <sergey.kintsel@trili.tech>
2023-06-25 09:34:00 +0000

    Limit the concurrency for bigmap requests

--- src/utils/multisig/helpers.ts
+++ src/utils/multisig/helpers.ts
@@ -1,5 +1,5 @@
 import { TezosNetwork } from "@airgap/tezos";
-import { compact } from "lodash";
+import { chunk, compact } from "lodash";
 import { parseContractPkh, parseImplicitPkh } from "../../types/Address";
 import { tzktGetSameMultisigsResponseType } from "../tzkt/types";
 import { getAllMultiSigContracts, getPendingOperations } from "./fetch";
@@ -18,31 +18,38 @@ export const getRelevantMultisigContracts = async (
 
 export const getOperationsForMultisigs = async (
   network: TezosNetwork,
-  multisigs: tzktGetSameMultisigsResponseType
+  multisigs: tzktGetSameMultisigsResponseType,
+  chunkSize = 5
 ): Promise<MultisigWithPendingOperations[]> => {
-  const multisigsWithOperations = await Promise.all(
-    multisigs.map(async ({ address, storage: { signers, pending_ops, threshold } }) => {
-      const response = await getPendingOperations(network, pending_ops);
+  let multisigsWithOperations: MultisigWithPendingOperations[] = [];
+  const multisigChunks = chunk(multisigs, chunkSize);
 
-      const operations = response.map(({ key, value }) => {
-        if (!value || !key) {
-          return null;
-        }
+  for (const chunk of multisigChunks) {
+    const responses = await Promise.all(
+      chunk.map(async ({ address, storage: { signers, pending_ops, threshold } }) => {
+        const response = await getPendingOperations(network, pending_ops);
+
+        const operations = response.map(({ key, value }) => {
+          if (!value || !key) {
+            return null;
+          }
+          return {
+            key,
+            rawActions: value.actions,
+            approvals: value.approvals.map(parseImplicitPkh),
+          };
+        });
+
         return {
-          key,
-          rawActions: value.actions,
-          approvals: value.approvals.map(parseImplicitPkh),
-        };
-      });
-
-      return {
-        address: parseContractPkh(address),
-        threshold: Number(threshold),
-        signers: signers.map(parseImplicitPkh),
-        pendingOperations: compact(operations),
-      };
-    })
-  );
+          address: parseContractPkh(address),
+          threshold: Number(threshold),
+          signers: signers.map(parseImplicitPkh),
+          pendingOperations: compact(operations),
+        } as MultisigWithPendingOperations;
+      })
+    );
+    multisigsWithOperations = multisigsWithOperations.concat(responses);
+  }
 
   return multisigsWithOperations;
 };
