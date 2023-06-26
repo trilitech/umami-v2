4a4a19c979d486fd282c07e7b75b5b335185b3f4
Leo Unoki <leo.unoki@trili.tech>
2023-06-26 13:34:22 +0100

    Use single accordion and rename

--- src/components/AccountCard/AssetsPannel/MultisigPendingAccordion/MultisigPendingAccordionItem.test.tsx
+++ src/components/AccountCard/AssetsPannel/MultisigPendingAccordion/MultisigPendingAccordionItem.test.tsx
@@ -1,6 +1,7 @@
+import { Accordion } from "@chakra-ui/react";
 import { mockImplicitAddress } from "../../../../mocks/factories";
 import { render, screen } from "../../../../mocks/testUtils";
-import MultisigPendingCard from "./MultisigPendingCard";
+import MultisigPendingAccordionItem from "./MultisigPendingAccordionItem";
 
 describe("<MultisigPendingCard/>", () => {
   it("displays the correct number of pending approvals", () => {
@@ -8,15 +9,17 @@ describe("<MultisigPendingCard/>", () => {
     const pkh1 = mockImplicitAddress(1);
     const pkh2 = mockImplicitAddress(2);
     render(
-      <MultisigPendingCard
-        operation={{
-          key: "1",
-          rawActions: "action",
-          approvals: [pkh0],
-        }}
-        threshold={3}
-        signers={[pkh0, pkh1, pkh2]}
-      />
+      <Accordion>
+        <MultisigPendingAccordionItem
+          operation={{
+            key: "1",
+            rawActions: "action",
+            approvals: [pkh0],
+          }}
+          threshold={3}
+          signers={[pkh0, pkh1, pkh2]}
+        />
+      </Accordion>
     );
 
     expect(screen.getByTestId("pending-approvals-count")).toHaveTextContent("2");
@@ -27,15 +30,17 @@ describe("<MultisigPendingCard/>", () => {
     const pkh1 = mockImplicitAddress(1);
     const pkh2 = mockImplicitAddress(2);
     render(
-      <MultisigPendingCard
-        operation={{
-          key: "1",
-          rawActions: "action",
-          approvals: [pkh0, pkh1, pkh2],
-        }}
-        threshold={2}
-        signers={[pkh0, pkh1, pkh2]}
-      />
+      <Accordion>
+        <MultisigPendingAccordionItem
+          operation={{
+            key: "1",
+            rawActions: "action",
+            approvals: [pkh0, pkh1, pkh2],
+          }}
+          threshold={2}
+          signers={[pkh0, pkh1, pkh2]}
+        />
+      </Accordion>
     );
 
     expect(screen.getByTestId("pending-approvals-count")).toHaveTextContent("0");
