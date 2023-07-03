841bd2a8d3c2573e1a14baa27f91931749910c71
Lev Kowalski <levkowalski@gmail.com>
2023-07-03 13:16:17 +0200

    add extra test case

--- src/components/AccountFilter/AccountFilter.test.tsx
+++ src/components/AccountFilter/AccountFilter.test.tsx
@@ -74,6 +74,10 @@ describe("AccountFilter", () => {
   test("mapToFilteredArray returns the right value", () => {
     expect(mapToFilteredArray({ foo: ["hello"], bar: ["cool"] }, ["foo"])).toEqual(["hello"]);
     expect(mapToFilteredArray({ foo: ["hello"], bar: ["cool"] }, [])).toEqual(["hello", "cool"]);
+    expect(mapToFilteredArray({ foo: ["hello"], bar: ["cool"] }, ["baz"])).toEqual([
+      "hello",
+      "cool",
+    ]);
     expect(mapToFilteredArray({ foo: ["hello"], bar: ["cool"] }, ["foo", "bar"])).toEqual([
       "hello",
       "cool",
