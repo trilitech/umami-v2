6775aee36463726d0dc8528fd3cc92b8cbb5d109
Sergey Kintsel <sergey.kintsel@trili.tech>
2023-07-14 11:10:38 +0100

    Improve useAddressKind tests

--- src/components/AddressPill/useAddressKind.test.ts
+++ src/components/AddressPill/useAddressKind.test.ts
@@ -1,105 +1,44 @@
 import { renderHook } from "@testing-library/react";
-import configureStore from "redux-mock-store";
-import { mockBaker, mockContractAddress, mockImplicitAddress } from "../../mocks/factories";
+import { cloneDeep } from "lodash";
+import { hedgehoge } from "../../mocks/fa12Tokens";
+import { uUSD } from "../../mocks/fa2Tokens";
+import { mockBaker, mockImplicitAccount, mockImplicitAddress } from "../../mocks/factories";
 import { multisigs } from "../../mocks/multisig";
 import { getWrapper } from "../../mocks/store";
-import { AccountType, ImplicitAccount } from "../../types/Account";
-import { parseImplicitPkh } from "../../types/Address";
-import { Baker } from "../../types/Baker";
-import { Contact } from "../../types/Contact";
-import { TokenBalance } from "../../types/TokenBalance";
-import { Multisig } from "../../utils/multisig/types";
+import { ReduxStore } from "../../providers/ReduxStore";
+import { parseContractPkh, parseImplicitPkh, parsePkh } from "../../types/Address";
+import accountsSlice from "../../utils/store/accountsSlice";
+import assetsSlice from "../../utils/store/assetsSlice";
+import contactsSlice from "../../utils/store/contactsSlice";
+import multisigsSlice from "../../utils/store/multisigsSlice";
+import { store } from "../../utils/store/store";
 import useAddressKind from "./useAddressKind";
 
-type MockStoreState = {
-  accounts: {
-    items: ImplicitAccount[];
-  };
-  assets: {
-    balances: { tokens: Record<string, TokenBalance[] | undefined> };
-    bakers: Baker[];
-  };
-  contacts: Record<string, Contact>;
-  multisigs: {
-    items: Multisig[];
-  };
-};
+afterEach(() => {
+  store.dispatch(accountsSlice.actions.reset());
+  store.dispatch(contactsSlice.actions.reset());
+  store.dispatch(multisigsSlice.actions.reset());
+  store.dispatch(assetsSlice.actions.reset());
+});
 
 describe("useAddressKind", () => {
-  const implicit0 = mockImplicitAddress(0);
-  const implicit1 = mockImplicitAddress(1);
-  const implicit2 = mockImplicitAddress(4);
-  const unknown = mockImplicitAddress(3);
-  const contract0 = mockContractAddress(0);
-  const contract1 = mockContractAddress(1);
-  const baker = mockBaker(2);
-
-  const initialState: MockStoreState = {
-    accounts: {
-      items: [
-        {
-          label: "label0",
-          curve: "ed25519",
-          derivationPath: "derivationPath",
-          type: AccountType.MNEMONIC,
-          seedFingerPrint: "seedFingerPrint",
-          address: implicit0,
-          pk: "pk",
-        },
-        {
-          label: "label1",
-          curve: "ed25519",
-          derivationPath: "derivationPath",
-          type: AccountType.MNEMONIC,
-          seedFingerPrint: "seedFingerPrint",
-          address: implicit1,
-          pk: "pk",
-        },
-      ],
-    },
-    assets: {
-      balances: {
-        tokens: {
-          pkh0: [
-            { type: "fa1.2", contract: contract0.pkh, balance: "0" },
-            {
-              type: "fa2",
-              contract: contract1.pkh,
-              balance: "0",
-              tokenId: "0",
-              metadata: { name: "fa2Token" },
-            },
-          ],
-        },
-      },
-      bakers: [baker],
-    },
-    contacts: {
-      [implicit1.pkh]: { name: "name1", pkh: implicit1.pkh },
-      [implicit2.pkh]: { name: "name2", pkh: implicit2.pkh },
-    },
-    multisigs: {
-      items: multisigs,
-    },
-  };
-
-  const mockStore = configureStore();
-  const store = mockStore(initialState);
-
-  it("returns owned implicit account addressKind", () => {
-    const { result: addressKindRef } = renderHook(() => useAddressKind(implicit0), {
-      wrapper: getWrapper(store),
+  test("owned implicit account", () => {
+    const implicitAccount0 = mockImplicitAccount(0);
+    store.dispatch(accountsSlice.actions.add([mockImplicitAccount(0)]));
+    const { result: addressKindRef } = renderHook(() => useAddressKind(implicitAccount0.address), {
+      wrapper: ReduxStore,
     });
     expect(addressKindRef.current).toEqual({
       type: "ownedImplicit",
-      pkh: implicit0.pkh,
-      label: "label0",
+      pkh: implicitAccount0.address.pkh,
+      label: "Account 0",
     });
   });
 
-  it("returns owned multisig account addressKind", () => {
+  test("owned multisig account", () => {
+    store.dispatch(multisigsSlice.actions.setMultisigs(multisigs));
     const { result: addressKindRef } = renderHook(() => useAddressKind(multisigs[0].address), {
-      wrapper: getWrapper(store),
+      wrapper: ReduxStore,
     });
     expect(addressKindRef.current).toEqual({
       type: "ownedMultisig",
@@ -108,29 +47,48 @@ describe("useAddressKind", () => {
     });
   });
 
-  it("returns fa1.2 addressKind", () => {
-    const { result: addressKindRef } = renderHook(() => useAddressKind(contract0), {
-      wrapper: getWrapper(store),
-    });
-    expect(addressKindRef.current).toEqual({
-      type: "fa1.2",
-      pkh: contract0.pkh,
-      label: null,
+  [
+    { type: "fa1.2", tokenBalance: hedgehoge(mockImplicitAddress(0)) },
+    { type: "fa2", tokenBalance: uUSD(mockImplicitAddress(0)) },
+  ].forEach(({ type, tokenBalance }) => {
+    describe(`${type} token`, () => {
+      const tokenContractAddress = parseContractPkh(tokenBalance.token.contract?.address as string);
+
+      it("returns empty label if name is not present", () => {
+        const withoutName = cloneDeep(tokenBalance);
+        delete withoutName.token.metadata?.name;
+        store.dispatch(assetsSlice.actions.updateTokenBalance([withoutName]));
+
+        const { result: addressKindRef } = renderHook(() => useAddressKind(tokenContractAddress), {
+          wrapper: getWrapper(store),
+        });
+        expect(addressKindRef.current).toEqual({
+          type: type,
+          pkh: tokenContractAddress.pkh,
+          label: null,
+        });
+      });
+
+      it("returns label", () => {
+        store.dispatch(assetsSlice.actions.updateTokenBalance([tokenBalance]));
+
+        const { result: addressKindRef } = renderHook(() => useAddressKind(tokenContractAddress), {
+          wrapper: getWrapper(store),
+        });
+
+        expect(addressKindRef.current).toEqual({
+          type: type,
+          pkh: tokenContractAddress.pkh,
+          label: tokenBalance.token.metadata?.name,
+        });
+      });
     });
   });
 
-  it("returns fa2 addressKind", () => {
-    const { result: addressKindRef } = renderHook(() => useAddressKind(contract1), {
-      wrapper: getWrapper(store),
-    });
-    expect(addressKindRef.current).toEqual({
-      type: "fa2",
-      pkh: contract1.pkh,
-      label: "fa2Token",
-    });
-  });
+  test("baker", () => {
+    const baker = mockBaker(2);
+    store.dispatch(assetsSlice.actions.updateBakers([baker]));
 
-  it("returns baker addressKind", () => {
     const { result: addressKindRef } = renderHook(
       () => useAddressKind(parseImplicitPkh(baker.address)),
       {
@@ -144,18 +102,53 @@ describe("useAddressKind", () => {
     });
   });
 
-  it("returns contact addressKind", () => {
-    const { result: addressKindRef } = renderHook(() => useAddressKind(implicit2), {
-      wrapper: getWrapper(store),
+  describe("contacts", () => {
+    it("returns contact if it exists", () => {
+      const contact1 = { name: "name1", pkh: mockImplicitAddress(3).pkh };
+      store.dispatch(contactsSlice.actions.upsert(contact1));
+      const { result: addressKindRef } = renderHook(() => useAddressKind(parsePkh(contact1.pkh)), {
+        wrapper: getWrapper(store),
+      });
+      expect(addressKindRef.current).toEqual({
+        type: "contact",
+        pkh: contact1.pkh,
+        label: contact1.name,
+      });
     });
-    expect(addressKindRef.current).toEqual({
-      type: "contact",
-      pkh: implicit2.pkh,
-      label: "name2",
+
+    [
+      { type: "ownedImplicit", address: mockImplicitAccount(0).address.pkh },
+      { type: "ownedMultisig", address: multisigs[0].address.pkh },
+      {
+        type: "fa1.2",
+        address: hedgehoge(mockImplicitAddress(0)).token.contract?.address as string,
+      },
+      { type: "fa2", address: uUSD(mockImplicitAddress(0)).token.contract?.address as string },
+      { type: "baker", address: mockBaker(1).address },
+    ].forEach(({ type, address }) => {
+      it(`prioritises ${type} over the contact`, () => {
+        store.dispatch(multisigsSlice.actions.setMultisigs(multisigs));
+        store.dispatch(accountsSlice.actions.add([mockImplicitAccount(0)]));
+        store.dispatch(
+          assetsSlice.actions.updateTokenBalance([
+            hedgehoge(mockImplicitAddress(0)),
+            uUSD(mockImplicitAddress(0)),
+          ])
+        );
+        store.dispatch(assetsSlice.actions.updateBakers([mockBaker(1)]));
+
+        store.dispatch(contactsSlice.actions.upsert({ name: "name1", pkh: address }));
+
+        const { result: addressKindRef } = renderHook(() => useAddressKind(parsePkh(address)), {
+          wrapper: getWrapper(store),
+        });
+        expect(addressKindRef.current.type).toEqual(type);
+      });
     });
   });
 
-  it("returns unknown addressKind", () => {
+  it("returns unknown if nothing matched", () => {
+    const unknown = mockImplicitAddress(3);
     const { result: addressKindRef } = renderHook(() => useAddressKind(unknown), {
       wrapper: getWrapper(store),
     });
@@ -163,17 +156,6 @@ describe("useAddressKind", () => {
       type: "unknown",
       pkh: unknown.pkh,
       label: null,
-    });
-  });
-
-  it("contact AddressKind has less priority", () => {
-    const { result: addressKindRef } = renderHook(() => useAddressKind(implicit1), {
-      wrapper: getWrapper(store),
-    });
-    expect(addressKindRef.current).toEqual({
-      type: "ownedImplicit",
-      pkh: implicit1.pkh,
-      label: "label1",
     });
   });
 });
