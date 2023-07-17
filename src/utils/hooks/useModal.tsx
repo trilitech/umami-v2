77dfc1d63da4e8e50ff5450f68485a67c9a653f5
Lev Kowalski <levkowalski@gmail.com>
2023-07-17 13:45:48 +0200

    refactor

---
+++ src/utils/hooks/useModal.tsx
@@ -0,0 +1,19 @@
+import { Modal, useDisclosure } from "@chakra-ui/react";
+import { useRef } from "react";
+
+export function useModal<T>(Component: React.ComponentType<{ params: T }>) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const paramsRef = useRef<T | undefined>(undefined);
+
+  return {
+    modalElement: (
+      <Modal isOpen={isOpen} onClose={onClose}>
+        {paramsRef.current && <Component params={paramsRef.current} />}
+      </Modal>
+    ),
+    onOpen: (options: T) => {
+      paramsRef.current = options;
+      onOpen();
+    },
+  };
+}
