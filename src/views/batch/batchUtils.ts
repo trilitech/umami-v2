93d7e0ab28b41013fa0443ed021b979ecd95d2de
Sergey Kintsel <sergey.kintsel@trili.tech>
2023-07-26 14:51:52 +0100

    Remove batch.isSimulating property
    It needs lots of special care but it doesn't add much value
    even if we estimate the batch twice in parallel it doesn't hurt much
    but is incredibly hard to do thanks to the isLoading prop of buttons

--- src/views/batch/batchUtils.ts
+++ src/views/batch/batchUtils.ts
@@ -1,7 +1,7 @@
 import { BigNumber } from "bignumber.js";
 import { Estimate } from "@taquito/taquito";
 import { estimateBatch } from "../../utils/tezos";
-import { zip } from "../../utils/helpers";
+import { zip } from "lodash";
 import { Operation, OperationWithFee } from "../../types/Operation";
 import { TezosNetwork } from "../../types/TezosNetwork";
 import { ImplicitAccount } from "../../types/Account";
@@ -44,8 +44,17 @@ export const operationsToBatchItems = async (
 
   return zip(operations, estimations).map(([operation, estimate]) => {
     return {
-      ...operation,
-      fee: String(estimate.suggestedFeeMutez),
+      ...(operation as OperationWithFee), // operations.length is always >= estimations.length
+      // The way taquito works we need to take the max of suggestedFeeMutez and totalCost
+      // because the suggestedFeeMutez does not include the storage & execution cost
+      // and in these cases the totalCost is the one to go (so, for contract calls)
+      // though totalCost doesn't work well with simple tez transfers and suggestedFeeMutez is more accurate
+      //
+      // multisig operations cannot be estimated as implicit ones and what we have is
+      // just one estimate for the whole batch (the contract call fee)
+      // to make it at least show the correct totalFee we assign the fee as the first operation's fee
+      // TODO: fix this and handle multisig and implicit operations separately
+      fee: String(Math.max(estimate?.suggestedFeeMutez || 0, estimate?.totalCost || 0)),
     };
   });
 };
