403dd1e594a02302b946ec69ce2045496ecc7d1f
Leo Unoki <leo.unoki@trili.tech>
2023-08-03 12:57:38 +0100

    Add getErrorContext

---
+++ src/utils/getErrorContext.test.ts
@@ -0,0 +1,12 @@
+import getErrorContext from "./getErrorContext";
+
+describe("getErrorContext", () => {
+  it("should get error context", () => {
+    const { description, stacktrace } = getErrorContext({
+      message: "some error message",
+      stack: "some stacktrace",
+    });
+    expect(description).toEqual("some error message");
+    expect(stacktrace).toEqual("some stacktrace");
+  });
+});
