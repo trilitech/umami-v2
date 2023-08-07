7b189c380c634f3b33c99afef2dbe802250d8732
Leo Unoki <leo.unoki@trili.tech>
2023-08-07 10:42:20 +0100

    Connect react-error-bounday

---
+++ src/components/Fallback.tsx
@@ -0,0 +1,6 @@
+import { Flex } from "@chakra-ui/react";
+
+export const Fallback: React.FC = () => {
+  return <Flex>Error thrown</Flex>;
+};
+export default Fallback;
--- src/index.tsx
+++ src/index.tsx
@@ -3,25 +3,29 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Router from "./Router";
-
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 import store from "./utils/redux/store";
-
 import { ReactQueryProvider } from "./providers/ReactQueryProvider";
 import { ReduxStore } from "./providers/ReduxStore";
 import { UmamiTheme } from "./providers/UmamiTheme";
+import { ErrorBoundary } from "react-error-boundary";
+import Fallback from "./components/Fallback";
+
+const logError = (error: Error, info: { componentStack: string }) => {};
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <UmamiTheme>
       <ReduxStore>
-        <PersistGate loading={null} persistor={persistStore(store)}>
-          <ReactQueryProvider>
-            <Router />
-          </ReactQueryProvider>
-        </PersistGate>
+        <ErrorBoundary FallbackComponent={Fallback} onError={logError}>
+          <PersistGate loading={null} persistor={persistStore(store)}>
+            <ReactQueryProvider>
+              <Router />
+            </ReactQueryProvider>
+          </PersistGate>
+        </ErrorBoundary>
       </ReduxStore>
     </UmamiTheme>
   </React.StrictMode>
