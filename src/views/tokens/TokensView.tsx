cd5acc85dfd4448487a46bc59cea57345046b46e
Leo Unoki <leo.unoki@trili.tech>
2023-08-07 14:17:22 +0100

    Remove no tokens

--- src/views/tokens/TokensView.tsx
+++ src/views/tokens/TokensView.tsx
@@ -1,6 +1,6 @@
 import { Box, Flex } from "@chakra-ui/react";
 import { useAccountsFilter } from "../../components/useAccountsFilter";
-import { NoTokens } from "../../components/NoItems";
+import NoItems from "../../components/NoItems";
 import { TopBar } from "../../components/TopBar";
 import { useGetAccountAllTokens } from "../../utils/hooks/assetsHooks";
 import { useSendFormModal } from "../home/useSendFormModal";
@@ -17,25 +17,25 @@ const TokensView = () => {
     .map(account => [account, getTokens(account.address.pkh)] as const)
     .filter(([, tokens]) => tokens.length > 0);
 
+  if (accountsWithTokens.length === 0) {
+    return <NoItems text="No Tokens found" />;
+  }
+
   return (
     <Flex direction="column" height="100%">
       <TopBar title="Tokens" />
       {accountsFilter}
 
-      {accountsWithTokens.length === 0 ? (
-        <NoTokens />
-      ) : (
-        <Box overflow="auto">
-          {accountsWithTokens.map(([account, tokens]) => (
-            <AccountTokensTile
-              key={account.address.pkh}
-              tokens={tokens}
-              account={account}
-              onOpenSendModal={onOpen}
-            />
-          ))}
-        </Box>
-      )}
+      <Box overflow="auto">
+        {accountsWithTokens.map(([account, tokens]) => (
+          <AccountTokensTile
+            key={account.address.pkh}
+            tokens={tokens}
+            account={account}
+            onOpenSendModal={onOpen}
+          />
+        ))}
+      </Box>
       {modalElement}
     </Flex>
   );
