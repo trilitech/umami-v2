b29380de7f824a72012ef94d8f8f4a1a29727c9f
Sergey Kintsel <sergey.kintsel@trili.tech>
2023-08-08 12:56:36 +0100

    Make tabs look like on Figma

---
+++ src/style/theme/tabs.ts
@@ -0,0 +1,22 @@
+import { tabsAnatomy } from "@chakra-ui/anatomy";
+import { createMultiStyleConfigHelpers } from "@chakra-ui/styled-system";
+import colors from "../colors";
+
+const { definePartsStyle, defineMultiStyleConfig } = createMultiStyleConfigHelpers(
+  tabsAnatomy.keys
+);
+
+const baseStyle = definePartsStyle({
+  tab: {
+    _selected: {
+      fontWeight: 600,
+      color: colors.white,
+      borderColor: colors.orange,
+      borderBottom: `2px solid ${colors.orange}`,
+    },
+  },
+});
+
+export const tabsTheme = defineMultiStyleConfig({
+  baseStyle,
+});
