2b5635d7c6b53ff1154a281e25dd549dba457f64
Sergey Kintsel <sergey.kintsel@trili.tech>
2023-08-08 13:27:57 +0100

    Add View All link to the Recent NFTs block

--- src/views/home/NftList.tsx
+++ src/views/home/NftList.tsx
@@ -1,15 +1,29 @@
-import { TabList, Tabs } from "@chakra-ui/react";
+import { Link, TabList, Tabs } from "@chakra-ui/react";
+import { Link as ReactRouterLink } from "react-router-dom";
 import { NFTsGrid } from "../../components/AccountCard/AssetsPanel/NFTsGrid";
 import NestedScroll from "../../components/NestedScroll";
 import SmallTab from "../../components/SmallTab";
 import { useAllNfts } from "../../utils/hooks/assetsHooks";
+import colors from "../../style/colors";
 export const NftList = () => {
   const nfts = useAllNfts();
 
   return (
-    <Tabs height="100%" display="flex" flexDirection="column" bg="umami.gray.900" borderRadius={4}>
-      <TabList>
-        <SmallTab>All NFTs</SmallTab>
+    <Tabs height="100%" display="flex" flexDirection="column" borderRadius={4}>
+      <TabList justifyContent="space-between">
+        <SmallTab>Recent NFTs</SmallTab>
+        <Link
+          as={ReactRouterLink}
+          to="/nfts"
+          marginTop="8px"
+          display="inline-block"
+          marginRight="12px"
+          fontSize="14px"
+          color={colors.gray[400]}
+          _hover={{ color: colors.gray[300] }}
+        >
+          View All
+        </Link>
       </TabList>
 
       <NestedScroll>
