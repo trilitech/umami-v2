9a9e7efde19002041b9858c639fad7b28e829d26
Sergey Kintsel <sergey.kintsel@trili.tech>
2023-08-10 19:50:27 +0100

    Update storybook package
    It's the only one that depends on webpack@4

--- src/stories/Headings.stories.tsx
+++ src/stories/Headings.stories.tsx
@@ -8,7 +8,7 @@ export default {
 } as ComponentMeta<typeof Heading>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Heading> = args => <Heading {...args} />;
+const Template: ComponentStory<typeof Heading> = (args: any) => <Heading {...args} />;
 
 const exampleTxt = "This is an Umami heading";
 export const Xs = Template.bind({});
