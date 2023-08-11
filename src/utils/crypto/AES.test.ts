c71a00bc0d29e45d36bb60bf00d355b0f6cd2346
Leo Unoki <leo.unoki@trili.tech>
2023-08-11 15:36:03 +0100

    Add new AES functions

---
+++ src/utils/crypto/AES.test.ts
@@ -0,0 +1,44 @@
+import { mnemonic } from "../../mocks/mockMnemonic";
+import { recoveredPhrases, umamiBackup } from "../../mocks/umamiV1BackupFile";
+import { decrypt, encrypt } from "./AES";
+
+const password = "password";
+
+describe("AES", () => {
+  it("encryption and decryption works", async () => {
+    const encrypted = await encrypt(mnemonic, password);
+    const decrypted = await decrypt(encrypted, password);
+
+    expect(decrypted).toEqual(mnemonic);
+  });
+
+  it("decryption restores mnemonic from v1 backup file", async () => {
+    for (let i = 0; i < umamiBackup.recoveryPhrases.length; i++) {
+      const encrypted = umamiBackup.recoveryPhrases[i];
+      const expected = recoveredPhrases[i];
+      const decrypted = await decrypt(encrypted, "password", "V1");
+      expect(decrypted).toEqual(expected);
+    }
+  });
+
+  it("V1 decryption fails with V2 encryption", async () => {
+    const encrypted = await encrypt(mnemonic, password);
+
+    await expect(decrypt(encrypted, password, "V1")).rejects.toThrowError("Error decrypting data");
+  });
+
+  it("decryption fails with cyclic password", async () => {
+    // Used to work in V1. Now it fails.
+    const encrypted = await encrypt(mnemonic, "abc");
+
+    await expect(decrypt(encrypted, "abcabc")).rejects.toThrowError("Error decrypting data");
+  });
+
+  it("decryption fails with wrong password", async () => {
+    const encrypted = await encrypt(mnemonic, password);
+
+    await expect(decrypt(encrypted, `wrong ${password}`)).rejects.toThrowError(
+      "Error decrypting data"
+    );
+  });
+});
---
+++ src/utils/crypto/KDF.ts
@@ -0,0 +1,47 @@
+import { AES_MODE } from "./AES";
+
+const BYTE_TO_BIT = 8;
+// Use the full output of the hash to maximize the derived key's strength (sha256)
+const KEY_SIZE = 32;
+
+const NUM_ITERATIONS_V2 = 600000; // https://en.wikipedia.org/wiki/PBKDF2
+const NUM_ITERATIONS_V1 = 10000; // https://en.wikipedia.org/wiki/PBKDF2
+
+export const derivePasswordBasedKeyV1 = async (
+  password: string,
+  salt: Uint8Array
+): Promise<CryptoKey> =>
+  derivePasswordBasedKey(Buffer.alloc(32, password, "utf-8"), salt, NUM_ITERATIONS_V1);
+
+export const derivePasswordBasedKeyV2 = async (
+  password: string,
+  salt: Uint8Array
+): Promise<CryptoKey> =>
+  derivePasswordBasedKey(Buffer.from(password, "utf-8"), salt, NUM_ITERATIONS_V2);
+
+const derivePasswordBasedKey = async (
+  password: Buffer,
+  salt: Uint8Array,
+  iterations: number
+): Promise<CryptoKey> => {
+  // PBKDF2 (Password Based Key Derivation Function 2) is typically used for deriving a cryptographic key from a password
+  const algorithm = {
+    name: "PBKDF2",
+    salt: salt,
+    iterations,
+    hash: "SHA-256",
+  };
+
+  // brings raw password into the Web Cryptography API environment.
+  const baseKey = await crypto.subtle.importKey("raw", password, algorithm.name, false, [
+    "deriveKey",
+  ]);
+
+  return await crypto.subtle.deriveKey(
+    algorithm,
+    baseKey,
+    { name: AES_MODE, length: KEY_SIZE * BYTE_TO_BIT },
+    false,
+    ["encrypt", "decrypt"]
+  );
+};
