37baab14e3f6cd609b535de3faf1d7032c7adb54
Leo Unoki <leo.unoki@trili.tech>
2023-08-21 15:12:52 +0100

    Implement FormPage and SIgnPage for token

---
+++ src/components/SendFlow/Token/FormPage.tsx
@@ -0,0 +1,171 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import colors from "../../../style/colors";
+import { parseContractPkh, parsePkh, RawPkh } from "../../../types/Address";
+import { FA12Operation, FA2Operation } from "../../../types/Operation";
+import { KnownAccountsAutocomplete, OwnedAccountsAutocomplete } from "../../AddressAutocomplete";
+import { formDefaultValues, FormPagePropsWithSender, FormSubmitButtons } from "../utils";
+import SignPage from "./SignPage";
+import {
+  useAddToBatchFormAction,
+  useHandleOnSubmitFormActions,
+  useOpenSignPageFormAction,
+} from "../onSubmitFormActionHooks";
+import {
+  FA12TokenBalance,
+  FA2TokenBalance,
+  formatTokenAmount,
+  getRealAmount,
+  tokenSymbol,
+} from "../../../types/TokenBalance";
+
+export type FormValues = {
+  sender: RawPkh;
+  recipient: RawPkh;
+  prettyAmount: string;
+};
+
+export type FATokenBalance = FA12TokenBalance | FA2TokenBalance;
+
+const toOperation =
+  (token: FATokenBalance) =>
+  (formValues: FormValues): FA12Operation | FA2Operation => {
+    const fa2Operation: FA2Operation = {
+      type: "fa2",
+      sender: parsePkh(formValues.sender),
+      recipient: parsePkh(formValues.recipient),
+      contract: parseContractPkh(token.contract),
+      tokenId: token.tokenId,
+      amount: getRealAmount(token, formValues.prettyAmount).toString(),
+    };
+
+    if (token.type === "fa2") {
+      return fa2Operation;
+    }
+
+    return { ...fa2Operation, type: "fa1.2", tokenId: "0" };
+  };
+
+const FormPage: React.FC<
+  FormPagePropsWithSender<FormValues> & { token: FATokenBalance }
+> = props => {
+  const { token } = props;
+  const openSignPage = useOpenSignPageFormAction({
+    SignPage,
+    signPageExtraData: { token },
+    FormPage,
+    defaultFormPageProps: props,
+    toOperation: toOperation(token),
+  });
+
+  const addToBatch = useAddToBatchFormAction(toOperation(token));
+
+  const {
+    onFormSubmitActionHandlers: [onSingleSubmit, onBatchSubmit],
+    isLoading,
+  } = useHandleOnSubmitFormActions([openSignPage, addToBatch]);
+
+  const form = useForm<FormValues>({
+    mode: "onBlur",
+    defaultValues: formDefaultValues(props),
+  });
+  const {
+    formState: { isValid, errors },
+    register,
+    handleSubmit,
+  } = form;
+
+  const prettyBalance = formatTokenAmount(token.balance, token.metadata?.decimals);
+
+  return (
+    <FormProvider {...form}>
+      <ModalContent>
+        <form>
+          <ModalHeader textAlign="center" p="40px 0 32px 0">
+            <Text size="2xl" fontWeight="600">
+              Send
+            </Text>
+            <Text textAlign="center" size="sm" color={colors.gray[400]}>
+              Send one or insert into batch.
+            </Text>
+            <ModalCloseButton />
+          </ModalHeader>
+          <ModalBody>
+            <FormControl mt={3} mb={7} isInvalid={!!errors.prettyAmount}>
+              <FormLabel>Amount</FormLabel>
+              <InputGroup>
+                <Input
+                  isDisabled={isLoading}
+                  type="number"
+                  step="any"
+                  variant="filled"
+                  {...register("prettyAmount", {
+                    required: "Amount is required",
+                    max: {
+                      value: prettyBalance.toString(),
+                      message: `Max quantity is ${prettyBalance}`,
+                    },
+                  })}
+                  placeholder="0"
+                />
+                <InputRightElement pr={3}>{tokenSymbol(token)}</InputRightElement>
+              </InputGroup>
+              {/* TODO: make a custom FormErrorMessage because its styling cannot be applied through theme.ts */}
+              {errors.prettyAmount && (
+                <FormErrorMessage data-testid="amount-error">
+                  {errors.prettyAmount.message}
+                </FormErrorMessage>
+              )}
+            </FormControl>
+
+            <FormControl my={8} isInvalid={!!errors.sender}>
+              <OwnedAccountsAutocomplete
+                label="From"
+                isDisabled={true}
+                inputName="sender"
+                allowUnknown={false}
+              />
+              {errors.sender && (
+                <FormErrorMessage data-testid="from-error">
+                  {errors.sender.message}
+                </FormErrorMessage>
+              )}
+            </FormControl>
+
+            <FormControl my={3} isInvalid={!!errors.recipient}>
+              <KnownAccountsAutocomplete label="To" inputName="recipient" allowUnknown />
+              {errors.recipient && (
+                <FormErrorMessage data-testid="recipient-error">
+                  {errors.recipient.message}
+                </FormErrorMessage>
+              )}
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <FormSubmitButtons
+              isLoading={isLoading}
+              isValid={isValid}
+              onSingleSubmit={handleSubmit(onSingleSubmit)}
+              onAddToBatch={handleSubmit(onBatchSubmit)}
+            />
+          </ModalFooter>
+        </form>
+      </ModalContent>
+    </FormProvider>
+  );
+};
+export default FormPage;
---
+++ src/components/SendFlow/Token/SignPage.tsx
@@ -0,0 +1,103 @@
+import {
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  Text,
+} from "@chakra-ui/react";
+import { FormProvider } from "react-hook-form";
+import colors from "../../../style/colors";
+import { AvailableSignersAutocomplete, OwnedAccountsAutocomplete } from "../../AddressAutocomplete";
+import SignButton from "../../sendForm/components/SignButton";
+import { mutezToPrettyTez, SignPageProps, useSignPageHelpers } from "../utils";
+import { SignPageHeader, headerText } from "../SignPageHeader";
+import { FATokenBalance } from "./FormPage";
+import { formatTokenAmount, tokenSymbol } from "../../../types/TokenBalance";
+import { FA12Operation } from "../../../types/Operation";
+
+const SignPage: React.FC<SignPageProps<{ token: FATokenBalance }>> = props => {
+  const {
+    mode,
+    operations: initialOperations,
+    fee: initialFee,
+    data: { token },
+  } = props;
+  const { fee, operations, estimationFailed, isLoading, form, signer, reEstimate, onSign } =
+    useSignPageHelpers(initialFee, initialOperations, mode);
+
+  const amount = (operations.content[0] as FA12Operation).amount;
+
+  return (
+    <FormProvider {...form}>
+      <ModalContent bg={colors.gray[900]} borderColor={colors.gray[700]} borderRadius="8px">
+        <form>
+          <SignPageHeader {...props} operationsType={operations.type} />
+          <ModalBody>
+            <FormLabel>Amount</FormLabel>
+            <InputGroup>
+              <Input
+                isDisabled={true}
+                type="number"
+                variant="filled"
+                disabled={true}
+                value={formatTokenAmount(amount, token.metadata?.decimals)}
+              />
+              <InputRightElement pr={3}>{tokenSymbol(token)}</InputRightElement>
+            </InputGroup>
+
+            <Flex my={3} alignItems="center" justifyContent="end" px={1}>
+              <Flex>
+                <Text size="sm" mr={1} color={colors.gray[450]}>
+                  Fee:
+                </Text>
+                <Text size="sm" data-testid="fee" color={colors.gray[400]}>
+                  {mutezToPrettyTez(fee)}
+                </Text>
+              </Flex>
+            </Flex>
+
+            {/* TODO: Add sender address tile */}
+            <FormControl my={3}>
+              <OwnedAccountsAutocomplete
+                inputName="sender"
+                label="From"
+                allowUnknown={false}
+                isDisabled
+              />
+            </FormControl>
+
+            {/* TODO: Add recipient address tile */}
+
+            {initialOperations.type === "proposal" && (
+              <FormControl>
+                <AvailableSignersAutocomplete
+                  account={initialOperations.sender}
+                  inputName="signer"
+                  label="Select Proposer"
+                  isDisabled={isLoading}
+                  onUpdate={reEstimate}
+                  keepValid
+                />
+              </FormControl>
+            )}
+          </ModalBody>
+          <ModalFooter>
+            <SignButton
+              isLoading={isLoading}
+              isDisabled={estimationFailed}
+              signer={signer}
+              onSubmit={onSign}
+              text={headerText(operations.type, mode)}
+            />
+          </ModalFooter>
+        </form>
+      </ModalContent>
+    </FormProvider>
+  );
+};
+export default SignPage;
