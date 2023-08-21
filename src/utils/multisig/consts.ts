47127f98e6bf295f94d6cb001d8d9720a9ae4884
Sergey Kintsel <sergey.kintsel@trili.tech>
2023-08-21 18:28:42 +0100

    Use code & type hashes to retrieve multisigs
    They are the same for all networks and we don't have a
    first contract problem on new networks anymore.

--- src/utils/multisig/consts.ts
+++
@@ -1,6 +0,0 @@
-import { TezosNetwork } from "../../types/TezosNetwork";
-
-export const multisigAddress = {
-  [TezosNetwork.MAINNET]: `KT1Lw11GPDxpdWXWudFUUBMA2Cihevmt8QCf`,
-  [TezosNetwork.GHOSTNET]: `KT1Mqvf7bnYe4Ty2n7ZbGkdbebCd4WoTJUUp`,
-};
