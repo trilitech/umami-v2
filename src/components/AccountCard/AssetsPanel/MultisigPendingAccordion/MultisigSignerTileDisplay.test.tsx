3a6f3c9093933852f3e2a9f0992124ac97edfd5b
Lev Kowalski <levkowalski@gmail.com>
2023-08-21 12:10:41 +0200

    fix test

--- src/components/AccountCard/AssetsPanel/MultisigPendingAccordion/MultisigSignerTileDisplay.test.tsx
+++ src/components/AccountCard/AssetsPanel/MultisigPendingAccordion/MultisigSignerTileDisplay.test.tsx
@@ -1,55 +1,72 @@
-import { render, screen } from "../../../../mocks/testUtils";
+import { mockImplicitAccount } from "../../../../mocks/factories";
+import { render, screen, cleanup } from "../../../../mocks/testUtils";
 import { AccountType } from "../../../../types/Account";
+import { formatPkh } from "../../../../utils/formatPkh";
 import { MultisigSignerTileDisplay } from "./MultisigSignerTileDisplay";
 
-test("<MultisigSignerTileDisplay /> renders the right icon given an account type", () => {
-  render(
-    <MultisigSignerTileDisplay
-      kind="contact"
-      pkh="foo"
-      signerState="approvable"
-      onClickApproveExecute={() => {}}
-    />
-  );
-  expect(screen.getByTestId("contact-icon")).toBeInTheDocument();
+const pkh = mockImplicitAccount(0).address.pkh;
 
-  render(
-    <MultisigSignerTileDisplay
-      kind="unknown"
-      pkh="foo"
-      signerState="approvable"
-      onClickApproveExecute={() => {}}
-    />
-  );
-  expect(screen.getByTestId("unknown-contact-icon")).toBeInTheDocument();
+describe("<MultisigSignerTileDisplay />", () => {
+  it("renders the right icon and a shrinked address given an account type", () => {
+    const shrinkedAddress = formatPkh(pkh);
+    render(
+      <MultisigSignerTileDisplay
+        kind="contact"
+        pkh={pkh}
+        signerState="approvable"
+        onClickApproveExecute={() => {}}
+      />
+    );
+    expect(screen.getByTestId("contact-icon")).toBeInTheDocument();
+    expect(screen.getByText(shrinkedAddress)).toBeInTheDocument();
 
-  render(
-    <MultisigSignerTileDisplay
-      kind={AccountType.LEDGER}
-      pkh="foo"
-      signerState="approvable"
-      onClickApproveExecute={() => {}}
-    />
-  );
-  expect(screen.getByTestId("ledger-icon")).toBeInTheDocument();
+    cleanup();
+    render(
+      <MultisigSignerTileDisplay
+        kind={AccountType.LEDGER}
+        pkh={pkh}
+        signerState="approvable"
+        onClickApproveExecute={() => {}}
+      />
+    );
+    expect(screen.getByTestId("ledger-icon")).toBeInTheDocument();
+    expect(screen.getByText(shrinkedAddress)).toBeInTheDocument();
 
-  render(
-    <MultisigSignerTileDisplay
-      kind={AccountType.MNEMONIC}
-      pkh="foo"
-      signerState="approvable"
-      onClickApproveExecute={() => {}}
-    />
-  );
-  expect(screen.getByTestId("identicon")).toBeInTheDocument();
+    cleanup();
+    render(
+      <MultisigSignerTileDisplay
+        kind={AccountType.MNEMONIC}
+        pkh={pkh}
+        signerState="approvable"
+        onClickApproveExecute={() => {}}
+      />
+    );
+    expect(screen.getByTestId("identicon")).toBeInTheDocument();
+    expect(screen.getByText(shrinkedAddress)).toBeInTheDocument();
 
-  render(
-    <MultisigSignerTileDisplay
-      kind={AccountType.SOCIAL}
-      pkh="foo"
-      signerState="approvable"
-      onClickApproveExecute={() => {}}
-    />
-  );
-  expect(screen.getByTestId("social-icon")).toBeInTheDocument();
+    cleanup();
+    render(
+      <MultisigSignerTileDisplay
+        kind={AccountType.SOCIAL}
+        pkh={pkh}
+        signerState="approvable"
+        onClickApproveExecute={() => {}}
+      />
+    );
+    expect(screen.getByTestId("social-icon")).toBeInTheDocument();
+    expect(screen.getByText(shrinkedAddress)).toBeInTheDocument();
+  });
+
+  it("should render correct icon and full address on unknown accounts", () => {
+    render(
+      <MultisigSignerTileDisplay
+        kind="unknown"
+        pkh={pkh}
+        signerState="approvable"
+        onClickApproveExecute={() => {}}
+      />
+    );
+    expect(screen.getByTestId("unknown-contact-icon")).toBeInTheDocument();
+    expect(screen.getByText(pkh)).toBeInTheDocument();
+  });
 });
