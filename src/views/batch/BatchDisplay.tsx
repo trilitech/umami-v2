743aad521c8b0689545ace60100e421203c2dfed
Sergey Kintsel <sergey.kintsel@trili.tech>
2023-08-22 12:34:36 +0100

    Add initial Batch view setup

--- src/views/batch/BatchDisplay.tsx
+++ src/views/batch/BatchDisplay.tsx
@@ -1,152 +1,176 @@
-import {
-  AspectRatio,
-  Box,
-  Button,
-  Flex,
-  IconButton,
-  Image,
-  Table,
-  TableContainer,
-  Tbody,
-  Td,
-  Text,
-  Tr,
-} from "@chakra-ui/react";
+import { Box, Button, Divider, Flex, Heading, IconButton, Text } from "@chakra-ui/react";
 import React from "react";
-import { BsTrash } from "react-icons/bs";
-import { FiExternalLink } from "react-icons/fi";
-import AddressPill from "../../components/AddressPill/AddressPill";
-import { IconAndTextBtnLink } from "../../components/IconAndTextBtn";
 import { AccountOperations } from "../../components/sendForm/types";
 import { Account } from "../../types/Account";
 import { Operation } from "../../types/Operation";
-import { formatTokenAmount, tokenSymbol } from "../../types/Token";
-import { formatPkh, prettyTezAmount } from "../../utils/format";
-import { useSelectedNetwork } from "../../utils/hooks/assetsHooks";
-import { TokenLookup, useGetToken } from "../../utils/hooks/tokensHooks";
-import { getIPFSurl } from "../../utils/token/nftUtils";
-import { buildTzktAddressUrl } from "../../utils/tzkt/helpers";
+import { prettyTezAmount } from "../../utils/format";
+import { useClearBatch } from "../../utils/hooks/assetsHooks";
 import { AccountSmallTile } from "../../components/AccountSelector/AccountSmallTile";
 import colors from "../../style/colors";
 import pluralize from "pluralize";
 import { headerText } from "../../components/SendFlow/SignPageHeader";
+import Trash from "../../assets/icons/Trash";
+import { nanoid } from "nanoid";
+import AddressPill from "../../components/AddressPill/AddressPill";
+import { TEZ } from "../../utils/tezos";
 
-const renderAmount = (operation: Operation, getToken: TokenLookup) => {
+const RightHeader = ({
+  operations: { type: operationsType, sender, operations },
+}: {
+  operations: AccountOperations;
+}) => {
+  const clearBatch = useClearBatch();
+  return (
+    <Box justifyContent="space-between" alignItems="center">
+      <Text color={colors.gray[400]} size="sm" display="inline-block">
+        {pluralize("transaction", operations.length, true)}
+      </Text>
+      <Button variant="primary" ml="30px">
+        {headerText(operationsType, "batch")}
+      </Button>
+      <IconButton
+        onClick={() => clearBatch(sender)} // TODO: add a confirmation modal
+        aria-label="remove-batch"
+        ml="18px"
+        variant="circle"
+        borderRadius="4px"
+        icon={<Trash />}
+      />
+    </Box>
+  );
+};
+
+// TODO: test
+export const prettyOperationType = (operation: Operation) => {
   switch (operation.type) {
     case "fa1.2":
-    case "fa2": {
-      const token = getToken(operation.contract.pkh, operation.tokenId);
-      if (!token) {
-        throw new Error(`Token not found ${operation.contract.pkh} ${operation.tokenId}}`);
-      }
-      const amount = formatTokenAmount(operation.amount, token.metadata?.decimals);
-      return (
-        <Flex>
-          <Text mr={1}>{amount} </Text>
-
-          {token.type === "nft" ? (
-            <AspectRatio ml={2} height={6} width={6} ratio={1}>
-              <Image src={getIPFSurl(token.metadata.displayUri)} />
-            </AspectRatio>
-          ) : (
-            <Text>{tokenSymbol(token)}</Text>
-          )}
-        </Flex>
-      );
-    }
-    case "tez":
-      return prettyTezAmount(operation.amount);
-    case "delegation":
+    case "fa2":
+      return "Token Transfer";
     case "undelegation":
+      return "End Delegation";
+    case "delegation":
+      return "Start Delegation"; // TODO: fix
+    case "tez":
+      return `${TEZ} Transfer`;
     case "contract_origination":
     case "contract_call":
       return "";
   }
 };
 
-const RightHeader = ({ operations }: { operations: AccountOperations }) => (
-  <Box justifyContent="space-between" verticalAlign="middle">
-    <Text color={colors.gray[400]} size="sm" display="inline-block">
-      {pluralize("transaction", operations.operations.length, true)}
-    </Text>
-    <Button size="sm" variant="primary" ml="30px">
-      {headerText(operations.type, "batch")}
-    </Button>
-    <IconButton aria-label="remove-batch" ml="18px" w="38px" size="sm" icon={<BsTrash />} />
-  </Box>
-);
+const OperationDisplay = ({ operation }: { operation: Operation }) => {
+  switch (operation.type) {
+    case "tez":
+      return (
+        <Flex>
+          <Heading size="sm">{prettyTezAmount(operation.amount)}</Heading>
+        </Flex>
+      );
+    case "fa1.2":
+    case "fa2":
+    case "delegation":
+    case "undelegation":
+    case "contract_origination":
+    case "contract_call":
+      return null;
+  }
+};
+
+const OperationRecipient = ({ operation }: { operation: Operation }) => {
+  let address;
+
+  switch (operation.type) {
+    case "undelegation":
+    case "contract_origination":
+      address = undefined;
+      break;
+    case "tez":
+    case "fa1.2":
+    case "fa2":
+    case "delegation":
+      address = operation.recipient;
+      break;
+
+    case "contract_call":
+      address = operation.contract;
+      break;
+  }
+  if (!address) {
+    return <Text color={colors.gray[500]}>N/A</Text>;
+  }
+  return (
+    <>
+      <Text mr="6px" color={colors.gray[450]}>
+        To:
+      </Text>
+      <AddressPill address={address} />
+    </>
+  );
+};
 
 export const BatchDisplay: React.FC<{
   account: Account;
   operations: AccountOperations;
 }> = ({ account, operations: accountOperations }) => {
-  const { operations, type: operationsType } = accountOperations;
-  const network = useSelectedNetwork();
-  const getToken = useGetToken();
+  const { operations } = accountOperations;
 
   return (
-    <Flex data-testid={`batch-table-${account.address.pkh}`} mb={4}>
-      <Box flex={1}>
+    <Box data-testid={`batch-table-${account.address.pkh}`} mb={4} w="100%">
+      <Flex
+        borderTopRadius="8px"
+        justifyContent="space-between"
+        p="20px 23px 20px 30px"
+        bg={colors.gray[800]}
+      >
+        <Flex alignItems="center">
+          <AccountSmallTile pkh={account.address.pkh} pl={0} />
+        </Flex>
+        <RightHeader operations={accountOperations} />
+      </Flex>
+      <Flex bg={colors.gray[900]} px="30px" py="20px" flexDirection="column">
+        {operations.map((operation, i) => (
+          <Box key={nanoid()}>
+            <Flex height="50px" flexDirection="column">
+              <Flex>
+                <OperationDisplay operation={operation} />
+              </Flex>
+
+              <Flex mt="8px" width="100%" alignItems="center" justifyContent="space-between">
+                <Flex>
+                  <OperationRecipient operation={operation} />
+                </Flex>
+                <Flex>
+                  <Text size="sm" color={colors.gray[450]} alignSelf="flex-end">
+                    {prettyOperationType(operation)}
+                  </Text>
+                  <IconButton
+                    ml="12px"
+                    aria-label="Remove"
+                    color={colors.gray[300]}
+                    icon={<Trash />}
+                    borderRadius="full"
+                    size="xs"
+                    width="24px"
+                    variant="circle"
+                  />
+                </Flex>
+              </Flex>
+            </Flex>
+            {i < operations.length - 1 && <Divider my="20px" />}
+          </Box>
+        ))}
+      </Flex>
+      {operations.length > 9 && (
         <Flex
-          justifyContent="space-between"
-          borderRadius="8px 8px 0 0"
+          justifyContent="end"
+          borderRadius="0 0 8px 8px"
           p="20px 23px 20px 30px"
           bg={colors.gray[800]}
           verticalAlign="middle"
         >
-          <Box pt="5px">
-            <AccountSmallTile pkh={account.address.pkh} />
-          </Box>
-          <Box justifyContent="space-between" verticalAlign="middle">
-            <Text color={colors.gray[400]} size="sm" display="inline-block">
-              {pluralize("transaction", operations.length, true)}
-            </Text>
-            <Button size="sm" variant="primary" ml="30px">
-              {headerText(operationsType, "batch")}
-            </Button>
-            <IconButton aria-label="remove-batch" ml="18px" size="sm" icon={<BsTrash />} />
-          </Box>
+          <RightHeader operations={accountOperations} />
         </Flex>
-        <TableContainer overflowX="unset" overflowY="unset">
-          <Table>
-            <Tbody>
-              {operations.map((operation, i) => (
-                // TODO: add better key for operations
-                // If you add two 1-tez transfers to the same recipient, the key will be the same
-                // `i` should not be used in the key
-                <Tr key={operation.type + i}>
-                  <Td>{operation.type !== "delegation" ? "Transaction" : operation.type}</Td>
-                  <Td>{renderAmount(operation, getToken)}</Td>
-                  <Td>
-                    {(operation.type === "fa2" || operation.type === "fa1.2") && (
-                      <IconAndTextBtnLink
-                        label={formatPkh(operation.contract.pkh)}
-                        icon={FiExternalLink}
-                        href={buildTzktAddressUrl(network, operation.contract.pkh)}
-                        textFirst
-                      />
-                    )}
-                  </Td>
-                  <Td>
-                    {"recipient" in operation && <AddressPill address={operation.recipient} />}
-                  </Td>
-                </Tr>
-              ))}
-            </Tbody>
-          </Table>
-        </TableContainer>
-        {operations.length > 10 && (
-          <Flex
-            justifyContent="end"
-            borderRadius="0 0 8px 8px"
-            p="20px 23px 20px 30px"
-            bg={colors.gray[800]}
-            verticalAlign="middle"
-          >
-            <RightHeader operations={accountOperations} />
-          </Flex>
-        )}
-      </Box>
-    </Flex>
+      )}
+    </Box>
   );
 };
