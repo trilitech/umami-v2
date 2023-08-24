5567e70e48ddeb284b75cfc7e58773f9ba92395a
Leo Unoki <leo.unoki@trili.tech>
2023-08-24 14:56:01 +0100

    Implement password input component

---
+++ src/assets/icons/Eye.tsx
@@ -0,0 +1,31 @@
+import { Icon, IconProps } from "@chakra-ui/react";
+
+const EyeIcon: React.FC<IconProps> = props => {
+  return (
+    <Icon
+      width="16"
+      height="12"
+      viewBox="0 0 16 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M10.2501 6C10.2501 7.24264 9.2427 8.25 8.00006 8.25C6.75742 8.25 5.75006 7.24264 5.75006 6C5.75006 4.75736 6.75742 3.75 8.00006 3.75C9.2427 3.75 10.2501 4.75736 10.2501 6Z"
+        stroke="#707070"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.00039 0.75C4.64217 0.75 1.79946 2.95716 0.84375 5.99997C1.79944 9.04282 4.64217 11.25 8.00041 11.25C11.3586 11.25 14.2013 9.04284 15.157 6.00003C14.2014 2.95719 11.3586 0.75 8.00039 0.75Z"
+        stroke="#707070"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Icon>
+  );
+};
+
+export default EyeIcon;
---
+++ src/components/PasswordInput.tsx
@@ -0,0 +1,60 @@
+// <T extends FieldValues> is needed to be compatible with the useForm's type parameter (FormData)
+
+import {
+  Button,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputProps,
+  InputRightElement,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { FieldValues, Path, RegisterOptions, useFormContext } from "react-hook-form";
+import EyeSlashIcon from "../assets/icons/EyeSlash";
+import EyeIcon from "../assets/icons/Eye";
+const MIN_LENGTH = 8;
+// <U extends Path<T>> makes sure that we can pass in only valid inputName that exists in FormData
+export type PasswordInputProps<T extends FieldValues, U extends Path<T>> = {
+  inputName: U;
+  label: string;
+
+  required?: RegisterOptions<T, U>["required"];
+  validate?: RegisterOptions<T, U>["validate"];
+} & InputProps;
+const PasswordInput = <T extends FieldValues, U extends Path<T>>({
+  inputName,
+  label,
+  required,
+  validate,
+  ...rest
+}: PasswordInputProps<T, U>) => {
+  const { register } = useFormContext<T>();
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  return (
+    <>
+      <FormLabel>{label}</FormLabel>
+      <InputGroup mt="12px">
+        <Input
+          type={showPassword ? "text" : "password"}
+          autoComplete="off"
+          {...register(inputName, {
+            required,
+            minLength: {
+              value: MIN_LENGTH,
+              message: `Your password must be at least ${MIN_LENGTH} characters long`,
+            },
+            validate,
+          })}
+          {...rest}
+        />
+        <InputRightElement>
+          <Button variant="unstyled" onClick={() => setShowPassword(val => !val)}>
+            {showPassword ? <EyeSlashIcon /> : <EyeIcon w="16.5px" />}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+    </>
+  );
+};
+
+export default PasswordInput;
