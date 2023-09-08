162ce8447233436d0def488609611c69471a7a9a
Leo Unoki <leo.unoki@trili.tech>
2023-09-08 11:09:51 +0100

    Add balance test for AddressTile

--- src/components/AddressTile/AddressTile.test.tsx
+++ src/components/AddressTile/AddressTile.test.tsx
@@ -3,6 +3,7 @@ import { render, screen } from "../../mocks/testUtils";
 import { Address } from "../../types/Address";
 import { formatPkh } from "../../utils/formatPkh";
 import accountsSlice from "../../utils/redux/slices/accountsSlice";
+import { assetsActions } from "../../utils/redux/slices/assetsSlice";
 import store from "../../utils/redux/store";
 import AddressTile from "./AddressTile";
 
@@ -37,6 +38,28 @@ describe("<AddressTileIcon />", () => {
       const account = mockImplicitAccount(0);
       render(fixture(account.address));
       expect(screen.getByText(account.address.pkh)).toBeInTheDocument();
+    });
+  });
+
+  describe("balance", () => {
+    it("hides balance", async () => {
+      const account = mockImplicitAccount(0);
+      render(fixture(account.address));
+      expect(screen.queryByTestId("pretty-number")).not.toBeInTheDocument();
+    });
+
+    it("shows balance if account holds tez", async () => {
+      const account = mockImplicitAccount(0);
+      store.dispatch(accountsSlice.actions.addAccount([account]));
+      store.dispatch(
+        assetsActions.updateTezBalance([
+          { address: mockImplicitAccount(0).address.pkh, balance: 5000000 },
+        ])
+      );
+      render(fixture(account.address));
+      expect(screen.getByTestId("pretty-number")).toBeInTheDocument();
+      expect(screen.getByText("5")).toBeInTheDocument();
+      expect(screen.getByText(".000000 ꜩ")).toBeInTheDocument();
     });
   });
 });
--- src/components/PrettyNumber.tsx
+++ src/components/PrettyNumber.tsx
@@ -8,7 +8,7 @@ const splitNumber = (num: string) => {
 export const PrettyNumber: React.FC<{ number: string }> = ({ number }) => {
   const { integer, decimal } = splitNumber(number);
   return (
-    <Flex alignItems="end">
+    <Flex alignItems="end" data-testid="pretty-number">
       <Heading size="md">{integer}</Heading>
       {decimal && <Heading size="sm">.{decimal}</Heading>}
     </Flex>
