9f4f8e843e2a59d0f79f10e172b268e52937aac7
Sergey Kintsel <sergey.kintsel@trili.tech>
2023-09-15 13:11:56 +0100

    Fix module mapping for tzkt-api in jest settings

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -95,8 +95,3 @@ afterEach(() => {
     store.dispatch(batchesActions.reset());
   });
 });
-
-// Fixes: Cannot find module '@tzkt/oazapfts/runtime' from 'node_modules/@tzkt/sdk-api/build/main/index.js'
-jest.mock("@tzkt/sdk-api", () => {
-  return {};
-});
