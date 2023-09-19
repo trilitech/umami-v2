93779b23f64daac9dee107d3db7b75db0162d5e4
Leo Unoki <leo.unoki@trili.tech>
2023-09-19 18:18:14 +0100

    Fixed ReceiveModal styling and removed useModal

--- src/components/ReceiveModal.tsx
+++ src/components/ReceiveModal.tsx
@@ -1,82 +1,43 @@
-import {
-  Box,
-  Text,
-  Flex,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  useDisclosure,
-  Heading,
-} from "@chakra-ui/react";
-import { FC, useRef } from "react";
-import colors from "../style/colors";
+import { Box, Flex, ModalBody, ModalContent, ModalFooter, Heading } from "@chakra-ui/react";
+import { FC } from "react";
 import { QRCode } from "react-qrcode-logo";
 import AddressPill from "./AddressPill/AddressPill";
 import { parsePkh } from "../types/Address";
 import { useGetOwnedAccountSafe } from "../utils/hooks/accountHooks";
+import FormPageHeader from "./SendFlow/FormPageHeader";
 
-type Options = {
+export const ReceiveModal: FC<{
   pkh: string;
-};
-
-export const useReceiveModal = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const optionsRef = useRef<Options>();
-  const options = optionsRef.current;
-
-  return {
-    modalElement: (
-      <>{options?.pkh && <ReceiveModal pkh={options.pkh} isOpen={isOpen} onClose={onClose} />}</>
-    ),
-    onOpen: (options: Options) => {
-      optionsRef.current = options;
-      onOpen();
-    },
-  };
-};
-
-const ReceiveModal: FC<{
-  pkh: string;
-  isOpen: boolean;
-  onClose: () => void;
-}> = ({ pkh, isOpen, onClose }) => {
+}> = ({ pkh }) => {
   const getOwnedAccount = useGetOwnedAccountSafe();
   const account = getOwnedAccount(pkh);
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
-      <ModalContent bg={colors.gray[900]} p={3}>
-        <ModalHeader textAlign="center">Receive</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          <Flex alignItems="center" direction="column" justifyContent="space-between">
-            <Text size="sm" color={colors.gray[400]} textAlign="center">
-              You can receive tez or other digital assets by scanning or sharing this QR code
-            </Text>
-            <Box mt={5}>
-              <QRCode value={pkh} size={400} />
-            </Box>
-          </Flex>
-        </ModalBody>
-
-        <ModalFooter>
-          <Box w="100%">
-            {account && (
-              <Heading textAlign="center" marginY={2}>
-                {account.label}
-              </Heading>
-            )}
-            <Flex justifyContent="center" w="100%">
-              <AddressPill address={parsePkh(pkh)} mode={{ type: "no_icons" }} />
-            </Flex>
+    <ModalContent>
+      <FormPageHeader
+        title="Receive"
+        subTitle="You can receive tez or other digital assets by scanning or sharing this QR code"
+      />
+      <ModalBody>
+        <Flex alignItems="center" direction="column" justifyContent="space-between">
+          <Box>
+            <QRCode value={pkh} size={232} />
           </Box>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+        </Flex>
+      </ModalBody>
+
+      <ModalFooter>
+        <Box w="100%">
+          {account && (
+            <Heading textAlign="center" marginY={2}>
+              {account.label}
+            </Heading>
+          )}
+          <Flex justifyContent="center" w="100%">
+            <AddressPill address={parsePkh(pkh)} mode={{ type: "no_icons" }} />
+          </Flex>
+        </Box>
+      </ModalFooter>
+    </ModalContent>
   );
 };
 
