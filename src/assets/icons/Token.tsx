300b457e3f5f458e96776a29b80a62d29a539d33
Leo Unoki <leo.unoki@trili.tech>
2023-09-21 10:57:20 +0100

    Import default token icon

---
+++ src/assets/icons/Token.tsx
@@ -0,0 +1,15 @@
+import { Image, ImageProps } from "@chakra-ui/react";
+
+import icon from "../coin-front.svg";
+import { RawPkh } from "../../types/Address";
+
+const TokenIconBase: React.FC<{ url: string } & ImageProps> = ({ url, ...props }) => {
+  return <Image src={url} fallbackSrc={icon} {...props} />;
+};
+
+const TokenIcon: React.FC<{ contract: RawPkh } & ImageProps> = ({ contract, ...props }) => {
+  const url = `https://services.tzkt.io/v1/avatars/${contract}`;
+  return <TokenIconBase url={url} {...props} />;
+};
+
+export default TokenIcon;
