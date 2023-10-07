f06a2f5f43b278e8cde56526fcd32de80f238298
Sergey Kintsel <sergey.kintsel@trili.tech>
2023-10-07 22:23:07 +0100

    Update nfts drawer tab to match design

--- src/components/AccountDrawer/AssetsPanel/NFTsGrid.tsx
+++ src/components/AccountDrawer/AssetsPanel/NFTsGrid.tsx
@@ -1,17 +1,25 @@
-import { AspectRatio, Heading, Image, SimpleGrid, SimpleGridProps } from "@chakra-ui/react";
+import {
+  AspectRatio,
+  Card,
+  CardBody,
+  Image,
+  SimpleGrid,
+  SimpleGridProps,
+  Text,
+} from "@chakra-ui/react";
 import { every } from "lodash";
 import { FC } from "react";
 import { Link } from "react-router-dom";
 import { RawPkh } from "../../../types/Address";
 import { fullId, thumbnailUri } from "../../../types/Token";
 import { NFTBalance } from "../../../types/TokenBalance";
-import { truncate } from "../../../utils/format";
 import { getIPFSurl } from "../../../utils/token/nftUtils";
 import { NoNFTs } from "../../NoItems";
+import colors from "../../../style/colors";
 
 export const NFTsGrid: FC<
-  { nftsByOwner: Record<RawPkh, NFTBalance[] | undefined>; showName?: boolean } & SimpleGridProps
-> = ({ nftsByOwner, showName, ...rest }) => {
+  { nftsByOwner: Record<RawPkh, NFTBalance[] | undefined> } & SimpleGridProps
+> = ({ nftsByOwner, ...rest }) => {
   const noNfts = every(nftsByOwner, nfts => !nfts || nfts.length === 0);
   if (noNfts) {
     return <NoNFTs small />;
@@ -25,14 +33,30 @@ export const NFTsGrid: FC<
           const fallbackUrl = getIPFSurl(nft.displayUri);
           return (
             <Link to={`/nfts/${owner}/${fullId(nft)}`} key={`${owner}:${fullId(nft)}`}>
-              <AspectRatio width="100%" ratio={1}>
-                <Image width="100%" height={40} src={url} fallbackSrc={fallbackUrl} />
-              </AspectRatio>
-              {showName && nft.metadata.name && (
-                <Heading size="sm" mt={3}>
-                  {truncate(nft.metadata.name, 12)}
-                </Heading>
-              )}
+              <Card bg={colors.gray[800]}>
+                <CardBody p="8px">
+                  <AspectRatio width="100%" ratio={1}>
+                    <Image width="100%" height={40} src={url} fallbackSrc={fallbackUrl} />
+                  </AspectRatio>
+                  {/* TODO: make a separate component to be shared between this and the drawer NFT card */}
+                  {Number(nft.balance) > 1 && (
+                    <Text
+                      data-testid="nft-owned-count"
+                      borderRadius="100px"
+                      padding="0 8px"
+                      height="20px"
+                      size="xs"
+                      backgroundColor="rgba(33, 33, 33, 0.75)"
+                      display="inline"
+                      position="absolute"
+                      marginTop="-24px"
+                      marginLeft="4px"
+                    >
+                      {"x" + nft.balance}
+                    </Text>
+                  )}
+                </CardBody>
+              </Card>
             </Link>
           );
         });
