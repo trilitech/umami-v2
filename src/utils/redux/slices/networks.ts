8a4a3059f417e66b71b90bdbc773e2d14d6bc059
Sergey Kintsel <sergey.kintsel@trili.tech>
2023-10-30 13:50:53 +0000

    Add upsert & remove actions to network slice

--- src/utils/redux/slices/networks.ts
+++ src/utils/redux/slices/networks.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { DefaultNetworks, MAINNET, Network } from "../../../types/Network";
+import { remove } from "lodash";
 
 type State = {
   available: Network[];
@@ -11,14 +12,33 @@ const initialState: State = {
   current: MAINNET,
 };
 
+const isDefault = (network: Network) => DefaultNetworks.map(n => n.name).includes(network.name)
+
 export const networksSlice = createSlice({
   name: "networks",
   initialState,
   reducers: {
     reset: () => initialState,
-    setCurrent: (state, { payload }: { payload: Network }) => {
-      state.current = payload;
+    setCurrent: (state, { payload: network }: { payload: Network }) => {
+      state.current = network;
     },
+    upsertNetwork: (state, { payload: network }: { payload: Network }) => {
+      if (isDefault(network)) {
+        return;
+      }
+      const index = state.available.findIndex(n => n.name === network.name);
+      if (index !== -1) {
+        state.available[index] = network;
+        return;
+      }
+      state.available.push(network);
+    },
+    removeNetwork: (state, { payload: network }: { payload: Network }) => {
+      if (isDefault(network)) {
+        return;
+      }
+      state.available = remove(state.available, n => n.name !== network.name);
+    }
   },
 });
 export const networksActions = networksSlice.actions;
