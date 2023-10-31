0c19f4c704d23f9b6cac19f7e3e2c03f89dd5b9f
Leo Unoki <leo.unoki@trili.tech>
2023-10-31 16:04:07 +0000

    Open modal on remove account

--- src/components/AccountDrawer/RenameRemoveMenuSwitch.tsx
+++ src/components/AccountDrawer/RenameRemoveMenuSwitch.tsx
@@ -3,9 +3,13 @@ import { Account, AccountType } from "../../types/Account";
 import RenameRemoveMenu from "../RenameRemoveMenu";
 import { DynamicModalContext } from "../DynamicModal";
 import { RenameAccountModal } from "./RenameAccountModal";
+import { ConfirmationModal } from "../ConfirmationModal";
+import { useAppDispatch } from "../../utils/redux/hooks";
+import accountsSlice from "../../utils/redux/slices/accountsSlice";
 
 const RenameRemoveMenuSwitch: React.FC<{ account: Account }> = ({ account }) => {
-  const { openWith } = useContext(DynamicModalContext);
+  const { openWith, onClose: onCloseModal } = useContext(DynamicModalContext);
+  const dispatch = useAppDispatch();
   switch (account.type) {
     case AccountType.MNEMONIC:
     case AccountType.MULTISIG:
@@ -24,7 +28,18 @@ const RenameRemoveMenuSwitch: React.FC<{ account: Account }> = ({ account }) => 
             openWith(<RenameAccountModal account={account} />);
           }}
           onRemove={() => {
-            // TODO: Remove ledger/social account individually
+            openWith(
+              <ConfirmationModal
+                title="Delete Account"
+                buttonLabel="Delete Account"
+                description="Are you sure you want to remove this account?"
+                onSubmit={() => {
+                  dispatch(accountsSlice.actions.removeAccount(account));
+                  onCloseModal();
+                  // TODO: close drawer
+                }}
+              />
+            );
           }}
         />
       );
