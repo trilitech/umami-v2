0ec5fe153a63c4e0e36e68a1c2570a8378428db8
Leo Unoki <leo.unoki@trili.tech>
2023-10-31 15:22:35 +0000

    Improve Timestamp component UI

--- src/components/OperationTile/Timestamp.tsx
+++ src/components/OperationTile/Timestamp.tsx
@@ -1,15 +1,38 @@
 import { Text } from "@chakra-ui/react";
-import { formatRelative } from "date-fns";
+import { differenceInDays, differenceInHours, differenceInMinutes, format } from "date-fns";
 import colors from "../../style/colors";
 
 export const Timestamp: React.FC<{ timestamp: string | undefined }> = ({ timestamp }) => {
   if (!timestamp) {
     return null;
   }
-  const relativeTimestamp = formatRelative(new Date(timestamp as string), new Date());
+
   return (
     <Text data-testid="timestamp" size="sm" color={colors.gray[400]}>
-      {relativeTimestamp}
+      {getDisplayTimestamp(timestamp)}
     </Text>
   );
 };
+
+// Display the time difference in minutes if it’s less than an hour, 
+// in hours if it’s less than a day, 
+// and in days if it’s less than two days. 
+// Otherwise, it will display the date in the dd MMM yyyy format
+export const getDisplayTimestamp = (timestamp:string): string  => {
+  const currentDate = new Date();
+  const timestampDate = new Date(timestamp as string);
+
+  const minuteDifference = differenceInMinutes(currentDate, timestampDate);
+  const hourDifference = differenceInHours(currentDate, timestampDate);
+  const dayDifference = differenceInDays(currentDate, timestampDate);
+
+  if (minuteDifference < 60) {
+    return `${minuteDifference} minutes ago`;
+  } else if (hourDifference < 24) {
+    return `${hourDifference} hours ago`;
+  } else if (dayDifference < 2) {
+    return `${dayDifference} days ago`;
+  } else {
+    return format(timestampDate, 'dd MMM yyyy');
+  }
+}
