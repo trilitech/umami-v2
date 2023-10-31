b1aa6a91b083873328783d62455fc2f965a1879f
Leo Unoki <leo.unoki@trili.tech>
2023-10-31 11:53:55 +0000

    Implement rename for multisig slice

--- src/utils/redux/slices/multisigsSlice.test.ts
+++ src/utils/redux/slices/multisigsSlice.test.ts
@@ -1,67 +1,125 @@
 import { multisigOperation, multisigs } from "../../../mocks/multisig";
+import { AccountType } from "../../../types/Account";
 import store from "../store";
 import { multisigActions } from "./multisigsSlice";
+import { mockContractAddress, mockMultisigAccount } from "../../../mocks/factories";
+import { Multisig } from "../../multisig/types";
 
-describe("Contacts reducer", () => {
-  test("store should initialize with empty state", () => {
-    expect(store.getState().multisigs).toEqual({ items: [], pendingOperations: {} });
+describe("Multisig reducer", () => {
+  describe("default values", () => {
+    it("store should initialize with empty state", () => {
+      expect(store.getState().multisigs).toEqual({ items: [], pendingOperations: {} });
+    });
   });
 
-  test("should set new multisigs", () => {
-    const multisig = multisigs[0];
-    store.dispatch(multisigActions.setMultisigs([multisig]));
-    expect(store.getState().multisigs).toEqual({ items: [multisig], pendingOperations: {} });
+  describe("setMultisigs", () => {
+    it("should set new multisigs", () => {
+      store.dispatch(multisigActions.setMultisigs(multisigs));
+      expect(store.getState().multisigs).toEqual({
+        items: multisigs.map((m, i) => ({
+          ...m,
+          label: `Multisig Account ${i}`,
+          type: AccountType.MULTISIG,
+        })),
+        pendingOperations: {},
+      });
+    });
+
+    it("should set default label for new multisigs", () => {
+      const oldMultisigs = [multisigs[0], multisigs[1]];
+      const newMultisigs = multisigs;
+      store.dispatch(multisigActions.setMultisigs(oldMultisigs));
+      store.dispatch(multisigActions.setMultisigs(newMultisigs));
+      expect(store.getState().multisigs).toEqual({
+        items: newMultisigs.map((m, i) => ({
+          ...m,
+          label: `Multisig Account ${i}`,
+          type: AccountType.MULTISIG,
+        })),
+        pendingOperations: {},
+      });
+    });
   });
 
-  test("should build mapping for multisig operations", () => {
-    const operation1 = multisigOperation;
-    const operation2 = { ...multisigOperation, id: "2" };
-    const operation3 = { ...multisigOperation, bigmapId: 1 };
-    store.dispatch(multisigActions.setPendingOperations([operation1, operation2, operation3]));
-    expect(store.getState().multisigs).toEqual({
-      items: [],
-      pendingOperations: {
-        "0": [
-          {
-            approvals: [
-              {
-                pkh: "pkh",
-                type: "implicit",
-              },
-            ],
-            bigmapId: 0,
-            id: "1",
-            rawActions:
-              '[{"prim":"DROP"},{"prim":"NIL","args":[{"prim":"operation"}]},{"prim":"PUSH","args":[{"prim":"key_hash"},{"bytes":"005fd0a7ece135cecfd71fcf78cf6656d5047fb980"}]},{"prim":"IMPLICIT_ACCOUNT"},{"prim":"PUSH","args":[{"prim":"mutez"},{"int":"100000"}]},{"prim":"UNIT"},{"prim":"TRANSFER_TOKENS"},{"prim":"CONS"}]',
-          },
-          {
-            approvals: [
-              {
-                pkh: "pkh",
-                type: "implicit",
-              },
-            ],
-            bigmapId: 0,
-            id: "2",
-            rawActions:
-              '[{"prim":"DROP"},{"prim":"NIL","args":[{"prim":"operation"}]},{"prim":"PUSH","args":[{"prim":"key_hash"},{"bytes":"005fd0a7ece135cecfd71fcf78cf6656d5047fb980"}]},{"prim":"IMPLICIT_ACCOUNT"},{"prim":"PUSH","args":[{"prim":"mutez"},{"int":"100000"}]},{"prim":"UNIT"},{"prim":"TRANSFER_TOKENS"},{"prim":"CONS"}]',
-          },
-        ],
-        "1": [
-          {
-            approvals: [
-              {
-                pkh: "pkh",
-                type: "implicit",
-              },
-            ],
-            bigmapId: 1,
-            id: "1",
-            rawActions:
-              '[{"prim":"DROP"},{"prim":"NIL","args":[{"prim":"operation"}]},{"prim":"PUSH","args":[{"prim":"key_hash"},{"bytes":"005fd0a7ece135cecfd71fcf78cf6656d5047fb980"}]},{"prim":"IMPLICIT_ACCOUNT"},{"prim":"PUSH","args":[{"prim":"mutez"},{"int":"100000"}]},{"prim":"UNIT"},{"prim":"TRANSFER_TOKENS"},{"prim":"CONS"}]',
-          },
-        ],
-      },
+  describe("setName", () => {
+    it("should not do anything if account does not exist", () => {
+      const account = mockMultisigAccount(0);
+      store.dispatch(multisigActions.setMultisigs([account as Multisig]));
+      store.dispatch(
+        multisigActions.setName({
+          newName: "new name",
+          account: { ...account, address: mockContractAddress(2) },
+        })
+      );
+
+      expect(store.getState().multisigs.items).toEqual([account]);
+    });
+
+    it("should update the label", () => {
+      const account = mockMultisigAccount(0);
+      store.dispatch(multisigActions.setMultisigs([account as Multisig]));
+      store.dispatch(
+        multisigActions.setName({
+          newName: "new name",
+          account,
+        })
+      );
+
+      expect(store.getState().multisigs.items).toEqual([{ ...account, label: "new name" }]);
+    });
+  });
+
+  describe("setPendingOperations", () => {
+    it("should build mapping for multisig operations", () => {
+      const operation1 = multisigOperation;
+      const operation2 = { ...multisigOperation, id: "2" };
+      const operation3 = { ...multisigOperation, bigmapId: 1 };
+      store.dispatch(multisigActions.setPendingOperations([operation1, operation2, operation3]));
+      expect(store.getState().multisigs).toEqual({
+        items: [],
+        pendingOperations: {
+          "0": [
+            {
+              approvals: [
+                {
+                  pkh: "pkh",
+                  type: "implicit",
+                },
+              ],
+              bigmapId: 0,
+              id: "1",
+              rawActions:
+                '[{"prim":"DROP"},{"prim":"NIL","args":[{"prim":"operation"}]},{"prim":"PUSH","args":[{"prim":"key_hash"},{"bytes":"005fd0a7ece135cecfd71fcf78cf6656d5047fb980"}]},{"prim":"IMPLICIT_ACCOUNT"},{"prim":"PUSH","args":[{"prim":"mutez"},{"int":"100000"}]},{"prim":"UNIT"},{"prim":"TRANSFER_TOKENS"},{"prim":"CONS"}]',
+            },
+            {
+              approvals: [
+                {
+                  pkh: "pkh",
+                  type: "implicit",
+                },
+              ],
+              bigmapId: 0,
+              id: "2",
+              rawActions:
+                '[{"prim":"DROP"},{"prim":"NIL","args":[{"prim":"operation"}]},{"prim":"PUSH","args":[{"prim":"key_hash"},{"bytes":"005fd0a7ece135cecfd71fcf78cf6656d5047fb980"}]},{"prim":"IMPLICIT_ACCOUNT"},{"prim":"PUSH","args":[{"prim":"mutez"},{"int":"100000"}]},{"prim":"UNIT"},{"prim":"TRANSFER_TOKENS"},{"prim":"CONS"}]',
+            },
+          ],
+          "1": [
+            {
+              approvals: [
+                {
+                  pkh: "pkh",
+                  type: "implicit",
+                },
+              ],
+              bigmapId: 1,
+              id: "1",
+              rawActions:
+                '[{"prim":"DROP"},{"prim":"NIL","args":[{"prim":"operation"}]},{"prim":"PUSH","args":[{"prim":"key_hash"},{"bytes":"005fd0a7ece135cecfd71fcf78cf6656d5047fb980"}]},{"prim":"IMPLICIT_ACCOUNT"},{"prim":"PUSH","args":[{"prim":"mutez"},{"int":"100000"}]},{"prim":"UNIT"},{"prim":"TRANSFER_TOKENS"},{"prim":"CONS"}]',
+            },
+          ],
+        },
+      });
     });
   });
 });
--- src/utils/redux/slices/multisigsSlice.ts
+++ src/utils/redux/slices/multisigsSlice.ts
@@ -1,9 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { groupBy } from "lodash";
 import { Multisig, MultisigOperation, MultisigPendingOperations } from "../../multisig/types";
+import { RawPkh } from "../../../types/Address";
+import { AccountType, MultisigAccount } from "../../../types/Account";
 
 export type State = {
-  items: Multisig[];
+  items: MultisigAccount[];
   pendingOperations: MultisigPendingOperations;
 };
 
@@ -15,10 +17,36 @@ const multisigsSlice = createSlice({
   reducers: {
     reset: () => initialState,
     setMultisigs: (state, { payload }: { payload: Multisig[] }) => {
-      state.items = payload;
+      const multisigLabels = (state.items as MultisigAccount[]).reduce(
+        (acc: Record<RawPkh, string | undefined>, multisig) => {
+          acc[multisig.address.pkh] = multisig.label;
+          return acc;
+        },
+        {}
+      );
+
+      state.items = payload.map((multisig, i) => ({
+        ...multisig,
+        label: multisigLabels[multisig.address.pkh] || `Multisig Account ${i}`,
+        type: AccountType.MULTISIG,
+      }));
     },
     setPendingOperations: (state, { payload }: { payload: MultisigOperation[] }) => {
       state.pendingOperations = groupBy(payload, operation => operation.bigmapId);
+    },
+    // Do not call this directly, use the RenameAccount thunk
+    setName: (state, { payload }: { payload: { newName: string; account: MultisigAccount } }) => {
+      const {
+        account: {
+          address: { pkh },
+        },
+        newName,
+      } = payload;
+
+      const position = state.items.findIndex(multisig => multisig.address.pkh === pkh);
+      if (position >= 0) {
+        state.items[position] = { ...state.items[position], label: newName };
+      }
     },
   },
 });
