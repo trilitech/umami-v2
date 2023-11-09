ac3f904024ef2610a6575ee99a20600bc32ce3f0
Leo Unoki <leo.unoki@trili.tech>
2023-11-09 14:48:24 +0000

    Add error messages to estimate

--- src/utils/tezos/estimate.ts
+++ src/utils/tezos/estimate.ts
@@ -8,13 +8,31 @@ export const estimate = async (
   network: Network
 ): Promise<BigNumber> => {
   const tezosToolkit = await makeToolkit({ type: "fake", signer: operations.signer, network });
+  try {
+    const estimations = await tezosToolkit.estimate.batch(operationsToBatchParams(operations));
+    // The way taquito works we need to take the max of suggestedFeeMutez and totalCost
+    // because the suggestedFeeMutez does not include the storage & execution cost
+    // and in these cases the totalCost is the one to go (so, for contract calls)
+    // though totalCost doesn't work well with simple tez transfers and suggestedFeeMutez is more accurate
+    return sumTez(
+      estimations.map(estimate =>
+        Math.max(estimate.suggestedFeeMutez, estimate.totalCost).toString()
+      )
+    );
+  } catch (err: any) {
+    throw new Error(handleTezError(err));
+  }
+};
 
-  const estimations = await tezosToolkit.estimate.batch(operationsToBatchParams(operations));
-  // The way taquito works we need to take the max of suggestedFeeMutez and totalCost
-  // because the suggestedFeeMutez does not include the storage & execution cost
-  // and in these cases the totalCost is the one to go (so, for contract calls)
-  // though totalCost doesn't work well with simple tez transfers and suggestedFeeMutez is more accurate
-  return sumTez(
-    estimations.map(estimate => Math.max(estimate.suggestedFeeMutez, estimate.totalCost).toString())
-  );
+// Converts a known L1 error message to a more user-friendly one
+const handleTezError = (err: any): string | any => {
+  let message = "";
+
+  if (err.message.includes("subtraction_underflow")) {
+    message = "Insufficient balance, please make sure you have enough funds.";
+  } else if (err.message.includes("contract.non_existing_contract")) {
+    message = "Contract does not exist, please check if the correct network is selected.";
+  }
+
+  return message ? message : err;
 };
