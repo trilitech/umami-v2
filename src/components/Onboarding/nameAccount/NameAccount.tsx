65536e9ff6cbed9bd70153c2d5b25531faf4b62f
Ana Maksimovskikh <ana.maksimovskikh@trili.tech>
2023-11-15 13:08:22 +0000

    Update NameAccount component.

--- src/components/Onboarding/nameAccount/NameAccount.tsx
+++ src/components/Onboarding/nameAccount/NameAccount.tsx
@@ -2,6 +2,18 @@ import { NameAccountStep, Step, StepType } from "../useOnboardingModal";
 import { useImplicitAccounts } from "../../../utils/hooks/accountHooks";
 import NameAccountDisplay from "./NameAccountDisplay";
 
+/**
+ * This component is used to add a label to ledger account or to mnemonic account group.
+ *
+ * NameAccount is only used when adding a mnemonic account group ar a ledger account,
+ * which could be either never used or used before.
+ *
+ * If the label is not provided by the user, the default name will be used.
+ *
+ * @goToStep - function to go to the next step.
+ * @account - account to be named.
+ * @returns NameAccount component to be rendered.
+ */
 export const NameAccount = ({
   goToStep,
   account,
@@ -18,21 +30,23 @@ export const NameAccount = ({
       case "secret_key":
         return goToStep({ type: StepType.masterPassword, account: { ...account, label: label } });
       case "ledger":
+        // Ledger account label should be unique among all other account labels / contact names.
         if (label.length === 0) {
           const usedLedgerLabels = accounts.map(account => account.label);
           label = firstUnusedIndexedLabel("Ledger Account", usedLedgerLabels);
         }
-        break;
+        return goToStep({ type: StepType.derivationPath, account: { ...account, label: label } });
       case "mnemonic":
-        if (label.length === 0) {
-          label = `Account ${accounts.length + 1}`;
-        }
-        break;
+        // This label is for the mnemonic account group, separate accounts will be named in {link}.
+        label = label.length > 0 ? label : `Restored Mnemonic Account`;
+        return goToStep({ type: StepType.derivationPath, account: { ...account, label: label } });
     }
 
-    return goToStep({ type: StepType.derivationPath, account: { ...account, label: label } });
+    // TODO: throw an error if the account type is not supported
   };
 
+  // TODO: Is the subtitle correct? Should we use a different one for mnemonics?
+  // TODO: maybe split name account step into name ledger & name mnemonic?
   return (
     <NameAccountDisplay
       subtitle="Please choose a name for your first account. You can edit your account name later."
