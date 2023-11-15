606f8726c239749f94693d1e1c85d84ee10aead8
Leo <leo.unoki@trili.tech>
2023-11-15 09:30:55 +0000

    Show NFT on AccountListWithDrawer

--- src/views/home/AccountListWithDrawer.tsx
+++ src/views/home/AccountListWithDrawer.tsx
@@ -1,22 +1,38 @@
 import { useDisclosure } from "@chakra-ui/hooks";
-import { Drawer, DrawerBody, DrawerContent, DrawerOverlay } from "@chakra-ui/react";
+import { Drawer, DrawerBody, DrawerContent, DrawerOverlay, Flex } from "@chakra-ui/react";
 import { useCallback, useEffect, useState } from "react";
 import AccountCard from "../../components/AccountDrawer";
 import { useAllAccounts } from "../../utils/hooks/getAccountDataHooks";
 import { AccountsList } from "./AccountsList";
-import { DrawerTopButtons } from "./DrawerTopButtons";
+import { CloseDrawerButton, DrawerTopButtons } from "./DrawerTopButtons";
 import { useDynamicModal } from "../../components/DynamicModal";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAllNfts } from "../../utils/hooks/assetsHooks";
+import { fullId } from "../../types/Token";
+import colors from "../../style/colors";
+import AddressPill from "../../components/AddressPill/AddressPill";
+import NFTDrawerCard from "../nfts/NFTDrawerCard";
+import { parsePkh } from "../../types/Address";
 
 const AccountListWithDrawer: React.FC = () => {
   const [selected, setSelected] = useState<string | null>(null);
   const allAccounts = useAllAccounts();
 
-  const { isOpen, onClose, onOpen } = useDisclosure();
+  const { ownerPkh, nftId } = useParams();
+  const isNFT = ownerPkh !== undefined && nftId !== undefined;
+  const nfts = useAllNfts();
+  const drawerNFT = ownerPkh && (nfts[ownerPkh] || []).find(nft => fullId(nft) === nftId);
+
+  const { isOpen, onClose, onOpen } = useDisclosure({ defaultIsOpen: isNFT });
   const { isOpen: isDynamicModalOpen } = useDynamicModal();
 
+  const navigate = useNavigate();
   const closeDrawer = useCallback(() => {
     setSelected(null);
     onClose();
+    if (!isNFT) {
+      navigate("/home");
+    }
   }, [setSelected, onClose]);
 
   // For some reason the drawer doesn't close on esc for this particular component
@@ -45,8 +61,30 @@ const AccountListWithDrawer: React.FC = () => {
         <DrawerOverlay />
         <DrawerContent>
           <DrawerBody>
-            <DrawerTopButtons onClose={closeDrawer} />
-            {account && <AccountCard account={account} />}
+            {isNFT ? (
+              <>
+                {drawerNFT && (
+                  <>
+                    <Flex
+                      justifyContent="space-between"
+                      color={colors.gray[400]}
+                      cursor="pointer"
+                      alignItems="center"
+                      paddingBottom="30px"
+                    >
+                      <AddressPill address={parsePkh(ownerPkh)} />
+                      <CloseDrawerButton onClose={closeDrawer} />
+                    </Flex>
+                    <NFTDrawerCard nft={drawerNFT} ownerPkh={ownerPkh} />
+                  </>
+                )}
+              </>
+            ) : (
+              <>
+                <DrawerTopButtons onClose={closeDrawer} />
+                {account && <AccountCard account={account} />}
+              </>
+            )}
           </DrawerBody>
         </DrawerContent>
       </Drawer>
