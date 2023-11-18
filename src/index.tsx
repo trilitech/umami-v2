b3b0fd5021664ee25cc90adeaea56e6eb418e455
Sergey Kintsel <sergey.kintsel@trili.tech>
2023-11-18 22:31:21 +0000

    Remove web vitals as they are not used

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import Router from "./Router";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
@@ -35,8 +34,3 @@ root.render(
     </UmamiTheme>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
