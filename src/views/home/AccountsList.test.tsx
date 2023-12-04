0c373006e1c0b13d4a196a908b5f46fa78e06084
Leo <leo.unoki@trili.tech>
2023-12-04 12:09:30 +0000

    Add tests for AccountGroup

--- src/views/home/AccountsList.test.tsx
+++ src/views/home/AccountsList.test.tsx
@@ -7,6 +7,7 @@ import {
   mockMnemonicAccount,
   mockMultisigWithOperations,
   mockPk,
+  mockSocialAccount,
 } from "../../mocks/factories";
 import { fakeExtraArguments } from "../../mocks/fakeExtraArgument";
 import { fakeRestoreFromMnemonic } from "../../mocks/helpers";
@@ -25,6 +26,44 @@ const MOCK_FINGETPRINT1 = "mockFin1";
 const MOCK_FINGETPRINT2 = "mockFin2";
 
 describe("<AccountList />", () => {
+  describe("<AccountGroup/>", () => {
+    it("shows removal message", () => {
+      const mnemonic = mockMnemonicAccount(0);
+      const social = mockSocialAccount(1);
+      store.dispatch(accountsSlice.actions.addMockMnemonicAccounts([mnemonic]));
+      store.dispatch(accountsSlice.actions.addAccount(social));
+      render(<AccountsList onOpen={() => {}} onSelect={() => {}} selected={null} />);
+
+      const [mnemonicPopover, socialPopover] = screen.getAllByTestId("popover-cta");
+      const [removeMnemonic, removeSocial] = screen.getAllByTestId("popover-remove");
+
+      fireEvent.click(mnemonicPopover);
+      fireEvent.click(removeMnemonic);
+      expect(screen.getByTestId("description")).toHaveTextContent(
+        "Are you sure you want to remove all accounts derived from Seedphrase mockPrint?"
+      );
+
+      fireEvent.click(socialPopover);
+      fireEvent.click(removeSocial);
+      expect(screen.getByTestId("description")).toHaveTextContent(
+        "Are you sure you want to remove all of your Social Accounts?"
+      );
+    });
+
+    it("shows offboarding message for last accounts", () => {
+      const social1 = mockSocialAccount(0);
+      const social2 = mockSocialAccount(1);
+      store.dispatch(accountsSlice.actions.addAccount(social1));
+      store.dispatch(accountsSlice.actions.addAccount(social2));
+      render(<AccountsList onOpen={() => {}} onSelect={() => {}} selected={null} />);
+
+      fireEvent.click(screen.getByTestId("popover-cta"));
+      fireEvent.click(screen.getByTestId("popover-remove"));
+      expect(screen.getByTestId("description")).toHaveTextContent(
+        "Removing your last account will off-board your from Umami. This will remove or reset all customised settings to their defaults. Personal data -including saved contacts, password and accounts- won't be affected."
+      );
+    });
+  });
   test("Displays accounts in store with label and formated pkh", async () => {
     store.dispatch(
       addMockMnemonicAccounts([
