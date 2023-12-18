06312377647b8f4121abde3537d887c4bb8ab032
Ana Maksimovskikh <ana.maksimovskikh@trili.tech>
2023-12-18 21:08:11 +0000

    Get rid of updating path on opening/closing NFT Drawer

--- src/views/nfts/NftsView.tsx
+++ src/views/nfts/NftsView.tsx
@@ -1,38 +1,35 @@
 import { Box, Drawer, DrawerBody, DrawerContent, DrawerOverlay, Flex } from "@chakra-ui/react";
-import { every, get, pick, sumBy } from "lodash";
-import { useCallback, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { every, pick, sumBy } from "lodash";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 import { NFTDrawerBody } from "./NFTDrawerBody";
-import { NFTGallery } from "./NFTGallery";
+import { NFTGallery, NFTWithOwner } from "./NFTGallery";
 import { useDynamicModal } from "../../components/DynamicModal";
 import { NoNFTs } from "../../components/NoItems";
 import { TopBar } from "../../components/TopBar";
 import { useAccountsFilter } from "../../components/useAccountsFilter";
-import { fullId } from "../../types/Token";
 import { useAllNfts } from "../../utils/hooks/assetsHooks";
 
 export const NFTsView = () => {
   const nfts = useAllNfts();
-  const { accountsFilter, selectedAccounts } = useAccountsFilter();
-  const navigate = useNavigate();
-  const { ownerPkh, nftId } = useParams();
-  const { isOpen: isDynamicModalOpen } = useDynamicModal();
 
-  const openNFTsPage = useCallback(() => {
-    navigate("/nfts");
-  }, [navigate]);
+  const { ownerPkh } = useParams();
+  const { accountsFilter, selectedAccounts } = useAccountsFilter(ownerPkh ? [ownerPkh] : undefined);
+
+  const { isOpen: isDynamicModalOpen } = useDynamicModal();
+  const [drawerNFT, setDrawerNFT] = useState<NFTWithOwner | undefined>(undefined);
 
   useEffect(() => {
     const onEscape = (event: KeyboardEvent) => {
       if (event.key === "Escape") {
-        openNFTsPage();
+        setDrawerNFT(undefined);
       }
     };
     document.addEventListener("keydown", onEscape);
 
     return () => document.removeEventListener("keydown", onEscape);
-  }, [openNFTsPage]);
+  }, []);
 
   const selectedNFTs = pick(
     nfts,
@@ -41,7 +38,6 @@ export const NFTsView = () => {
   const totalNFTs = sumBy(Object.values(selectedNFTs).flat(), nft => Number(nft?.balance || 0));
 
   const noNFTs = every(selectedNFTs, nfts => !nfts || nfts.length === 0);
-  const drawerNFT = ownerPkh && get(nfts, [ownerPkh], []).find(nft => fullId(nft) === nftId);
 
   return (
     <Flex flexDirection="column" height="100%">
@@ -53,26 +49,25 @@ export const NFTsView = () => {
       ) : (
         <>
           <Box overflowY="scroll">
-            <NFTGallery
-              nftsByOwner={selectedNFTs}
-              onSelect={nft => {
-                navigate(`/nfts/${nft.owner}/${fullId(nft)}`);
-              }}
-            />
+            <NFTGallery nftsByOwner={selectedNFTs} onSelect={nft => setDrawerNFT(nft)} />
           </Box>
 
           <Drawer
             autoFocus={false}
             blockScrollOnMount={!isDynamicModalOpen}
             isOpen={!!drawerNFT}
-            onClose={openNFTsPage}
+            onClose={() => setDrawerNFT(undefined)}
             placement="right"
           >
             <DrawerOverlay />
             <DrawerContent>
               <DrawerBody>
                 {drawerNFT && (
-                  <NFTDrawerBody nft={drawerNFT} onCloseDrawer={openNFTsPage} ownerPkh={ownerPkh} />
+                  <NFTDrawerBody
+                    nft={drawerNFT}
+                    onCloseDrawer={() => setDrawerNFT(undefined)}
+                    ownerPkh={drawerNFT.owner}
+                  />
                 )}
               </DrawerBody>
             </DrawerContent>
