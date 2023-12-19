74022733703127377330054b7769b38843cc69c4
Ana Maksimovskikh <ana.maksimovskikh@trili.tech>
2023-12-19 21:04:13 +0000

    Filter operations by account on opening operations view from account's assets panel

--- src/components/AccountDrawer/AssetsPanel/OperationListDisplay.tsx
+++ src/components/AccountDrawer/AssetsPanel/OperationListDisplay.tsx
@@ -2,6 +2,7 @@ import { Box, Divider } from "@chakra-ui/react";
 import React from "react";
 
 import { ViewAllLink } from "./ViewAllLink";
+import { RawPkh } from "../../../types/Address";
 import { TzktCombinedOperation } from "../../../utils/tezos";
 import { NoOperations } from "../../NoItems";
 import { OperationTile } from "../../OperationTile";
@@ -12,9 +13,10 @@ export const MAX_OPERATIONS_SIZE = 20;
  * Operations list to be displayed in the account drawer
  * Limits the total number of displayed operations to {@link MAX_OPERATIONS_SIZE}
  */
-export const OperationListDisplay: React.FC<{ operations: TzktCombinedOperation[] }> = ({
-  operations,
-}) => {
+export const OperationListDisplay: React.FC<{
+  owner: RawPkh;
+  operations: TzktCombinedOperation[];
+}> = ({ owner, operations }) => {
   if (operations.length === 0) {
     return <NoOperations small />;
   }
@@ -29,7 +31,7 @@ export const OperationListDisplay: React.FC<{ operations: TzktCombinedOperation[
           {i < chunk.length - 1 && <Divider marginY="20px" />}
         </Box>
       ))}
-      <ViewAllLink to="/operations" />
+      <ViewAllLink to={`/operations?accounts=${owner}`} />
     </>
   );
 };
