e5866c6fffa6f9c7bbd934fbc02bb7eee09dc6c0
Leo <leo.unoki@trili.tech>
2024-01-03 16:10:21 +0000

    Display estimate status for operations in batch

--- src/views/batch/BatchView.tsx
+++ src/views/batch/BatchView.tsx
@@ -2,12 +2,13 @@ import { Box, Button, Divider, Flex, IconButton, Text } from "@chakra-ui/react";
 import { compact } from "lodash";
 import { nanoid } from "nanoid";
 import pluralize from "pluralize";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import { AccountSmallTile } from "./AccountSmallTile";
+import { EstimateStatus, getEstimateStatus, getEstimateStatusColor } from "./estimateStatus";
 import { OperationRecipient } from "./OperationRecipient";
 import { OperationView } from "./OperationView";
-import { TrashIcon } from "../../assets/icons";
+import { CheckmarkIcon, ExclamationIcon, TrashIcon, WarningIcon } from "../../assets/icons";
 import { ConfirmationModal } from "../../components/ConfirmationModal";
 import { DynamicModalContext } from "../../components/DynamicModal";
 import { SignPage } from "../../components/SendFlow/Batch/SignPage";
@@ -37,42 +38,25 @@ export const addOrdinal = (n: number): string => {
   return `${n}${ordinal}`;
 };
 
-const RightHeader: React.FC<{ operations: AccountOperations }> = ({
-  operations: accountOperations,
-}) => {
+const RightHeader: React.FC<{
+  operations: AccountOperations;
+  onBatchSubmit: () => Promise<void>;
+  batchSubmitIsLoading: boolean;
+}> = ({ operations: accountOperations, onBatchSubmit, batchSubmitIsLoading }) => {
   const { type: operationsType, sender, operations } = accountOperations;
   const { openWith } = useContext(DynamicModalContext);
-
-  const { handleAsyncAction, isLoading } = useAsyncActionHandler();
-  const network = useSelectedNetwork();
-
-  const openBatchSignPage = () =>
-    handleAsyncAction(async () => {
-      try {
-        const initialFee = await estimate(accountOperations, network);
-        openWith(<SignPage initialFee={initialFee} initialOperations={accountOperations} />);
-      } catch (_) {
-        // In case of error, we identify the specific in the batch.
-        for (let i = 0; i < operations.length; i += 1) {
-          const operation = operations[i];
-          try {
-            await estimate({ ...accountOperations, operations: [operation] }, network);
-          } catch (error: any) {
-            const operationType = prettyOperationType(operation);
-            throw new Error(
-              `The ${addOrdinal(i + 1)} operation "${operationType}" is invalid: ${error.message}`
-            );
-          }
-        }
-      }
-    });
 
   return (
     <Box alignItems="center" justifyContent="space-between" data-testid="right-header">
       <Text display="inline-block" color={colors.gray[400]} size="sm">
         {pluralize("transaction", operations.length, true)}
       </Text>
-      <Button marginLeft="30px" isLoading={isLoading} onClick={openBatchSignPage} variant="primary">
+      <Button
+        marginLeft="30px"
+        isLoading={batchSubmitIsLoading}
+        onClick={onBatchSubmit}
+        variant="primary"
+      >
         {headerText(operationsType, "batch")}
       </Button>
       <IconButton
@@ -130,6 +114,18 @@ export const tokenTitle = (token: Token | undefined, amount: string) => {
   return compact([prettyAmount, symbol, name]).join(" ");
 };
 
+const getEstimateStatusIcon = (status: EstimateStatus) => {
+  const color = getEstimateStatusColor(status);
+  switch (status) {
+    case "Estimated":
+      return <CheckmarkIcon height="14.5px" />;
+    case "Failed":
+      return <WarningIcon width="13px" height="12px" stroke={color} />;
+    case "Not Estimated":
+      return <ExclamationIcon stroke={color} />;
+  }
+};
+
 export const BatchView: React.FC<{
   operations: AccountOperations;
 }> = ({ operations: accountOperations }) => {
@@ -138,11 +134,43 @@ export const BatchView: React.FC<{
 
   const showFooter = operations.length > 9;
 
+  const { openWith } = useContext(DynamicModalContext);
+
+  const { handleAsyncAction, isLoading: batchSubmitIsLoading } = useAsyncActionHandler();
+  const network = useSelectedNetwork();
+
+  // lastEstimatedIndex is the index of the operation that was estimated last.
+  // if the estimatedIndex === operations.length, then the entire batch was estimated successfully.
+  const [lastEstimatedIndex, setLastEstimatedIndex] = useState<null | number>(null);
+
+  const onBatchSubmit = () =>
+    handleAsyncAction(async () => {
+      try {
+        const initialFee = await estimate(accountOperations, network);
+        setLastEstimatedIndex(accountOperations.operations.length);
+        openWith(<SignPage initialFee={initialFee} initialOperations={accountOperations} />);
+      } catch (_) {
+        // In case of error, we identify the specific operation in the batch.
+        for (let i = 0; i < operations.length; i += 1) {
+          const operation = operations[i];
+          try {
+            await estimate({ ...accountOperations, operations: [operation] }, network);
+          } catch (error: any) {
+            const operationType = prettyOperationType(operation);
+            setLastEstimatedIndex(i);
+            throw new Error(
+              `The ${addOrdinal(i + 1)} operation "${operationType}" is invalid: ${error.message}`
+            );
+          }
+        }
+      }
+    });
+
   return (
     <Box width="100%" marginBottom="16px" data-testid={`batch-table-${sender.address.pkh}`}>
       <Flex
         justifyContent="space-between"
-        padding="20px 23px 20px 30px"
+        padding="20px 23px 25px 30px"
         background={colors.gray[800]}
         borderTopRadius="8px"
         data-testid="header"
@@ -150,7 +178,11 @@ export const BatchView: React.FC<{
         <Flex alignItems="center">
           <AccountSmallTile paddingLeft={0} account={sender} />
         </Flex>
-        <RightHeader operations={accountOperations} />
+        <RightHeader
+          batchSubmitIsLoading={batchSubmitIsLoading}
+          onBatchSubmit={onBatchSubmit}
+          operations={accountOperations}
+        />
       </Flex>
       <Flex
         flexDirection="column"
@@ -159,37 +191,60 @@ export const BatchView: React.FC<{
         paddingX="30px"
         paddingY="20px"
       >
-        {operations.map((operation, index) => (
-          <Box key={nanoid()} data-testid="operation">
-            <Flex flexDirection="column" height="50px">
-              <Flex>
-                <OperationView operation={operation} />
-              </Flex>
+        {operations.map((operation, index) => {
+          const estimateStatus =
+            lastEstimatedIndex !== null ? getEstimateStatus(index, lastEstimatedIndex) : null;
+          return (
+            <Box key={nanoid()} data-testid="operation">
+              <Flex flexDirection="column" height={estimateStatus ? "70px" : "50px"}>
+                <Flex>
+                  <OperationView operation={operation} />
+                </Flex>
 
-              <Flex alignItems="center" justifyContent="space-between" width="100%" marginTop="8px">
-                <Flex>
-                  <OperationRecipient operation={operation} />
-                </Flex>
-                <Flex>
-                  <Text alignSelf="flex-end" color={colors.gray[450]} size="sm">
-                    {prettyOperationType(operation)}
-                  </Text>
-                  <IconButton
-                    width="24px"
-                    marginLeft="12px"
-                    borderRadius="full"
-                    aria-label="Remove"
-                    icon={<TrashIcon stroke={colors.gray[300]} />}
-                    onClick={() => removeItem(sender, index)}
-                    size="xs"
-                    variant="circle"
-                  />
+                <Flex
+                  alignItems="center"
+                  justifyContent="space-between"
+                  width="100%"
+                  marginTop="8px"
+                >
+                  <Box>
+                    <Flex>
+                      <OperationRecipient operation={operation} />
+                    </Flex>
+                    {estimateStatus && (
+                      <Flex alignItems="center" marginTop="8px">
+                        {getEstimateStatusIcon(estimateStatus)}
+                        <Text
+                          marginLeft="4px"
+                          color={getEstimateStatusColor(estimateStatus)}
+                          size="xs"
+                        >
+                          {estimateStatus}
+                        </Text>
+                      </Flex>
+                    )}
+                  </Box>
+                  <Flex>
+                    <Text alignSelf="flex-end" color={colors.gray[450]} size="sm">
+                      {prettyOperationType(operation)}
+                    </Text>
+                    <IconButton
+                      width="24px"
+                      marginLeft="12px"
+                      borderRadius="full"
+                      aria-label="Remove"
+                      icon={<TrashIcon stroke={colors.gray[300]} />}
+                      onClick={() => removeItem(sender, index)}
+                      size="xs"
+                      variant="circle"
+                    />
+                  </Flex>
                 </Flex>
               </Flex>
-            </Flex>
-            {index < operations.length - 1 && <Divider marginY="20px" />}
-          </Box>
-        ))}
+              {index < operations.length - 1 && <Divider marginY="20px" />}
+            </Box>
+          );
+        })}
       </Flex>
       {showFooter && (
         <Flex
@@ -200,7 +255,11 @@ export const BatchView: React.FC<{
           borderRadius="0 0 8px 8px"
           data-testid="footer"
         >
-          <RightHeader operations={accountOperations} />
+          <RightHeader
+            batchSubmitIsLoading={batchSubmitIsLoading}
+            onBatchSubmit={onBatchSubmit}
+            operations={accountOperations}
+          />
         </Flex>
       )}
     </Box>
