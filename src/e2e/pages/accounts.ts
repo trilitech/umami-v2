6b992e2d5bb3cad7cb333766f03595fb4b2df7e5
Ana Maksimovskikh <ana.maksimovskikh@trili.tech>
2024-01-05 16:39:53 +0000

    Add accountGroup helper & refactor related e2e tests

--- src/e2e/pages/accounts.ts
+++ src/e2e/pages/accounts.ts
@@ -16,15 +16,16 @@ export class AccountsPage {
 
   async getGroup(groupTitle: string): Promise<AccountGroup> {
     const group = this.page.getByTestId(`account-group-${groupTitle}`);
+
     const label = await group.getByTestId("account-group-title").innerText();
+
     const accountTiles = await group.getByTestId("account-identifier").all();
-    const accounts = await Promise.all(
-      accountTiles.map(async accountTile => {
-        const label = await accountTile.getByRole("heading", { level: 2 }).innerText();
-        const address = await accountTile.getByTestId("short-address").innerText();
-        return { label, address };
-      })
-    );
+    const accounts: Account[] = [];
+    for (const accountTile of accountTiles) {
+      const label = await accountTile.getByRole("heading", { level: 2 }).innerText();
+      const address = await accountTile.getByTestId("short-address").innerText();
+      accounts.push({ label, address });
+    }
 
     return {
       label,
