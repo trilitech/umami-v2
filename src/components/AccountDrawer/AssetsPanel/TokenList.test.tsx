6f76cb878c43ea714ba486efa91a7eea26a187bf
Ana Maksimovskikh <ana.maksimovskikh@trili.tech>
2024-01-11 16:55:42 +0000

    Hide "View All" link when all tokens are shown on assets panel

--- src/components/AccountDrawer/AssetsPanel/TokenList.test.tsx
+++ src/components/AccountDrawer/AssetsPanel/TokenList.test.tsx
@@ -6,6 +6,7 @@ import { FA2TokenBalance, fromRaw } from "../../../types/TokenBalance";
 
 describe("<TokenList />", () => {
   const ACCOUNT = mockMnemonicAccount(0);
+  const HEDGEHOGE_TOKEN = fromRaw(hedgehoge(ACCOUNT.address))! as FA2TokenBalance;
 
   describe("with no tokens", () => {
     it('displays a "No tokens found" message', () => {
@@ -32,7 +33,7 @@ describe("<TokenList />", () => {
       const tokens = [
         mockFA2Token(0, ACCOUNT, 123456, 5, "T1", "Token 1"),
         mockFA12Token(1, ACCOUNT, 123456),
-        fromRaw(hedgehoge(ACCOUNT.address))! as FA2TokenBalance,
+        HEDGEHOGE_TOKEN,
       ];
 
       render(<TokenList owner={ACCOUNT.address.pkh} tokens={tokens} />);
@@ -61,8 +62,49 @@ describe("<TokenList />", () => {
       expect(screen.getByTestId("verified-icon")).toBeInTheDocument();
     });
 
-    it('renders a "View All" link', () => {
-      const tokens = [mockFA2Token(0, ACCOUNT), mockFA12Token(1, ACCOUNT)];
+    const fakeFA12Token = (i: number) => {
+      const t12 = mockFA12Token(0, ACCOUNT);
+      t12.contract = `fake-FA2-pkh-${i}`;
+      return t12;
+    };
+    const fakeFA2Token = (i: number) => {
+      const t2 = mockFA12Token(0, ACCOUNT);
+      t2.contract = `fake-FA12-pkh-${i}`;
+      return t2;
+    };
+
+    it("renders all tokens without view all link when <= 20 tokens", () => {
+      const tokens = [];
+      for (let i = 0; i < 10; i++) {
+        tokens.push(fakeFA12Token(i));
+        tokens.push(fakeFA2Token(i));
+      }
+
+      render(<TokenList owner={ACCOUNT.address.pkh} tokens={tokens} />);
+
+      expect(screen.getAllByTestId("token-tile")).toHaveLength(20);
+      expect(screen.queryByRole("link", { name: "View All" })).not.toBeInTheDocument();
+    });
+
+    it("renders top 20 tokens (in the same order as given) when > 20 tokens", () => {
+      const tokens = [];
+      for (let i = 0; i < 10; i++) {
+        tokens.push(fakeFA12Token(i));
+        tokens.push(fakeFA2Token(i));
+      }
+      tokens.push(HEDGEHOGE_TOKEN);
+
+      render(<TokenList owner={ACCOUNT.address.pkh} tokens={tokens} />);
+      expect(screen.queryByText("Hedgehoge")).not.toBeInTheDocument();
+    });
+
+    it('renders a "View All" link when > 20 tokens', () => {
+      const tokens = [];
+      for (let i = 0; i < 10; i++) {
+        tokens.push(fakeFA12Token(i));
+        tokens.push(fakeFA2Token(i));
+      }
+      tokens.push(HEDGEHOGE_TOKEN);
 
       render(<TokenList owner={ACCOUNT.address.pkh} tokens={tokens} />);
 
