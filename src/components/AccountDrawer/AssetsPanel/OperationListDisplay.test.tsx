95e01a7d56073a60aeda2bf128d4a3a20e412aad
Ana Maksimovskikh <ana.maksimovskikh@trili.tech>
2024-01-11 16:56:23 +0000

    Hide "View All" link when all operations are shown on assets panel

--- src/components/AccountDrawer/AssetsPanel/OperationListDisplay.test.tsx
+++ src/components/AccountDrawer/AssetsPanel/OperationListDisplay.test.tsx
@@ -1,27 +1,84 @@
 import { OperationListDisplay } from "./OperationListDisplay";
-import { mockImplicitAddress } from "../../../mocks/factories";
+import { mockDelegation, mockImplicitAddress } from "../../../mocks/factories";
 import { render, screen } from "../../../mocks/testUtils";
 import { mockTzktTezTransfer } from "../../../mocks/transfers";
 import { TzktCombinedOperation } from "../../../utils/tezos";
 
 describe("<OperationListDisplay />", () => {
-  const operations = [
-    {
-      ...mockTzktTezTransfer(mockImplicitAddress(0).pkh, mockImplicitAddress(1).pkh, 1000000),
-      id: 1,
-    } as TzktCombinedOperation,
-    {
-      ...mockTzktTezTransfer(mockImplicitAddress(1).pkh, mockImplicitAddress(0).pkh, 2000000),
-      id: 2,
-    } as TzktCombinedOperation,
-  ];
+  const OWNER = mockImplicitAddress(0).pkh;
 
-  it('renders a "View All" link', () => {
-    render(<OperationListDisplay operations={operations} owner={mockImplicitAddress(0).pkh} />);
+  describe("with no operations", () => {
+    it('displays a "No operations found" message', () => {
+      render(<OperationListDisplay operations={[]} owner={OWNER} />);
 
-    expect(screen.getByRole("link", { name: "View All" })).toHaveAttribute(
-      "href",
-      `#/operations?accounts=${mockImplicitAddress(0).pkh}`
+      expect(screen.getByText("No operations found")).toBeInTheDocument();
+    });
+
+    it("does not show operation tiles", () => {
+      render(<OperationListDisplay operations={[]} owner={OWNER} />);
+
+      expect(screen.queryByTestId(/^operation-tile/)).not.toBeInTheDocument();
+    });
+
+    it("doesn't show a 'View All' link", () => {
+      render(<OperationListDisplay operations={[]} owner={OWNER} />);
+
+      expect(screen.queryByRole("link", { name: "View All" })).not.toBeInTheDocument();
+    });
+  });
+
+  describe("with operations", () => {
+    const transferOperations = [...Array(10)].flatMap(
+      (_, i) =>
+        [
+          {
+            ...mockTzktTezTransfer(OWNER, mockImplicitAddress(1).pkh, 1000000),
+            id: i + 10,
+          },
+          {
+            ...mockTzktTezTransfer(mockImplicitAddress(1).pkh, OWNER, 1000000),
+            id: i + 20,
+          },
+        ] as TzktCombinedOperation[]
     );
+    const delegationOperation = mockDelegation(
+      0,
+      6000000,
+      mockImplicitAddress(2).pkh,
+      "Some baker"
+    ) as TzktCombinedOperation;
+
+    it("renders all operations without view all link when <= 20 operations", () => {
+      render(<OperationListDisplay operations={transferOperations} owner={OWNER} />);
+
+      expect(screen.getAllByTestId(/^operation-tile/)).toHaveLength(20);
+      expect(screen.queryByRole("link", { name: "View All" })).not.toBeInTheDocument();
+    });
+
+    it("renders top 20 tokens (in the same order as given) when > 20 tokens", () => {
+      render(
+        <OperationListDisplay
+          operations={[...transferOperations, delegationOperation]}
+          owner={OWNER}
+        />
+      );
+
+      expect(screen.getAllByTestId(/^operation-tile/)).toHaveLength(20);
+      expect(screen.queryByText("Delegate")).not.toBeInTheDocument();
+    });
+
+    it('renders a "View All" link when > 20 operations', () => {
+      render(
+        <OperationListDisplay
+          operations={[...transferOperations, delegationOperation]}
+          owner={OWNER}
+        />
+      );
+
+      expect(screen.getByRole("link", { name: "View All" })).toHaveAttribute(
+        "href",
+        `#/operations?accounts=${mockImplicitAddress(0).pkh}`
+      );
+    });
   });
 });
