813ea0a177dd1fe7ed759923d47e59d9f0d50a5c
Sergey Kintsel <sergey.kintsel@trili.tech>
2024-01-23 19:09:43 +0000

    Remove @svgr/webpack dep

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -44,17 +44,6 @@ declare module "*.webp" {
   export default src;
 }
 
-declare module "*.svg" {
-  import * as React from "react";
-
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-
-  const src: string;
-  export default src;
-}
-
 declare module "*.module.css" {
   const classes: { readonly [key: string]: string };
   export default classes;
