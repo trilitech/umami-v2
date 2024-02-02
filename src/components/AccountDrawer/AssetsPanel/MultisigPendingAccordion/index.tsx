1788e96f2a5c4543ea35afa1912e375386f6d114
Ana Maksimovskikh <ana.maksimovskikh@trili.tech>
2024-02-02 13:11:06 +0000

    Hide pending operations tab when multisig doesn't have any

--- src/components/AccountDrawer/AssetsPanel/MultisigPendingAccordion/index.tsx
+++ src/components/AccountDrawer/AssetsPanel/MultisigPendingAccordion/index.tsx
@@ -5,16 +5,12 @@ import React from "react";
 import { MultisigPendingAccordionItem } from "./MultisigPendingAccordionItem";
 import { MultisigAccount } from "../../../../types/Account";
 import { useGetPendingMultisigOperations } from "../../../../utils/hooks/multisigHooks";
-import { NoItems } from "../../../NoItems";
 
 export const MultisigPendingAccordion: React.FC<{
   account: MultisigAccount;
 }> = ({ account }) => {
   const getPendingOperations = useGetPendingMultisigOperations();
   const pendingOperations = getPendingOperations(account);
-  if (pendingOperations.length === 0) {
-    return <NoItems small title="No multisig pending operations" />;
-  }
   return (
     <Box width="100%">
       <Accordion allowMultiple={true} defaultIndex={range(pendingOperations.length)}>
