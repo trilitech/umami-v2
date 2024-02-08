9a153c6d7a60f401962ff9ef66fcaebd55798516
Ana Maksimovskikh <ana.maksimovskikh@trili.tech>
2024-02-08 17:27:53 +0000

    Polish SignPayloadRequestPanel

--- src/utils/beacon/BeaconNotification/panels/SignPayloadRequestPanel.tsx
+++ src/utils/beacon/BeaconNotification/panels/SignPayloadRequestPanel.tsx
@@ -4,17 +4,21 @@ import {
   SignPayloadResponseInput,
 } from "@airgap/beacon-wallet";
 import {
+  Box,
+  Heading,
   ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalFooter,
   ModalHeader,
+  Text,
   useToast,
 } from "@chakra-ui/react";
 import { TezosToolkit } from "@taquito/taquito";
 import React from "react";
 
 import { SignButton } from "../../../../components/SendFlow/SignButton";
+import colors from "../../../../style/colors";
 import { useGetImplicitAccountSafe } from "../../../hooks/getAccountDataHooks";
 import { walletClient } from "../../beacon";
 
@@ -47,15 +51,30 @@ export const SignPayloadRequestPanel: React.FC<{
 
   return (
     <ModalContent>
-      <ModalHeader marginBottom="10px">
-        Sign payload request from {request.appMetadata.name}
+      <ModalHeader marginBottom="32px" textAlign="center">
+        Connect with pairing request
       </ModalHeader>
-
       <ModalCloseButton />
-      <ModalBody>{request.payload}</ModalBody>
 
-      <ModalFooter justifyContent="center" display="flex">
-        <SignButton onSubmit={sign} signer={signerAccount} />
+      <ModalBody>
+        <Heading marginBottom="12px" size="l">
+          {`${request.appMetadata.name}/dApp Pairing Request`}
+        </Heading>
+        <Box
+          padding="15px"
+          border="1px solid"
+          borderColor={colors.gray[500]}
+          borderRadius="4px"
+          backgroundColor={colors.gray[800]}
+        >
+          <Text color={colors.gray[450]} size="md">
+            {request.payload}
+          </Text>
+        </Box>
+      </ModalBody>
+
+      <ModalFooter justifyContent="center" display="flex" padding="16px 0 0 0">
+        <SignButton onSubmit={sign} signer={signerAccount} text="Connect" />
       </ModalFooter>
     </ModalContent>
   );
