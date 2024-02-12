1089c314b0ebff0a9c4e17f57badb438b11d324b
Sergey Kintsel <sergey.kintsel@trili.tech>
2024-02-12 11:31:01 +0000

    Add encrypted secret key support

--- src/components/Onboarding/restoreSecretKey/RestoreSecretKey.test.tsx
+++ src/components/Onboarding/restoreSecretKey/RestoreSecretKey.test.tsx
@@ -2,31 +2,85 @@ import { Provider } from "react-redux";
 
 import { RestoreSecretKey } from "./RestoreSecretKey";
 import { fireEvent, render, screen, waitFor } from "../../../mocks/testUtils";
+import { mockToast } from "../../../mocks/toast";
 import { store } from "../../../utils/redux/store";
 import { Step } from "../useOnboardingModal";
 
-const fixture = (goToStep: (step: Step) => void) => (
+const fixture = (goToStep: (step: Step) => void = jest.fn()) => (
   <Provider store={store}>
     <RestoreSecretKey goToStep={goToStep} />;
   </Provider>
 );
 
+const ENCRYPTED_SECRET_KEY =
+  "edesk1GXwWmGjXiLHBKxGBxwmNvG21vKBh6FBxc4CyJ8adQQE2avP5vBB57ZUZ93Anm7i4k8RmsHaPzVAvpnHkFF";
+const UNENCRYPTED_SECRET_KEY =
+  "edskRk1hRPhBCsGRDfqRBKDY5ecPKLfBhQDC4MvmWwa8i8dXUiGEyWJ7vUDjFo1k59PHfRrQKSEM9ieJNH3FbqrrDFg18ZZorh";
+
 describe("<RestoreSecretKey />", () => {
-  it("requires the secret key", async () => {
-    render(fixture(jest.fn()));
+  describe("validations", () => {
+    it("requires the secret key", async () => {
+      render(fixture());
 
-    fireEvent.blur(screen.getAllByRole("textbox")[0]);
+      fireEvent.blur(screen.getByTestId("secret-key"));
 
-    await waitFor(() => {
-      expect(screen.getByText("Secret key is required")).toBeInTheDocument();
+      await waitFor(() => {
+        expect(screen.getByText("Secret key is required")).toBeInTheDocument();
+      });
+    });
+
+    it("requires the password when the secret key is encrypted", async () => {
+      render(fixture());
+
+      fireEvent.change(screen.getByTestId("secret-key"), { target: { value: "edesk..." } });
+      await screen.findByTestId("password");
+
+      fireEvent.blur(screen.getByTestId("password"));
+
+      await waitFor(() => {
+        expect(screen.getByText("Password is required")).toBeInTheDocument();
+      });
     });
   });
 
-  it("goes to the name account step", async () => {
+  it("doesn't show the password field when the secret key is not encrypted", async () => {
+    render(fixture());
+
+    expect(screen.queryByTestId("password")).not.toBeInTheDocument();
+    fireEvent.change(screen.getByTestId("secret-key"), { target: { value: "edsk..." } });
+    await waitFor(() => expect(screen.queryByTestId("password")).not.toBeInTheDocument());
+  });
+
+  it.each([
+    {
+      title: "encrypted secret key",
+      password: "test",
+      secretKey: ENCRYPTED_SECRET_KEY,
+    },
+    {
+      title: "unencrypted secret key",
+      secretKey: UNENCRYPTED_SECRET_KEY,
+    },
+  ])("goes to the name account step with $title", async ({ password, secretKey }) => {
     const goToStepMock = jest.fn();
     render(fixture(goToStepMock));
 
-    fireEvent.change(screen.getAllByRole("textbox")[0], { target: { value: "test data" } });
+    fireEvent.change(screen.getByTestId("secret-key"), {
+      target: {
+        value: `   ${secretKey} `,
+      },
+    });
+
+    if (password) {
+      await screen.findByTestId("password");
+      fireEvent.change(screen.getByTestId("password"), {
+        target: {
+          value: password,
+        },
+      });
+    }
+
+    await waitFor(() => expect(screen.getByRole("button")).toBeEnabled());
 
     fireEvent.click(screen.getByRole("button", { name: "Continue" }));
     await waitFor(() => {
@@ -34,8 +88,75 @@ describe("<RestoreSecretKey />", () => {
         type: "nameAccount",
         account: {
           type: "secret_key",
-          secretKey: "test data",
+          secretKey: UNENCRYPTED_SECRET_KEY,
         },
+      });
+    });
+  });
+
+  it("shows an error when the password is invalid", async () => {
+    render(fixture());
+
+    fireEvent.change(screen.getByTestId("secret-key"), {
+      target: {
+        value: ENCRYPTED_SECRET_KEY,
+      },
+    });
+
+    await screen.findByTestId("password");
+    fireEvent.change(screen.getByTestId("password"), {
+      target: {
+        value: "wrong password",
+      },
+    });
+
+    await waitFor(() => expect(screen.getByRole("button")).toBeEnabled());
+
+    fireEvent.click(screen.getByRole("button", { name: "Continue" }));
+
+    await waitFor(() => {
+      expect(mockToast).toHaveBeenCalledWith({
+        description: "Key-password pair is invalid",
+        status: "error",
+      });
+    });
+  });
+
+  it("shows an error when the secret key is invalid", async () => {
+    render(fixture());
+
+    fireEvent.change(screen.getByTestId("secret-key"), {
+      target: {
+        value: UNENCRYPTED_SECRET_KEY + "asdasd",
+      },
+    });
+
+    await waitFor(() => expect(screen.getByRole("button")).toBeEnabled());
+    fireEvent.click(screen.getByRole("button", { name: "Continue" }));
+    await waitFor(() => {
+      expect(mockToast).toHaveBeenCalledWith({
+        description: "Invalid secret key: checksum doesn't match",
+        status: "error",
+      });
+    });
+  });
+
+  it("shows an error when the secret key is completely invalid", async () => {
+    render(fixture());
+
+    fireEvent.change(screen.getByTestId("secret-key"), {
+      target: {
+        value: "something invalid",
+      },
+    });
+
+    await waitFor(() => expect(screen.getByRole("button")).toBeEnabled());
+    fireEvent.click(screen.getByRole("button", { name: "Continue" }));
+    await waitFor(() => {
+      expect(mockToast).toHaveBeenCalledWith({
+        description:
+          "Invalid private key with unsupported prefix expecting one of the following 'edesk', 'edsk', 'spsk', 'spesk', 'p2sk' or 'p2esk'..",
+        status: "error",
       });
     });
   });
--- src/components/Onboarding/restoreSecretKey/RestoreSecretKey.tsx
+++ src/components/Onboarding/restoreSecretKey/RestoreSecretKey.tsx
@@ -1,25 +1,69 @@
-import { Button, FormControl, FormErrorMessage, FormLabel, Textarea } from "@chakra-ui/react";
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
+import { InMemorySigner } from "@taquito/signer";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 import { KeyIcon } from "../../../assets/icons";
 import colors from "../../../style/colors";
+import { useAsyncActionHandler } from "../../../utils/hooks/useAsyncActionHandler";
 import { ModalContentWrapper } from "../ModalContentWrapper";
 import { Step, StepType } from "../useOnboardingModal";
 
 export const RestoreSecretKey = ({ goToStep }: { goToStep: (step: Step) => void }) => {
+  const [isEncrypted, setIsEncrypted] = useState(false);
+  const { handleAsyncAction } = useAsyncActionHandler();
+
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<{ secretKey: string }>({
+    formState: { errors, isValid },
+  } = useForm<{ secretKey: string; password: string }>({
     mode: "onBlur",
+    defaultValues: { password: "" },
   });
 
-  const onSubmit = ({ secretKey }: { secretKey: string }) =>
-    goToStep({
-      type: StepType.nameAccount,
-      account: { type: "secret_key", secretKey: secretKey.trim() },
+  const onSubmit = async ({
+    secretKey: rawSecretKey,
+    password,
+  }: {
+    secretKey: string;
+    password: string;
+  }) =>
+    handleAsyncAction(async () => {
+      let secretKey = rawSecretKey.trim();
+
+      try {
+        // validate secret key
+        const signer = await InMemorySigner.fromSecretKey(secretKey, password);
+        secretKey = await signer.secretKey();
+      } catch (error: any) {
+        const message = error.message || "";
+
+        // if the password doesn't match taquito throws this error
+        if (message.includes("Cannot read properties of null (reading 'slice')")) {
+          throw new Error("Key-password pair is invalid");
+        }
+
+        if (message.includes("Invalid checksum")) {
+          throw new Error("Invalid secret key: checksum doesn't match");
+        }
+
+        throw error;
+      }
+
+      goToStep({
+        type: StepType.nameAccount,
+        account: { type: "secret_key", secretKey: secretKey },
+      });
     });
+
   return (
     <ModalContentWrapper
       icon={<KeyIcon width="24px" height="24px" stroke={colors.gray[450]} />}
@@ -30,22 +74,35 @@ export const RestoreSecretKey = ({ goToStep }: { goToStep: (step: Step) => void 
           <FormLabel>Secret Key</FormLabel>
           <Textarea
             minHeight="130px"
-            data-testid="name"
+            data-testid="secret-key"
             {...register("secretKey", {
               required: "Secret key is required",
+              // taken from https://github.com/ecadlabs/taquito/blob/master/packages/taquito-signer/src/taquito-signer.ts#L95
+              onChange: event => setIsEncrypted(event.target.value.trim().substring(2, 3) === "e"),
             })}
             placeholder="Your secret key"
           />
           {errors.secretKey && <FormErrorMessage>{errors.secretKey.message}</FormErrorMessage>}
         </FormControl>
 
-        <Button
-          width="100%"
-          marginTop="32px"
-          isDisabled={!!errors.secretKey}
-          size="lg"
-          type="submit"
-        >
+        {isEncrypted && (
+          <FormControl marginTop="20px" isInvalid={!!errors.password}>
+            <FormLabel>Password</FormLabel>
+            <Input
+              data-testid="password"
+              {...register("password", {
+                validate: value => {
+                  if (!value.trim()) {
+                    return "Password is required";
+                  }
+                },
+              })}
+            />
+            {errors.password && <FormErrorMessage>{errors.password.message}</FormErrorMessage>}
+          </FormControl>
+        )}
+
+        <Button width="100%" marginTop="32px" isDisabled={!isValid} size="lg" type="submit">
           Continue
         </Button>
       </form>
