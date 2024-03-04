f0d998d071c4ff3ec4ba0c13a735b3b7daac9587
Sergey Kintsel <sergey.kintsel@trili.tech>
2024-03-04 14:57:45 +0000

    Get rid of circular deps in icons

--- src/assets/icons/IncomingArrow.tsx
+++ src/assets/icons/IncomingArrow.tsx
@@ -1,6 +1,6 @@
 import { IconProps } from "@chakra-ui/react";
 
-import { OutgoingArrow } from ".";
+import { OutgoingArrow } from "./OutgoingArrow";
 
 export const IncomingArrow: React.FC<IconProps> = props => (
   <OutgoingArrow css={{ rotate: "180deg" }} {...props} />
