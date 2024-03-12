4b1a09d8bdd9525202ac4d44d65037c66d1a3afb
Ana Maksimovskikh <ana.maksimovskikh@trili.tech>
2024-03-12 16:49:06 +0000

    Adjust ModalContentWrapper to look like mocks

--- src/components/Onboarding/ModalContentWrapper.tsx
+++ src/components/Onboarding/ModalContentWrapper.tsx
@@ -13,13 +13,21 @@ type Props = {
 
 export const ModalContentWrapper = ({ children, icon, title, subtitle }: Props) => (
   <VStack maxHeight="83vh" spacing={0}>
-    <Box marginBottom="20px">
+    <Box marginBottom="16px">
       <CircleIcon icon={icon} size="48px" />
     </Box>
-    <Center flexDirection="column" marginBottom="32px">
-      <Heading size="xl">{title}</Heading>
+    <Center flexDirection="column" width="340px" marginBottom="32px">
+      <Heading lineHeight="26px" size="xl">
+        {title}
+      </Heading>
       {subtitle && (
-        <Text marginTop="10px" color={colors.gray[400]} textAlign="center" size="sm">
+        <Text
+          marginTop="10px"
+          color={colors.gray[400]}
+          lineHeight="18px"
+          textAlign="center"
+          size="sm"
+        >
           {subtitle}
         </Text>
       )}
