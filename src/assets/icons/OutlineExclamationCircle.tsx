0f3f633767c09d0c4c1f60e41f903c83f25ff624
Ana Maksimovskikh <ana.maksimovskikh@trili.tech>
2024-03-13 11:18:37 +0000

    Switch to using icons specs from Figma

--- src/assets/icons/OutlineExclamationCircle.tsx
+++ src/assets/icons/OutlineExclamationCircle.tsx
@@ -6,12 +6,17 @@ export const OutlineExclamationCircleIcon: React.FC<IconProps> = props => (
   <Icon
     width="16px"
     height="16px"
-    fill={colors.white}
-    viewBox="0 0 1024 1024"
+    fill="none"
+    stroke={colors.gray[450]}
+    viewBox="0 0 16 16"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z" />
-    <path d="M464 688a48 48 0 1 0 96 0 48 48 0 1 0-96 0zm24-112h48c4.4 0 8-3.6 8-8V296c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v272c0 4.4 3.6 8 8 8z" />
+    <path
+      d="M8 5V8.75M8 11H8.0075M14.75 8C14.75 11.7279 11.7279 14.75 8 14.75C4.27208 14.75 1.25 11.7279 1.25 8C1.25 4.27208 4.27208 1.25 8 1.25C11.7279 1.25 14.75 4.27208 14.75 8Z"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.2"
+    />
   </Icon>
 );
--- src/assets/icons/ThreeDots.tsx
+++ src/assets/icons/ThreeDots.tsx
@@ -4,13 +4,19 @@ import colors from "../../style/colors";
 
 export const ThreeDotsIcon: React.FC<IconProps> = props => (
   <Icon
-    width="14px"
-    height="14px"
-    fill={colors.white}
-    viewBox="0 0 16 16"
+    width="18px"
+    height="18px"
+    fill="none"
+    stroke={colors.gray[300]}
+    viewBox="0 0 18 18"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+    <path
+      d="M13.5 9H13.5075M9 9H9.0075M4.5 9H4.5075M9.75 9C9.75 9.41421 9.41421 9.75 9 9.75C8.58579 9.75 8.25 9.41421 8.25 9C8.25 8.58579 8.58579 8.25 9 8.25C9.41421 8.25 9.75 8.58579 9.75 9ZM14.25 9C14.25 9.41421 13.9142 9.75 13.5 9.75C13.0858 9.75 12.75 9.41421 12.75 9C12.75 8.58579 13.0858 8.25 13.5 8.25C13.9142 8.25 14.25 8.58579 14.25 9ZM5.25 9C5.25 9.41421 4.91421 9.75 4.5 9.75C4.08579 9.75 3.75 9.41421 3.75 9C3.75 8.58579 4.08579 8.25 4.5 8.25C4.91421 8.25 5.25 8.58579 5.25 9Z"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    />
   </Icon>
 );
