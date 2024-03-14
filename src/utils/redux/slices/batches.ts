952c8ba863b56569476c9dbb61864aa83e92820c
Ana Maksimovskikh <ana.maksimovskikh@trili.tech>
2024-03-14 15:46:45 +0000

    Add method to remove all batches for given accounts to BatchesSlice

--- src/utils/redux/slices/batches.ts
+++ src/utils/redux/slices/batches.ts
@@ -65,6 +65,13 @@ export const batchesSlice = createSlice({
         batches.splice(batchIndex, 1);
       }
     },
+    removeByAccounts: (state, { payload: { pkhs } }: { payload: { pkhs: RawPkh[] } }) => {
+      Object.keys(state).forEach(networkName => {
+        const batches = state[networkName as NetworkName] || [];
+        const newBatches = batches.filter(batch => !pkhs.includes(batch.sender.address.pkh));
+        state[networkName as NetworkName] = newBatches;
+      });
+    },
   },
 });
 
