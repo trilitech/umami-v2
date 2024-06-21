16c7f20ecf2b35b053dea430753c691861194537
Ana Maksimovskikh <ana.maksimovskikh@trili.tech>
2024-06-21 13:25:57 +0100

    Reuse auth logic in the embed-iframe login modal

--- apps/embed-iframe/src/LoginModalContent.tsx
+++ apps/embed-iframe/src/LoginModalContent.tsx
@@ -1,11 +1,13 @@
 import { Box, Button, Center, Flex, Heading, Text, VStack } from "@chakra-ui/react";
 import { InMemorySigner } from "@taquito/signer";
+import * as Auth from "@umami/social-auth";
 import { useState } from "react";
 
+import { FacebookLogoIcon } from "./assets/icons/FacebookLogo";
 import { GoogleLogoIcon } from "./assets/icons/GoogleLogo";
 import { TezosLogoIcon } from "./assets/icons/TezosLogo";
+import { TwitterLogoIcon } from "./assets/icons/TwitterLogo";
 import { UmamiLogoIcon } from "./assets/icons/UmamiLogo";
-import { getGoogleCredentials } from "./imported/getGoogleCredentials";
 import colors from "./imported/style/colors";
 import { getErrorContext } from "./imported/utils/getErrorContext";
 import { sendLoginErrorResponse, sendResponse } from "./utils";
@@ -13,10 +15,10 @@ import { sendLoginErrorResponse, sendResponse } from "./utils";
 export const LoginModalContent: React.FC<{ closeModal: () => void }> = ({ closeModal }) => {
   const [isLoading, setIsLoading] = useState(false);
 
-  const onGoogleLoginClick = async () => {
+  const onLoginClick = async (idp: Auth.IDP) => {
     setIsLoading(true);
     try {
-      const { secretKey, email } = await getGoogleCredentials();
+      const { secretKey, name } = await Auth.forIDP(idp, "embed").getCredentials();
 
       const signer = new InMemorySigner(secretKey);
       const { pk, pkh } = { pk: await signer.publicKey(), pkh: await signer.publicKeyHash() };
@@ -25,7 +27,7 @@ export const LoginModalContent: React.FC<{ closeModal: () => void }> = ({ closeM
         type: "login_response",
         pk,
         pkh,
-        userData: { typeOfLogin: "google", id: email },
+        userData: { typeOfLogin: "google", id: name },
       });
     } catch (error) {
       sendLoginErrorResponse(getErrorContext(error).description);
@@ -43,15 +45,53 @@ export const LoginModalContent: React.FC<{ closeModal: () => void }> = ({ closeM
       <Heading marginBottom="30px" textColor={colors.white} fontSize="16px" lineHeight="22px">
         Choose Provider
       </Heading>
+      <VStack width="100%" spacing="10px">
+        <Button width="100%" isLoading={isLoading} onClick={() => onLoginClick("google")} size="lg">
+          <Flex alignItems="center" justifyContent="flex-start" flex={1}>
+            <GoogleLogoIcon position="absolute" />
+            <Heading margin="auto" textColor={colors.white} fontSize="14px" lineHeight="18px">
+              Continue with Google
+            </Heading>
+          </Flex>
+        </Button>
 
-      <Button width="100%" isLoading={isLoading} onClick={onGoogleLoginClick} size="lg">
-        <Flex alignItems="center" justifyContent="flex-start" flex={1}>
-          <GoogleLogoIcon position="absolute" />
-          <Heading margin="auto" textColor={colors.white} fontSize="14px" lineHeight="18px">
-            Continue with Google
-          </Heading>
-        </Flex>
-      </Button>
+        <Button
+          width="100%"
+          isLoading={isLoading}
+          onClick={() => onLoginClick("facebook")}
+          size="lg"
+        >
+          <Flex alignItems="center" justifyContent="flex-start" flex={1}>
+            <FacebookLogoIcon position="absolute" />
+            <Heading margin="auto" textColor={colors.white} fontSize="14px" lineHeight="18px">
+              Continue with Facebook
+            </Heading>
+          </Flex>
+        </Button>
+
+        <Button
+          width="100%"
+          isLoading={isLoading}
+          onClick={() => onLoginClick("twitter")}
+          size="lg"
+        >
+          <Flex alignItems="center" justifyContent="flex-start" flex={1}>
+            <TwitterLogoIcon position="absolute" />
+            <Heading margin="auto" textColor={colors.white} fontSize="14px" lineHeight="18px">
+              Continue with X
+            </Heading>
+          </Flex>
+        </Button>
+
+        <Button width="100%" isLoading={isLoading} onClick={() => onLoginClick("reddit")} size="lg">
+          <Flex alignItems="center" justifyContent="flex-start" flex={1}>
+            <GoogleLogoIcon position="absolute" />
+            <Heading margin="auto" textColor={colors.white} fontSize="14px" lineHeight="18px">
+              Continue with Reddit
+            </Heading>
+          </Flex>
+        </Button>
+      </VStack>
 
       <Center marginTop="30px">
         <Text marginRight="10px" color={colors.gray[450]} fontSize="xs" lineHeight="14px">
---
+++ apps/embed-iframe/src/assets/icons/TwitterLogo.tsx
@@ -0,0 +1,17 @@
+import { Icon, type IconProps } from "@chakra-ui/react";
+
+export const TwitterLogoIcon = (props: IconProps) => (
+  <Icon
+    width="18px"
+    height="17px"
+    fill="none"
+    viewBox="0 0 18 17"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M0.0437997 0L6.99339 9.2921L0 16.847H1.57406L7.6969 10.2324L12.6438 16.847H18L10.6592 7.03235L17.1687 0H15.5947L9.95603 6.09173L5.4 0H0.0437997ZM2.35852 1.15932H4.81913L15.685 15.6877H13.2244L2.35852 1.15932Z"
+      fill="white"
+    />
+  </Icon>
+);
