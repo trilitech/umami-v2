9a59eccec1e69ff02acf6ad00f6e39c61db9fc13
Sergey Kintsel <sergey.kintsel@trili.tech>
2024-06-27 10:30:28 +0100

    Add a testing account for web app

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,18 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      include: ["stream", "util", "crypto", "vm"],
+      globals: {
+        Buffer: true,
+      },
+    }),
+  ],
   build: {
     emptyOutDir: false,
   },
