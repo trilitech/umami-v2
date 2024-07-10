b0cddf5c759f03c896d088e1d57575cbb9e8d4b3
Sergey Kintsel <sergey.kintsel@trili.tech>
2024-07-10 10:40:58 +0100

    Use dev account for prod

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -8,7 +8,6 @@ import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 
 import { App } from "./App";
-import { IS_DEV } from "./env";
 import { ReactQueryProvider } from "./providers/ReactQueryProvider";
 import { ReduxStore } from "./providers/ReduxStore";
 import { UmamiTheme } from "./providers/UmamiTheme";
@@ -24,32 +23,31 @@ const logError = (error: Error, info: { componentStack?: string | null }) => {
   store.dispatch(errorsActions.add(errorContext));
 };
 
-if (IS_DEV) {
-  const state = store.getState();
-  if (!state.accounts.items.length) {
-    // Add a testing account if there are no accounts in the store
-    store.dispatch(
-      accountsActions.addMnemonicAccounts({
-        seedFingerprint: "test group",
-        accounts: [
-          {
-            type: "mnemonic",
-            address: {
-              pkh: "tz1UNer1ijeE9ndjzSszRduR3CzX49hoBUB3",
-              type: "implicit",
-            },
-            curve: "ed25519",
-            label: "Account 1",
-            derivationPath: "44'/1729'/0'/0'",
-            derivationPathTemplate: "44'/1729'/?'/0'",
-            pk: "edpkuwYWCugiYG7nMnVUdopFmyc3sbMSiLqsJHTQgGtVhtSdLSw6HG",
-            seedFingerPrint: "test group",
+// TODO: remove when onboarding is in place
+const state = store.getState();
+if (!state.accounts.items.length) {
+  // Add a testing account if there are no accounts in the store
+  store.dispatch(
+    accountsActions.addMnemonicAccounts({
+      seedFingerprint: "test group",
+      accounts: [
+        {
+          type: "mnemonic",
+          address: {
+            pkh: "tz1UNer1ijeE9ndjzSszRduR3CzX49hoBUB3",
+            type: "implicit",
           },
-        ],
-        encryptedMnemonic: encryptedMnemonic1,
-      })
-    );
-  }
+          curve: "ed25519",
+          label: "Account 1",
+          derivationPath: "44'/1729'/0'/0'",
+          derivationPathTemplate: "44'/1729'/?'/0'",
+          pk: "edpkuwYWCugiYG7nMnVUdopFmyc3sbMSiLqsJHTQgGtVhtSdLSw6HG",
+          seedFingerPrint: "test group",
+        },
+      ],
+      encryptedMnemonic: encryptedMnemonic1,
+    })
+  );
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
