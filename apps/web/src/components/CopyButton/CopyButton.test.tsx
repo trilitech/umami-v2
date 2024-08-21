798e184d0c12e8be50219e28e501a6350a5914c7
Oleg Chendighelean <oleg.chendighelean@trili.tech>
2024-08-21 17:57:29 +0100

    Add test to copy button

--- apps/web/src/components/CopyButton/CopyButton.test.tsx
+++ apps/web/src/components/CopyButton/CopyButton.test.tsx
@@ -12,4 +12,19 @@ describe("<CopyButton />", () => {
 
     expect(navigator.clipboard.writeText).toHaveBeenCalledWith("hello");
   });
+
+  it("disables copy logic when isCopyDisabled is true", async () => {
+    const user = userEvent.setup();
+    jest.spyOn(navigator.clipboard, "writeText");
+
+    render(
+      <CopyButton isCopyDisabled value="hello">
+        Copy
+      </CopyButton>
+    );
+
+    await act(() => user.click(screen.getByTestId("copy-button")));
+
+    expect(navigator.clipboard.writeText).not.toHaveBeenCalled();
+  });
 });
