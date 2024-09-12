e92c788fd965b0a4c0e5af0a73acd2f42d4b12df
Sergey Kintsel <sergey.kintsel@trili.tech>
2024-09-12 13:11:08 +0100

    Improve multisig account fetching

--- packages/multisig/src/fetch.test.ts
+++ packages/multisig/src/fetch.test.ts
@@ -1,6 +1,7 @@
 import * as api from "@tzkt/sdk-api";
 import { DefaultNetworks, GHOSTNET, mockImplicitAddress } from "@umami/tezos";
 import axios from "axios";
+import range from "lodash/range";
 
 import {
   CODE_HASH,
@@ -13,43 +14,9 @@ import {
 const mockedAxios = jest.spyOn(axios, "get");
 
 const mockedContractsGet = jest.spyOn(api, "contractsGet");
+const mockedContractsGetCount = jest.spyOn(api, "contractsGetCount");
 
 const multisigContracts = [
-  {
-    id: 533705,
-    type: "contract",
-    address: "KT1Mqvf7bnYe4Ty2n7ZbGkdbebCd4WoTJUUp",
-    kind: "smart_contract",
-    balance: 0,
-    creator: { address: "tz1LbSsDSmekew3prdDGx1nS22ie6jjBN6B3" },
-    numContracts: 0,
-    activeTokensCount: 0,
-    tokensCount: 0,
-    tokenBalancesCount: 0,
-    tokenTransfersCount: 0,
-    numDelegations: 0,
-    numOriginations: 1,
-    numTransactions: 0,
-    numReveals: 0,
-    numMigrations: 0,
-    transferTicketCount: 0,
-    increasePaidStorageCount: 0,
-    eventsCount: 0,
-    firstActivity: 1636117,
-    firstActivityTime: "2022-12-09T16:49:25Z",
-    lastActivity: 1636117,
-    lastActivityTime: "2022-12-09T16:49:25Z",
-    storage: {
-      owner: "tz1LbSsDSmekew3prdDGx1nS22ie6jjBN6B3",
-      signers: ["tz1LbSsDSmekew3prdDGx1nS22ie6jjBN6B3", "tz1dyX3B1CFYa2DfdFLyPtiJCfQRUgPVME6E"],
-      metadata: 216412,
-      threshold: "1",
-      last_op_id: "0",
-      pending_ops: 216411,
-    },
-    typeHash: 1963879877,
-    codeHash: -1890025422,
-  },
   {
     id: 536908,
     type: "contract",
@@ -124,11 +91,47 @@ const multisigContracts = [
     typeHash: 1963879877,
     codeHash: -1890025422,
   },
+  {
+    id: 533705,
+    type: "contract",
+    address: "KT1Mqvf7bnYe4Ty2n7ZbGkdbebCd4WoTJUUp",
+    kind: "smart_contract",
+    balance: 0,
+    creator: { address: "tz1LbSsDSmekew3prdDGx1nS22ie6jjBN6B3" },
+    numContracts: 0,
+    activeTokensCount: 0,
+    tokensCount: 0,
+    tokenBalancesCount: 0,
+    tokenTransfersCount: 0,
+    numDelegations: 0,
+    numOriginations: 1,
+    numTransactions: 0,
+    numReveals: 0,
+    numMigrations: 0,
+    transferTicketCount: 0,
+    increasePaidStorageCount: 0,
+    eventsCount: 0,
+    firstActivity: 1636117,
+    firstActivityTime: "2022-12-09T16:49:25Z",
+    lastActivity: 1636117,
+    lastActivityTime: "2022-12-09T16:49:25Z",
+    storage: {
+      owner: "tz1LbSsDSmekew3prdDGx1nS22ie6jjBN6B3",
+      signers: ["tz1LbSsDSmekew3prdDGx1nS22ie6jjBN6B3", "tz1dyX3B1CFYa2DfdFLyPtiJCfQRUgPVME6E"],
+      metadata: 216412,
+      threshold: "1",
+      last_op_id: "0",
+      pending_ops: 216411,
+    },
+    typeHash: 1963879877,
+    codeHash: -1890025422,
+  },
 ];
 
 describe("multisig fetch", () => {
   const expectedMockedMultisigContracts = [
     {
+      id: 533705,
       address: "KT1Mqvf7bnYe4Ty2n7ZbGkdbebCd4WoTJUUp",
       storage: {
         pending_ops: 216411,
@@ -137,6 +140,7 @@ describe("multisig fetch", () => {
       },
     },
     {
+      id: 536908,
       address: "KT1VwWbTMRN5uX4bfxCcpJnPP6iAhboqhGZr",
       storage: {
         pending_ops: 219458,
@@ -149,6 +153,7 @@ describe("multisig fetch", () => {
       },
     },
     {
+      id: 537023,
       address: "KT1Vdhz4izz7LASWU4tTLu3GBsvhJ8ULSi3G",
       storage: {
         pending_ops: 219535,
@@ -160,25 +165,72 @@ describe("multisig fetch", () => {
 
   describe("getAllMultiSigContracts", () => {
     it("fetches all multisig contracts", async () => {
-      mockedContractsGet.mockResolvedValue(multisigContracts as any);
+      mockedContractsGetCount.mockResolvedValue(multisigContracts.length);
+      mockedContractsGet.mockResolvedValue(multisigContracts);
       const result = await getAllMultisigContracts(GHOSTNET);
 
+      expect(mockedContractsGetCount).toHaveBeenCalledTimes(1);
+      expect(mockedContractsGetCount).toHaveBeenCalledWith(
+        {
+          kind: { eq: "smart_contract" },
+          typeHash: { eq: TYPE_HASH },
+          codeHash: { eq: CODE_HASH },
+        },
+        { baseUrl: GHOSTNET.tzktApiUrl }
+      );
       expect(mockedContractsGet).toHaveBeenCalledTimes(1);
       expect(mockedContractsGet).toHaveBeenCalledWith(
         {
+          kind: { eq: "smart_contract" },
           typeHash: { eq: TYPE_HASH },
           codeHash: { eq: CODE_HASH },
+          select: { fields: ["id,address,storage"] },
           includeStorage: true,
           limit: 10000,
+          offset: { pg: 0 },
         },
         { baseUrl: GHOSTNET.tzktApiUrl }
       );
       expect(
-        result.map(({ address, storage: { pending_ops, signers, threshold } }) => ({
+        result.map(({ id, address, storage: { pending_ops, signers, threshold } }) => ({
+          id,
           address,
           storage: { pending_ops, signers, threshold },
         }))
       ).toEqual(expectedMockedMultisigContracts);
+    });
+
+    it("handles pagination", async () => {
+      mockedContractsGetCount.mockResolvedValue(79123);
+      mockedContractsGet.mockResolvedValue([]);
+
+      await getAllMultisigContracts(GHOSTNET);
+
+      expect(mockedContractsGetCount).toHaveBeenCalledTimes(1);
+      expect(mockedContractsGetCount).toHaveBeenCalledWith(
+        {
+          kind: { eq: "smart_contract" },
+          typeHash: { eq: TYPE_HASH },
+          codeHash: { eq: CODE_HASH },
+        },
+        { baseUrl: GHOSTNET.tzktApiUrl }
+      );
+
+      expect(mockedContractsGet).toHaveBeenCalledTimes(8);
+      range(8).forEach(index => {
+        expect(mockedContractsGet).toHaveBeenCalledWith(
+          {
+            kind: { eq: "smart_contract" },
+            typeHash: { eq: TYPE_HASH },
+            codeHash: { eq: CODE_HASH },
+            select: { fields: ["id,address,storage"] },
+            includeStorage: true,
+            limit: 10000,
+            offset: { pg: index },
+          },
+          { baseUrl: GHOSTNET.tzktApiUrl }
+        );
+      });
     });
   });
 
