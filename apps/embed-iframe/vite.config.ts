2683a21a6e61d85dde497942c99f414867584c0d
Ana Maksimovskikh <ana.maksimovskikh@trili.tech>
2024-09-23 10:43:37 +0100

    [embed] Fix build for different environments

--- apps/embed-iframe/vite.config.ts
+++ apps/embed-iframe/vite.config.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
@@ -6,6 +8,7 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
   base: "./",
   plugins: [
+    copyVercelConfigPlugin(),
     react(),
     svgr(),
     nodePolyfills({
@@ -17,10 +20,23 @@ export default defineConfig({
   ],
   optimizeDeps: {
     esbuildOptions: {
-      // Node.js global to browser globalThis
       define: {
         global: "globalThis",
+        Buffer: "Buffer",
       },
     },
   },
 });
+
+function copyVercelConfigPlugin() {
+  return {
+    name: "copy-vercel-config",
+    buildStart() {
+      const network_environment = process.env.VITE_EMBED_ENVIRONMENT || "ghostnet";
+      const vercelConfigFile =
+        network_environment === "mainnet" ? "vercel.mainnet.json" : "vercel.ghostnet.json";
+      fs.copyFileSync(vercelConfigFile, "vercel.json");
+      console.log(`Copied ${vercelConfigFile} to vercel.json for network ${network_environment}`);
+    },
+  };
+}
