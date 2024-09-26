ffcbedef6678bebb16a5ad94180be8f9c131cd13
Diana Savatina <diana.savvatina@trili.tech>
2024-09-26 15:23:04 +0100

    feat: WalletConnect integration, part 1, session proposal
    This is the first part of the WalletConnect integration. It includes the following components:
     - initiating WalletKit by WalletConnect
     - subscribing to basic events
     - handling session proposal on a basic level
    
    Limitations:
     - all requests are rejected
     - no pairing list
     - no way to disconnect
     - no verification of dapp, no check for scam

--- apps/web/src/components/Menu/AppsMenu/AppsMenu.tsx
+++ apps/web/src/components/Menu/AppsMenu/AppsMenu.tsx
@@ -2,6 +2,7 @@ import { Button, Divider, Text } from "@chakra-ui/react";
 import { useAddPeer } from "@umami/state";
 
 import { BeaconPeers } from "../../beacon";
+import { onConnect } from "../../WalletConnect";
 import { DrawerContentWrapper } from "../DrawerContentWrapper";
 
 export const AppsMenu = () => {
@@ -10,13 +11,17 @@ export const AppsMenu = () => {
   return (
     <DrawerContentWrapper title="Apps">
       <Text marginTop="12px" size="lg">
-        Connect with Pairing Request
+        Connect with Pairing Request for Beacon or WalletConnect
       </Text>
       <Button
         width="fit-content"
         marginTop="18px"
         padding="0 24px"
-        onClick={() => navigator.clipboard.readText().then(addPeer)}
+        onClick={() =>
+          navigator.clipboard
+            .readText()
+            .then(payload => (payload.startsWith("wc:") ? onConnect(payload) : addPeer(payload)))
+        }
         variant="secondary"
       >
         Connect
