4954ce46567d0302fcc2e0e5297c5e4ae30e7e48
Oleg Chendighelean <oleg.chendighelean@trili.tech>
2024-10-07 12:30:16 +0100

    Update url validation rules

--- packages/components/src/utils/validationSchemes.ts
+++ packages/components/src/utils/validationSchemes.ts
@@ -1,6 +1,22 @@
 import { type Network } from "@umami/tezos";
 import { z } from "zod";
 
+const URL_REGEX = new RegExp(
+  "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+    "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR IP (v4) address
+    "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+    "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+    "(\\#[-a-z\\d_]*)?$",
+  "i"
+);
+
+const urlScheme = (urlType: string) =>
+  z
+    .string()
+    .min(1, `${urlType} URL is required`)
+    .regex(URL_REGEX, `Enter a valid ${urlType} URL`)
+    .startsWith("https://", { message: `${urlType} URL must be secure and start with 'https://'` });
+
 export const getNetworkValidationScheme = (availableNetworks: Network[], network?: Network) =>
   z.object({
     name: network
@@ -11,11 +27,8 @@ export const getNetworkValidationScheme = (availableNetworks: Network[], network
           .refine(name => !availableNetworks.find(n => n.name === name), {
             message: "Network with this name already exists",
           }),
-    rpcUrl: z.string().min(1, "RPC URL is required").url("Enter a valid RPC URL"),
-    tzktApiUrl: z.string().min(1, "Tzkt API URL is required").url("Enter a valid Tzkt API URL"),
-    tzktExplorerUrl: z
-      .string()
-      .min(1, "Tzkt Explorer URL is required")
-      .url("Enter a valid Tzkt Explorer URL"),
-    buyTezUrl: z.string().url("Enter a valid Buy Tez URL").or(z.literal("")),
+    rpcUrl: urlScheme("RPC"),
+    tzktApiUrl: urlScheme("Tzkt API"),
+    tzktExplorerUrl: urlScheme("Tzkt Explorer"),
+    buyTezUrl: urlScheme("Buy Tez").or(z.literal("")),
   });
