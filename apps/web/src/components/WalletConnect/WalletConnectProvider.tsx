a1d75cb03a15d44e9af7f5703a181b88b7904db7
Sergey Kintsel <sergey.kintsel@trili.tech>
2024-10-21 12:14:20 +0100

    Improve WalletConnectProvider useEffect usage

--- apps/web/src/components/WalletConnect/WalletConnectProvider.tsx
+++ apps/web/src/components/WalletConnect/WalletConnectProvider.tsx
@@ -8,29 +8,37 @@ import {
   useAvailableNetworks,
   walletKit,
 } from "@umami/state";
-import { type Network } from "@umami/tezos";
 import { formatJsonRpcError } from "@walletconnect/jsonrpc-utils";
 import { getSdkError } from "@walletconnect/utils";
 import { type PropsWithChildren, useEffect } from "react";
 
 import { SessionProposalModal } from "./SessionProposalModal";
+import { useCallback } from "react";
 
 export const WalletConnectProvider = ({ children }: PropsWithChildren) => {
   const onSessionProposal = useOnSessionProposal();
-  const onSessionDelete = useOnSessionDelete();
   const onSessionRequest = useOnSessionRequest();
+  const toast = useToast();
 
   useEffect(() => {
-    const initializeWallet = async () => {
-      await createWalletKit();
-      walletKit.on("session_proposal", event => void onSessionProposal(event));
-      walletKit.on("session_request", event => void onSessionRequest(event));
-      walletKit.on("session_delete", event => void onSessionDelete(event));
-    };
+    const onSessionDelete = (event: WalletKitTypes.SessionDelete) =>
+      toast({
+        description: `dApp ${event.topic} released the connection.`,
+        status: "info",
+      });
 
-    void initializeWallet();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    createWalletKit().then(() => {
+      walletKit.on("session_proposal", onSessionProposal);
+      walletKit.on("session_request", onSessionRequest);
+      walletKit.on("session_delete", onSessionDelete);
+    });
+
+    return () => {
+      walletKit.removeListener("session_proposal", onSessionProposal);
+      walletKit.removeListener("session_request", onSessionRequest);
+      walletKit.removeListener("session_delete", onSessionDelete);
+    };
+  }, [onSessionProposal, onSessionRequest, toast]);
 
   return children;
 };
@@ -38,53 +46,62 @@ export const WalletConnectProvider = ({ children }: PropsWithChildren) => {
 const useOnSessionProposal = () => {
   const { handleAsyncActionUnsafe } = useAsyncActionHandler();
   const { openWith } = useDynamicModalContext();
-  const availableNetworks: Network[] = useAvailableNetworks();
+  const availableNetworks = useAvailableNetworks();
 
-  return (proposal: WalletKitTypes.SessionProposal) =>
-    handleAsyncActionUnsafe(async () => {
-      // dApp sends in the session proposal the required networks and the optional networks.
-      // The response must contain all the required networks but Umami supports just one per request.
-      // So if the list of required networks is more than one or the required network is not supported, we can only reject the proposal.
-      const requiredNetworks = Object.entries(proposal.params.requiredNamespaces)
-        .map(([key, values]) => (key.includes(":") ? key : (values.chains ?? [])))
-        .flat()
-        .filter(Boolean);
+  return useCallback(
+    (proposal: WalletKitTypes.SessionProposal) =>
+      handleAsyncActionUnsafe(async () => {
+        // dApp sends in the session proposal the required networks and the optional networks.
+        // The response must contain all the required networks but Umami supports just one per request.
+        // So if the list of required networks is more than one or the required network is not supported, we can only reject the proposal.
+        const requiredNetworks = Object.entries(proposal.params.requiredNamespaces)
+          .map(([key, values]) => (key.includes(":") ? key : (values.chains ?? [])))
+          .flat()
+          .filter(Boolean);
 
-      if (requiredNetworks.length !== 1) {
-        throw new Error(
-          `Umami supports only one network per request, got required networks: ${requiredNetworks}`
-        );
-      }
-      const network = requiredNetworks[0] as NetworkType;
-      const availablenetworks = availableNetworks.map(network => network.name);
-      // the network contains a namespace, e.g. tezos:mainnet
-      if (!availablenetworks.includes(network.split(":")[1])) {
-        throw new Error(
-          `The requested required network "${network}" is not supported. Available: ${availablenetworks}`
-        );
-      }
+        if (requiredNetworks.length !== 1) {
+          throw new Error(
+            `Umami supports only one network per request, got required networks: ${requiredNetworks}`
+          );
+        }
+        const network = requiredNetworks[0] as NetworkType;
+        const networkNames = availableNetworks.map(network => network.name);
+        // the network contains a namespace, e.g. tezos:mainnet
+        if (!networkNames.includes(network.split(":")[1])) {
+          throw new Error(
+            `The requested required network "${network}" is not supported. Available: ${networkNames}`
+          );
+        }
 
-      await openWith(<SessionProposalModal network={network} proposal={proposal} />, {});
-    }).catch(async () => {
-      // dApp is waiting so we need to notify it
-      await walletKit.rejectSession({ id: proposal.id, reason: getSdkError("UNSUPPORTED_CHAINS") });
-    });
+        return openWith(<SessionProposalModal network={network} proposal={proposal} />, {});
+      }).catch(() =>
+        // dApp is waiting so we need to notify it
+        walletKit.rejectSession({
+          id: proposal.id,
+          reason: getSdkError("UNSUPPORTED_CHAINS"),
+        })
+      ),
+    [availableNetworks, openWith, handleAsyncActionUnsafe]
+  );
 };
 
 const useOnSessionRequest = () => {
-  const { handleAsyncAction } = useAsyncActionHandler();
+  const { handleAsyncActionUnsafe } = useAsyncActionHandler();
 
-  return (event: WalletKitTypes.SessionRequest) =>
-    handleAsyncAction(async () => {
-      console.log("TODO: Session request received. Handling to be implemented", event);
+  return useCallback(
+    (event: WalletKitTypes.SessionRequest) =>
+      handleAsyncActionUnsafe(async () => {
+        console.log("TODO: Session request received. Handling to be implemented", event);
 
-      const response = formatJsonRpcError(event.id, getSdkError("USER_REJECTED_METHODS").message);
-      await walletKit.respondSessionRequest({ topic: event.topic, response });
-    }).catch(async () => {
-      // dApp is waiting so we need to notify it
-      const response = formatJsonRpcError(event.id, getSdkError("INVALID_METHOD").message);
-      await walletKit.respondSessionRequest({ topic: event.topic, response });
-    });
+        const response = formatJsonRpcError(event.id, getSdkError("USER_REJECTED_METHODS").message);
+        return walletKit.respondSessionRequest({ topic: event.topic, response });
+      }).catch(async () => {
+        // dApp is waiting so we need to notify it
+        const response = formatJsonRpcError(event.id, getSdkError("INVALID_METHOD").message);
+        return walletKit.respondSessionRequest({ topic: event.topic, response });
+      }),
+    [handleAsyncActionUnsafe]
+  );
 };
 
 // dApp can release WalletConnect session at any time and then the Wallet is notified by the WalletConnect server.
@@ -98,10 +115,4 @@ const useOnSessionDelete = () => {
     });
 };
 
-export const useOnWalletConnect = () => {
-  const { handleAsyncAction } = useAsyncActionHandler();
-  return (uri: string) =>
-    handleAsyncAction(async () => {
-      await walletKit.pair({ uri });
-    });
-};
+export const onWalletConnect = (uri: string) => () => walletKit.pair({ uri });
