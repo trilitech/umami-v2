8113b63b1c2ab0ddb4d120f4cb14d482c267506e
Ana Maksimovskikh <ana.maksimovskikh@trili.tech>
2024-10-31 14:47:49 +0000

    Add subresource integrity to embed

---
+++ apps/embed-iframe/src/vite-env.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="../../../global.d.ts" />
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
--- apps/embed-iframe/vite.config.ts
+++ apps/embed-iframe/vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import svgr from "vite-plugin-svgr";
+import sri from "vite-plugin-sri";
 
 export default defineConfig({
   base: "./",
@@ -14,6 +15,7 @@ export default defineConfig({
         Buffer: true,
       },
     }),
+    sri({ algorithm: "sha384" }),
   ],
   optimizeDeps: {
     esbuildOptions: {
