ec9041864878aa888fcf3b2b755989ca71587f6f
Oleg Chendighelean <oleg.chendighelean@trili.tech>
2024-11-18 19:17:51 +0000

    Add blur to navbar

--- apps/web/src/components/Navbar/Navbar.tsx
+++ apps/web/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
-import { Card, TabList, Tabs } from "@chakra-ui/react";
+import { Card, TabList, Tabs, useColorMode } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 
 import { Tab } from "../Tab";
 
@@ -21,23 +22,28 @@ const tabs = [
   },
 ];
 
-export const Navbar = () => (
-  <Card
-    zIndex={1}
-    padding={{ base: "6px 12px", md: "10px" }}
-    opacity={0.95}
-    borderRadius={{ base: 0, md: "100px" }}
-    boxShadow={{
-      base: "-2px -4px 10px 0px rgba(45, 55, 72, 0.06)",
-      md: "2px 4px 12px 0px rgba(45, 55, 72, 0.05)",
-    }}
-  >
-    <Tabs defaultIndex={-1} isFitted variant="solid-rounded">
-      <TabList gap="1px">
-        {tabs.map(tab => (
-          <Tab key={tab.to} label={tab.label} to={tab.to} />
-        ))}
-      </TabList>
-    </Tabs>
-  </Card>
-);
+export const Navbar = () => {
+  const colorMode = useColorMode();
+
+  return (
+    <Card
+      zIndex={1}
+      padding={{ base: "6px 12px", md: "10px" }}
+      background={mode("rgba(255, 255, 255, 0.90)", "rgba(34, 34, 63, 0.90)")(colorMode)}
+      borderRadius={{ base: 0, md: "100px" }}
+      boxShadow={{
+        base: "-2px -4px 10px 0px rgba(45, 55, 72, 0.06)",
+        md: "2px 4px 12px 0px rgba(45, 55, 72, 0.05)",
+      }}
+      backdropFilter="blur(5px)"
+    >
+      <Tabs defaultIndex={-1} isFitted variant="solid-rounded">
+        <TabList gap="1px">
+          {tabs.map(tab => (
+            <Tab key={tab.to} label={tab.label} to={tab.to} />
+          ))}
+        </TabList>
+      </Tabs>
+    </Card>
+  );
+};
