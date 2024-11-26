c2575feb794152ba27098dfb9896f7fbfa1c485b
Mario Norato <norato.mario@gmail.com>
2024-11-26 22:27:04 +0000

    feat: added the expo app

---
+++ apps/mobile-expo/metro.config.js
@@ -0,0 +1,18 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
+
+const workspaceRoot = path.resolve(__dirname, "../..");
+const projectRoot = __dirname;
+
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [workspaceRoot];
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules")
+];
+
+config.resolver.disableHierarchicalLookup = true;
+
+module.exports = config;
