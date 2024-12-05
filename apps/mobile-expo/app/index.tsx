2b010b711e4baa4a0f8f5761a911c535dd3137cd
Mario Norato <norato.mario@gmail.com>
2024-12-05 12:55:05 +0000

    feat: expo project - work in progress

--- apps/mobile-expo/app/index.tsx
+++ apps/mobile-expo/app/index.tsx
@@ -1,8 +1,20 @@
-import {TestComponent} from '@umami/mobile-components';
+import { TestComponent } from "@umami/mobile-components";
+import { config } from "@tamagui/config/v3";
+import { TamaguiProvider, createTamagui } from "@tamagui/core";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "../store/store";
 
+const tamaguiConfig = createTamagui(config);
 
 export default function Index() {
   return (
-    <TestComponent />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <TamaguiProvider config={tamaguiConfig}>
+          <TestComponent />
+         </TamaguiProvider>
+       </PersistGate>
+     </Provider>
   );
 }
