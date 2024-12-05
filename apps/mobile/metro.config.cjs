fb0e400cffd63b5b75e51f195f7e212a9e1ecd81
Mario Norato <norato.mario@gmail.com>
2024-12-05 12:55:05 +0000

    feat: expo project - work in progress

--- apps/mobile/metro.config.cjs
+++ apps/mobile/metro.config.cjs
@@ -1,30 +1,26 @@
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 const path = require("path");
 
-// Find the project and workspace directories
 const projectRoot = __dirname;
-// This can be replaced with `find-yarn-workspace-root`
 const monorepoRoot = path.resolve(projectRoot, "../..");
 
-module.exports = mergeConfig(getDefaultConfig(projectRoot),
-  {
-    watchFolders: [
-      monorepoRoot
-    ],
-    projectRoot,
-    resolver: {
-      extraNodeModules: {
-        crypto: require.resolve("crypto-browserify"),
-        http: require.resolve("stream-http"),
-        https: require.resolve("https-browserify"),
-        stream: require.resolve("stream-browserify"),
-        util: require.resolve("util"),
-        vm: require.resolve("vm-browserify"),
-        zlib: require.resolve("browserify-zlib"),
-      },
-      unstable_enablePackageExports: true,
-      unstable_enableSymlinks: true
+module.exports = mergeConfig(getDefaultConfig(projectRoot), {
+  watchFolders: [monorepoRoot],
+  projectRoot,
+  resolver: {
+    extraNodeModules: {
+      crypto: require.resolve("crypto-browserify"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      stream: require.resolve("stream-browserify"),
+      util: require.resolve("util"),
+      vm: require.resolve("vm-browserify"),
+      zlib: require.resolve("browserify-zlib"),
     },
-  });
-
-
+    unstable_enablePackageExports: true,
+    unstable_enableSymlinks: true,
+  },
+  transformer: {
+    babelTransformerPath: require.resolve("metro-react-native-babel-transformer"),
+  },
+});
