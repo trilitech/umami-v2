499e2a2ef1d4edb08ec66637ed2b1cd3bc47c04f
Mario Norato <norato.mario@gmail.com>
2024-12-05 12:55:05 +0000

    feat: expo project - work in progress

--- apps/mobile-expo/metro.config.js
+++ apps/mobile-expo/metro.config.js
@@ -1,18 +1,42 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
+// Learn more https://docs.expo.dev/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 const path = require("path");
 
-const workspaceRoot = path.resolve(__dirname, "../..");
+// Find the project and workspace directories
 const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, "../..");
 
-const config = getDefaultConfig(projectRoot);
+// Get the default configuration
+const defaultConfig = getDefaultConfig(projectRoot);
 
-config.watchFolders = [workspaceRoot];
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, "node_modules"),
-  path.resolve(workspaceRoot, "node_modules")
+// Extend the default configuration
+defaultConfig.watchFolders = [
+  workspaceRoot, // Add workspace root for monorepo compatibility
 ];
 
-config.resolver.disableHierarchicalLookup = true;
+defaultConfig.resolver = {
+  ...defaultConfig.resolver,
+  nodeModulesPaths: [
+    path.resolve(projectRoot, "node_modules"),
+    path.resolve(workspaceRoot, "node_modules"),
+  ],
+  extraNodeModules: {
+    crypto: require.resolve("crypto-browserify"),
+    http: require.resolve("stream-http"),
+    https: require.resolve("https-browserify"),
+    stream: require.resolve("stream-browserify"),
+    buffer: require.resolve("buffer"),
+    util: require.resolve("util"),
+    vm: require.resolve("vm-browserify"),
+    // zlib: require.resolve("browserify-zlib"),
+    process: require.resolve("process"),
+    "@tamagui/web": path.resolve(__dirname, "node_modules/@tamagui/web"),
+    "@tamagui/core": path.resolve(__dirname, "node_modules/@tamagui/core"),
+    zlib: require.resolve("pako"),
+  },
+  disableHierarchicalLookup: true,
+  unstable_enablePackageExports: true,
+  unstable_enableSymlinks: true,
+};
 
-module.exports = config;
+module.exports = defaultConfig;
