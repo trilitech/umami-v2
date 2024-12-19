1af3d1537d129aa4565de0e911dce92598cc5059
Diana Savatina <diana.savvatina@trili.tech>
2024-12-19 14:39:00 +0000

    feat: WalletConnect integration, part 9, getAccounts

--- apps/web/src/components/WalletConnect/useHandleWcRequest.tsx
+++ apps/web/src/components/WalletConnect/useHandleWcRequest.tsx
@@ -1,6 +1,12 @@
 import { SigningType } from "@airgap/beacon-wallet";
+import { useToast } from "@chakra-ui/react";
 import { useDynamicModalContext } from "@umami/components";
-import { type ImplicitAccount, estimate, toAccountOperations } from "@umami/core";
+import {
+  type ImplicitAccount,
+  estimate,
+  getPublicKeyAndCurve,
+  toAccountOperations,
+} from "@umami/core";
 import {
   useAsyncActionHandler,
   useFindNetwork,
@@ -9,7 +15,7 @@ import {
   walletKit,
 } from "@umami/state";
 import { WalletConnectError } from "@umami/utils";
-import { formatJsonRpcError } from "@walletconnect/jsonrpc-utils";
+import { formatJsonRpcError, formatJsonRpcResult } from "@walletconnect/jsonrpc-utils";
 import { type SessionTypes, type SignClientTypes, type Verify } from "@walletconnect/types";
 import { type SdkErrorKey, getSdkError } from "@walletconnect/utils";
 
@@ -21,7 +27,6 @@ import {
   type SignHeaderProps,
   type SignPayloadProps,
 } from "../SendFlow/utils";
-
 /**
  * @returns a function that handles a beacon message and opens a modal with the appropriate content
  *
@@ -34,6 +39,7 @@ export const useHandleWcRequest = () => {
   const getAccount = useGetOwnedAccountSafe();
   const getImplicitAccount = useGetImplicitAccount();
   const findNetwork = useFindNetwork();
+  const toast = useToast();
 
   return async (
     event: {
@@ -57,11 +63,42 @@ export const useHandleWcRequest = () => {
 
       switch (request.method) {
         case "tezos_getAccounts": {
-          throw new WalletConnectError(
-            "Getting accounts is not supported yet",
-            "WC_METHOD_UNSUPPORTED",
+          const wcPeers = walletKit.getActiveSessions();
+          if (!(topic in wcPeers)) {
+            throw new WalletConnectError(`Unknown session ${topic}`, "UNAUTHORIZED_EVENT", null);
+          }
+          const session = wcPeers[topic];
+          const accountPkh = session.namespaces.tezos.accounts[0].split(":")[2];
+          const signer = getImplicitAccount(accountPkh);
+          const networkName = session.namespaces.tezos.chains?.[0].split(":")[1];
+          const network = networkName ? findNetwork(networkName) : null;
+          if (!network) {
+            throw new WalletConnectError(
+              `Unsupported network ${networkName}`,
+              "UNSUPPORTED_CHAINS",
+              session
+            );
+          }
+          const { publicKey, curve } = await getPublicKeyAndCurve(
+            accountPkh,
+            signer,
+            network,
             session
           );
+          const response = formatJsonRpcResult(id, [
+            {
+              algo: curve,
+              address: accountPkh,
+              pubkey: publicKey,
+            },
+          ]);
+          await walletKit.respondSessionRequest({ topic, response });
+
+          toast({
+            description: "Successfully provided the requested account data",
+            status: "success",
+          });
+          return;
         }
 
         case "tezos_sign": {
