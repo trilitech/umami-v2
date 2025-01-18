cda3444a560c40c6e05288070f1b77386b6f3d80
MarioTrilitech <mario.norato@trili.tech>
2025-01-18 14:50:35 +0000

    refactor: removed expo-app and expo-app-test folders

--- apps/mobile-expo/store/store.ts
+++
@@ -1,8 +0,0 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { makeStore } from "@umami/state";
-import { persistStore } from "redux-persist";
-
-const store = makeStore(AsyncStorage);
-export const persistor = persistStore(store);
-
-export default store;
