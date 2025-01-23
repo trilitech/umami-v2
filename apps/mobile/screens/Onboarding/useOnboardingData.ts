39d7e2c2189820f441d6291512fd84463fb392be
Oleg Chendighelean <oleg.chendighelean@trili.tech>
2025-01-23 17:41:46 +0000

    Add social auth providers

--- apps/mobile/screens/Onboarding/useOnboardingData.ts
+++ apps/mobile/screens/Onboarding/useOnboardingData.ts
@@ -1,35 +1,18 @@
-import { b58cencode, prefix } from "@taquito/utils";
-// @ts-ignore
-import { useRestoreSocial } from "@umami/state";
-import { MAINNET, getPublicKeyPairFromSk, makeToolkit } from "@umami/tezos";
+
+import { type IDP } from "@umami/social-auth";
+import { useAsyncActionHandler, useRestoreSocial } from "@umami/state";
+import { getPublicKeyPairFromSk } from "@umami/tezos";
 import * as Linking from "expo-linking";
 import { useRouter } from "expo-router";
 import { useCallback, useEffect } from "react";
 
 import { STRINGS } from "../../constants";
-import { createWeb3AuthInstance } from "../../services/web3AuthFactory";
+import { forIDP } from "../../services/forIDP";
 import { openBrowser } from "../../utils/browserUtils";
-import { saveToken } from "../../utils/tokenManager";
-
-type Web3AuthLoginResponse = {
-  aggregateVerifier: string;
-  appState: string;
-  dappShare: string;
-  email?: string;
-  idToken?: string;
-  isMfaEnabled: boolean;
-  name?: string;
-  oAuthAccessToken?: string;
-  oAuthIdToken?: string;
-  profileImage?: string;
-  typeOfLogin: string;
-  verifier: string;
-  verifierId: string;
-};
 
 export const useOnboardingData = () => {
   const router = useRouter();
-  const web3auth = createWeb3AuthInstance();
+  const { handleAsyncAction } = useAsyncActionHandler();
   const restoreSocial = useRestoreSocial();
 
   useEffect(() => {
@@ -40,67 +23,20 @@ export const useOnboardingData = () => {
 
     const subscription = Linking.addEventListener("url", handleUrl);
 
-    const initializeWeb3Auth = async () => {
-      try {
-        await web3auth.init();
-      } catch (error) {
-        console.error("Error initializing Web3Auth:", error);
-      }
-    };
-
-    void initializeWeb3Auth();
-
     return () => {
       subscription.remove();
     };
-  }, [web3auth]);
+  }, []);
 
-  const login = useCallback(
-    async (loginProvider: string) => {
-      if (!web3auth.ready) {
-        console.error("Web3Auth is not initialized.");
-        return;
-      }
+  const login = (idp: IDP) =>
+    handleAsyncAction(async () => {
+        const { secretKey, name, id, email } = await forIDP(idp).getCredentials();
+        const { pk, pkh } = await getPublicKeyPairFromSk(secretKey);
+      restoreSocial(pk, pkh, email || name || id, idp);
+      router.replace("/home");
+    });
 
-      try {
-        const web3authResponse = (await web3auth.login({
-          loginProvider,
-        })) as unknown as Web3AuthLoginResponse;
-
-        if (web3auth.connected) {
-          const userInfo = web3auth.userInfo();
-          console.log("userInfo", userInfo);
-          const web3authProvider = web3auth.provider;
-          const privateKey = (await web3authProvider?.request({ method: "private_key" })) as string;
-          const secretKey = b58cencode(privateKey, prefix.spsk);
-          const tezos = await makeToolkit({ type: "social", secretKey, network: MAINNET });
-
-          console.log("private key", privateKey);
-
-          // const keyPair = tezosCrypto.utils.seedToKeyPair(hex2buf(privateKey));
-          // const account = keyPair?.pkh;
-          const { pk, pkh } = await getPublicKeyPairFromSk(secretKey);
-
-          restoreSocial(pk, pkh, userInfo?.email || userInfo?.name || "Social account", "google");
-          console.log("account", pkh);
-
-          const balance = await tezos.tz.getBalance(pkh);
-          console.log("balance", balance);
-
-          if (web3authResponse.idToken) {
-            await saveToken("authToken", web3authResponse.idToken);
-          }
-
-          router.replace("/home");
-        }
-      } catch (error: any) {
-        console.error("Login error:", error.message);
-      }
-    },
-    [web3auth, router]
-  );
-
-  const createLoginHandler = (provider: string) => () => login(provider);
+  const createLoginHandler = (provider: IDP) => () => login(provider);
 
   const onGoogleLogin = createLoginHandler("google");
   const onFacebookLogin = createLoginHandler("facebook");
