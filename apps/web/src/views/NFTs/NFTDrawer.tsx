fc9f9dd9c26087c5791c488fade2a9dd37761eeb
Oleg Chendighelean <oleg.chendighelean@trili.tech>
2025-03-06 16:47:33 +0000

    feat: improve NFT drawer styling

--- apps/web/src/views/NFTs/NFTDrawer.tsx
+++ apps/web/src/views/NFTs/NFTDrawer.tsx
@@ -3,14 +3,29 @@ import { type NFTBalance } from "@umami/core";
 
 import { NFTDrawerCard } from "./NFTDrawerCard";
 import { DrawerCloseButton } from "../../components/CloseButton";
+import { useColor } from "../../styles/useColor";
 
-export const NFTDrawer = ({ nft }: { nft: NFTBalance }) => (
-  <>
-    <DrawerHeader justifyContent="flex-end" display="flex" minHeight={{ base: "54px", md: "70px" }}>
-      <DrawerCloseButton />
-    </DrawerHeader>
-    <DrawerBody>
-      <NFTDrawerCard nft={nft} />
-    </DrawerBody>
-  </>
-);
+export const NFTDrawer = ({ nft }: { nft: NFTBalance }) => {
+  const color = useColor();
+  const bgColor = color("255, 255, 255", "34, 34, 63");
+
+  return (
+    <>
+      <DrawerHeader
+        position="absolute"
+        zIndex="1"
+        justifyContent="flex-end"
+        display="flex"
+        width="100%"
+        minHeight={{ base: "54px", md: "70px" }}
+        backdropFilter="blur(10px)"
+        backgroundColor={`rgba(${bgColor}, 0.9)`}
+      >
+        <DrawerCloseButton />
+      </DrawerHeader>
+      <DrawerBody paddingTop={{ base: "54px", md: "70px" }}>
+        <NFTDrawerCard nft={nft} />
+      </DrawerBody>
+    </>
+  );
+};
