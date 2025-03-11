6872a3020a1c54d4c5ae63c9fff7f6e0fd721241
Alessandro Catania <alecat88@gmail.com>
2025-03-11 14:59:53 +0000

    test sign contract on vercel

--- apps/server/src/passkey/config.ts
+++ apps/server/src/passkey/config.ts
@@ -16,5 +16,7 @@ export const rpName = 'SimpleWebAuthn Example';
 // export const origin = `https://${rpID}`;
 
 // for localhost
-export const rpID = 'localhost';
-export const origin = `http://${rpID}:5173`;
+export const rpID = 'umami-v2-d87pos9qt-trili-tech.vercel.app';
+export const origin = "https://umami-v2-d87pos9qt-trili-tech.vercel.app";
+// export const rpID = 'localhost';
+// export const origin = `http://${rpID}:5173`;
