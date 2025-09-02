bcfc11038192b294910d6714ec744bb208db7e70
Alessandro Catania <alecat88@gmail.com>
2025-09-02 12:33:34 +0100

    update taquito to latest version

--- apps/desktop/src/components/Onboarding/restoreSecretKey/RestoreSecretKey.test.tsx
+++ apps/desktop/src/components/Onboarding/restoreSecretKey/RestoreSecretKey.test.tsx
@@ -94,7 +94,7 @@ describe("<RestoreSecretKey />", () => {
     await act(() => user.click(screen.getByTestId("restore-continue-button")));
 
     expect(mockToast).toHaveBeenCalledWith({
-      description: "Key-password pair is invalid",
+      description: "can't decrypt secret key",
       status: "error",
       isClosable: true,
     });
@@ -111,7 +111,7 @@ describe("<RestoreSecretKey />", () => {
     await act(() => user.click(screen.getByTestId("restore-continue-button")));
 
     expect(mockToast).toHaveBeenCalledWith({
-      description: "Invalid secret key: checksum doesn't match",
+      description: "Invalid private key",
       status: "error",
       isClosable: true,
     });
@@ -128,8 +128,7 @@ describe("<RestoreSecretKey />", () => {
     await act(() => user.click(screen.getByTestId("restore-continue-button")));
 
     expect(mockToast).toHaveBeenCalledWith({
-      description:
-        "Invalid private key with unsupported prefix expecting one of the following 'edesk', 'edsk', 'spsk', 'spesk', 'p2sk' or 'p2esk'..",
+      description: "Invalid private key",
       status: "error",
       isClosable: true,
     });
